fc97ce5f68da44ee7861c05eaae275469179bb60
Steven Penny <svnpenn@gmail.com>
2012-06-29 21:03:51 -0500

    M  AdobeHDS.sh

--- AdobeHDS.sh
+++ AdobeHDS.sh
@@ -2,20 +2,22 @@
 PATH+=":."
 p="plugin-container.exe"
 
-pid(){
-  ps -W | grep "$1" | cut -c-9
-}
-
 binparse(){
   grep -azm1 "$1" p.core
+}
+
+pid(){
+  ps -W | grep "$1" | cut -c-9
 }
 
 red(){
   printf "\e[1;31m%s\e[m\n" "$1"
 }
 
-# Clear Firefox cache
+# Kill flash player
 pid "$p" | xargs /bin/kill -f
+# Disable protected mode, 32 and 64 bit Windows
+printf "ProtectedMode=0" > "$(cygpath -S)/Macromed/Flash/mms.cfg"
 red 'Press enter after video starts'; read
 red 'Printing results'
 # Dump flash player
