d22dbcd9f7974e7782893e10626d4b63830395c6
Steven Penny <svnpenn@gmail.com>
2012-07-04 05:49:28 -0500

    M  git-large.sh

--- git-large.sh
+++ git-large.sh
@@ -12,11 +12,11 @@ red 'Printing results'
 
 while read commit; do
   while read bits type sha size path; do
-    [ $size -gt 100000 ] && big_files[$sha]="$path $size"
-  done < <(git ls-tree -rl $commit)
+    [ $size -gt 100000 ] && big_files[$sha]="$sha $size $path"
+  done < <(git ls-tree --abbrev -rl $commit)
 done < <(git rev-list HEAD)
 
 for file in "${big_files[@]}"; do
-  read path size <<< "$file"
-  git cat-file -e HEAD:$path 2>/dev/null || echo $file 
+  read sha size path <<< "$file"
+  git ls-tree -r HEAD | grep -q $sha || echo $file
 done
