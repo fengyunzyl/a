25647f3985bec9723827422ae55da4cb0bfdb827
Steven Penny <svnpenn@gmail.com>
2012-07-04 21:20:50 -0500

    A  git-remove.sh

---
+++ git-remove.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Git remove sensitive data
+# help.github.com/articles/remove-sensitive-data
+
+[ ! $1 ] && echo "Usage: rsd FILE" && return
+
+git filter-branch \
+  --index-filter "git rm --cached --ignore-unmatch $1" \
+  --prune-empty --tag-name-filter cat -- --all || exit
+
+# Cleanup and reclaming space
+rm -r .git/refs/original
+git reflog expire --expire=now --all
+git gc --prune=now
+git gc --aggressive --prune=now
