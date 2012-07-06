4f1842e9b56817dd0ec8d68fca69e743094d86b4
Steven Penny <svnpenn@gmail.com>
2012-07-06 05:18:43 -0500

    A  youtube.sh

---
+++ youtube.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Bash download from YouTube
+# youtube.com/watch?v=LHelEIJVxiE
+
+[ ! $1 ] && echo "Usage: ${0##*/} URL" && exit
+
+host="m.youtube.com"
+exec 3<>/dev/tcp/$host/80
+
+headers=(
+  "GET ${1#*youtube.com} HTTP/1.1"
+  "Connection: close"
+  "Host: $host"
+  "User-Agent: iPad"
+  ""
+)
+
+printf "%s\r\n" "${headers[@]}" >&3
+cat > index.htm <&3
+sed 's.\\".\n.g' index.htm | grep videoplayback
