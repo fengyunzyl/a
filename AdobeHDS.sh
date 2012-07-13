a4b111738bd36fd30824607acd722f574998eade
Steven Penny <svnpenn@gmail.com>
2012-07-13 18:25:45 -0500

    AdobeHDS

--- AdobeHDS.sh
+++ AdobeHDS.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-PATH+=":."
 p="plugin-container.exe"
 
 binparse(){
   grep -azm1 "$1" p.core
 }
 
-pid(){
+pidof(){
   ps -W | grep "$1" | cut -c-9
 }
 
@@ -14,28 +13,16 @@ red(){
   printf "\e[1;31m%s\e[m\n" "$1"
 }
 
-# Kill flash player
-pid "$p" | xargs /bin/kill -f
-# Disable protected mode, 32 and 64 bit Windows
+pidof "$p" | xargs /bin/kill -f
 printf "ProtectedMode=0" > "${COMSPEC%\\*}/Macromed/Flash/mms.cfg"
 red 'Press enter after video starts'; read
 red 'Printing results'
-# Dump flash player
-pid "$p" | xargs dumper p &
+pidof "$p" | xargs dumper p &
 sleep 1
-
-# Script
+pidof "$p" | xargs /bin/kill -f
 read s < <(which AdobeHDS.php | cygpath -mf-)
-
-# Auth
 read a < <(binparse "Frag.?" | cut -d? -f2)
-
-# Manifest
 read m < <(binparse "http.*f4m?")
-
-# Useragent
 read u < <(binparse "Mozilla/5.0")
-
-# Run
 set -x
 php "$s" --auth "$a" --manifest "$m" --useragent "$u"
