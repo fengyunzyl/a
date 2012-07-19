d826a7f33249deb460ea1175d172a54175a6b816
Steven Penny <svnpenn@gmail.com>
2012-07-19 08:48:13 -0500

    ?? t.sh

---
+++ t.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Tests for my scripts
+
+# nick.com/videos/clip/penguins-of-madagascar-223a-a-visit-from-uncle-nigel-full-episode.html
+# stream-recorder.com/forum/capture-clip-t13192.html
+# stream-recorder.com/forum/radio-105-net-link-t13161.html
