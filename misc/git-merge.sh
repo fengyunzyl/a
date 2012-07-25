d50204565602475185f6f7309b5d1907fafe5d00
Steven Penny <svnpenn@gmail.com>
2012-07-24 22:33:01 -0500

    M misc/git-merge.sh

--- misc/git-merge.sh
+++ misc/git-merge.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
-# Merge commits from another repo
+# Merge commits from another branch
 
-echo 'Enter my user name'
-read user
-
-echo 'Enter repo name'
-read repo
-git clone git@github.com:$user/$repo.git
-cd $repo
-
-echo 'Enter repo URL to merge from'
-read from_url
-git remote add upstream $from_url
-git fetch upstream
-git branch -a
-echo 'Enter name of branch with new commits'
-read target_branch
-
-git checkout $target_branch || {
-    echo 'Enter starting branch'
-    read start_branch
-    git checkout $start_branch
-    git checkout -b $target_branch
-}
-
-# git merge upstream/$target_branch
-git push origin $target_branch
+git clone git://github.com/svnpenn/rtmpdump.git
+cd rtmpdump
+git checkout master
+git branch ksv
+git checkout ksv
+git apply -p0 ../Patch.diff
+git status -s >/tmp/s
+git add -A
+git commit -F- </tmp/s
+git checkout handshake-10
+git merge -X theirs ksv
