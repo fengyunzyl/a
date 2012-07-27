2b0d0d443ebf33aa330a53ac888bf3e998e4b40c
Steven Penny <svnpenn@gmail.com>
2012-07-27 17:20:38 -0500

    D build/win32/git/git-mingw-full.sh  D build/win32/git/git-mingw-min.sh  D build/win32/openssh/openssh-copy.sh  D build/win32/openssh/openssh-w32-build.sh  D build/win32/openssh/openssh.patch  D build/win32/openssl/openssl-w32-cygwin.sh  D build/win32/wget/wget-w32-mingw.sh  M jekyll-publish.sh

--- jekyll-publish.sh
+++ jekyll-publish.sh
@@ -9,14 +9,14 @@ git status -s | git commit -F-
 git push origin source || exit
 
 # Push master branch
-jekyll
+jekyll || exit
 grep --color -r 'Liquid.error' . && exit
 git checkout master
 git rm -qr .
 cp -r _site/. .
 rm -r _site
 git add -A
-read s f < <(git status -s)
+read _ s < <(git status -s)
 git status -s | git commit -F-
 git push origin master || exit
 
@@ -26,6 +26,6 @@ check(){
     for i in {0..9}; do echo -n $i; sleep 1; done
   done
 }
-check $f svnpenn.github.com/$f
+check $s svnpenn.github.com/$s
 git checkout source
 echo 'Publish complete!'
