a71cc9552b1c3254afbc91d4eea739d4b28c07fa
Steven Penny <svnpenn@gmail.com>
2012-07-28 02:21:09 -0500

    M misc/git-merge.sh

--- misc/git-merge.sh
+++ misc/git-merge.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Merge commits from another branch
 
-git clone git://github.com/svnpenn/rtmpdump.git
+git clone git@github.com:svnpenn/rtmpdump.git
 cd rtmpdump
 git checkout master
-git branch ksv
-git checkout ksv
-git apply -p0 ../Patch.diff
-git status -s >/tmp/s
-git add -A
-git commit -F- </tmp/s
-git checkout handshake-10
-git merge -X theirs ksv
+git remote add upstream git://git.ffmpeg.org/rtmpdump
+git fetch upstream
+git merge upstream/master
+git push origin master
+
+git checkout svnpenn
+git merge master
+git push origin svnpenn
