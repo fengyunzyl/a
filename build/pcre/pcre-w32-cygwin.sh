ff1d18427d3e9ab79ed0b48103380d2f2424ee74
Steven Penny <svnpenn@gmail.com>
2012-07-29 21:29:24 -0500

    M README  D build/intltool/intltool-w32-build.sh ?? build/pcre/

---
+++ build/pcre/pcre-w32-cygwin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Build PCRE
+
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.31.tar.bz2
+tar xf pcre*
+cd pcre*
+: i686-w64-mingw32
+./configure --disable-cpp --host=$_ --prefix=/usr/$_/sys-root/mingw
+make install
