1183bf9fb281b84ab42999bd8958f2f5656facde
Steven Penny <svnpenn@gmail.com>
2012-09-07 12:53:00 -0500

    M AdobeHDS.sh

--- AdobeHDS.sh
+++ AdobeHDS.sh
@@ -10,15 +10,21 @@ pidof(){
   ps -W | grep $1 | cut -c-9
 }
 
+die(){
+  echo -e "\e[1;31m$1\e[m"
+  exit
+}
+
 warn(){
   echo -e "\e[1;35m$1\e[m"
 }
 
 pidof $p | xargs /bin/kill -f
-echo ProtectedMode=0 > \\windows/system32/macromed/flash/mms.cfg
+echo ProtectedMode=0 2>/dev/null >\\windows/system32/macromed/flash/mms.cfg
 warn 'Killed flash player for clean dump.
 Restart video then press enter here'; read
-pidof $p | xargs timeout 1 dumper p
+read < <(pidof $p) || die "$p not found!"
+timeout 1 dumper p $REPLY 2>/dev/null
 IFS=? read _ a < <(binparse "Frag")
 read m < <(binparse "http.*f4m?")
 read u < <(binparse "Mozilla/5.0")
