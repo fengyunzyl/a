4a6f6a0e69aa48b813e9de6b37408a5db19b0b58
Steven Penny <svnpenn@gmail.com>
2012-10-17 01:25:44 -0500

    M AdobeHDS.sh

--- AdobeHDS.sh
+++ AdobeHDS.sh
@@ -8,11 +8,6 @@ pidof(){
   ps -W | awk /$1/'{print$4;exit}'
 }
 
-die(){
-  echo -e "\e[1;31m$1\e[m"
-  exit
-}
-
 warn(){
   echo -e "\e[1;35m$1\e[m"
   read
@@ -28,7 +23,7 @@ killall $pc
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
 warn 'Killed flash player for clean dump.
 Restart video then press enter here.'
-read < <(pidof $pc) || die "$p not found!"
+until read < <(pidof $pc); do warn "$pc not found!"; done
 rm -f pg.core
 dumper pg $REPLY &
 until [ -s pg.core ]; do sleep 1; done
@@ -37,5 +32,5 @@ read mn < <(binparse "http[^?]*f4m(\?|$)[^']*")
 read ur < <(binparse "Mozilla/5.0.*")
 rm pg.core
 set -x
-php "$ab" --manifest "$mn" && exit
+php "$ab" --manifest "$mn" ||
 php "$ab" --manifest "$mn" --auth "$ah" --useragent "$ur"
