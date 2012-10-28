db7e75d9aaafb5aaffd668253a7f90b9f764ec43
Steven Penny <svnpenn@gmail.com>
2012-10-28 12:46:12 -0500

    M hulu.sh

--- hulu.sh
+++ hulu.sh
@@ -5,20 +5,20 @@ warn(){
   read rp
 }
 
-pidof(){
+pgrep(){
   ps -W | awk /$1/'{print$4;exit}'
 }
 
-killall(){
-  pidof $1 | xargs kill -f
+pkill(){
+  pgrep $1 | xargs kill -f
 }
 
 pc=plugin-container
-killall $pc
+pkill $pc
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
 warn 'Killed flash player for clean dump.
 Restart video then press enter here.'
-until read < <(pidof $pc); do warn "$pc not found!"; done
+until read < <(pgrep $pc); do warn "$pc not found!"; done
 rm -f pg.core
 dumper pg $REPLY &
 until [ -s pg.core ]; do sleep 1; done
@@ -30,17 +30,17 @@ for i in "${!vids[@]}"; do
   IFS=\" read -a vid <<< "${vids[i]}"
   j=0
   while [[ "${vid[j]}" =~ \ ([^=]*) ]]; do
-    attr[$i,${BASH_REMATCH[1]}]="${vid[j+1]}"
+    read ${BASH_REMATCH[1]//[-:]}[i] <<< "${vid[j+1]}"
     ((j+=2))
   done
-  printf "%2d\t%9s\t%s\n" "$i" "${attr[$i,file-type]}" "${attr[$i,cdn]}"
+  printf "%2d\t%9s\t%s\n" "$i" "${filetype[i]}" "${cdn[i]}"
 done
 
 warn 'Make choice. Avoid level3.'
 set -x
 rtmpdump \
--W "http://download.hulu.com/huludesktop.swf" \
--a "${attr[$rp,server]#*//*/}?${attr[$rp,token]//amp;}" \
--o a.flv \
--r "${attr[$rp,server]}" \
--y "${attr[$rp,stream]}"
+  -o a.flv \
+  -W http://download.hulu.com/huludesktop.swf \
+  -r "${server[rp]}" \
+  -y "${stream[rp]}" \
+  -a "${server[rp]#*//*/}?${token[rp]//amp;}"
