7fc7a141aa6486168d5e28afb21a91677daccc0c
Steven Penny <svnpenn@gmail.com>
2012-11-03 18:10:09 -0500

    M install/gitk/gitk-cygwin.sh

--- install/gitk/gitk-cygwin.sh
+++ install/gitk/gitk-cygwin.sh
@@ -2,20 +2,18 @@
 # Use gitk on Cygwin without X11
 # Requires Cygwin packages: git, make, mingw64-i686-gcc-core, wget 
 
-v=8.5.11
-
 # Install Tcl
-wget prdownloads.sf.net/tcl/tcl$v-src.tar.gz
-tar xf tcl$v-src.tar.gz
-cd tcl$v/win
+wget prdownloads.sf.net/tcl/tcl8.5.12-src.tar.gz
+tar xf tcl8.5.12-src.tar.gz
+cd tcl8.5.12/win
 ./configure --host i686-w64-mingw32
 make install
 cd -
 
 # Install Tk
-wget prdownloads.sf.net/tcl/tk$v-src.tar.gz
-tar xf tk$v-src.tar.gz
-cd tk$v/win
+wget prdownloads.sf.net/tcl/tk8.5.12-src.tar.gz
+tar xf tk8.5.12-src.tar.gz
+cd tk8.5.12/win
 ./configure --host i686-w64-mingw32
 make install
 cd -
@@ -23,5 +21,6 @@ cd -
 # Install gitk
 cd /usr/local/bin
 wget raw.github.com/git/git/master/gitk-git/gitk
+chmod 700 gitk
 echo 'cygpath -m "$1" | xargs wish85' > wish
 cd -
