6512bf638cf32504729303770a69acba34a46e2d
Steven Penny <svnpenn@gmail.com>
2012-11-09 23:29:20 -0600

    M hulu.sh

--- hulu.sh
+++ hulu.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-warn(){
+warn()
+{
   echo -e "\e[1;35m$1\e[m"
   read rp
 }
 
-pgrep(){
+pgrep()
+{
   ps -W | awk /$1/'{print$4;exit}'
 }
 
-pkill(){
+pkill()
+{
   pgrep $1 | xargs kill -f
 }
 
@@ -18,22 +21,34 @@ pkill $pc
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
 warn 'Killed flash player for clean dump.
 Restart video then press enter here.'
-until read < <(pgrep $pc); do warn "$pc not found!"; done
+
+until read < <(pgrep $pc)
+  do
+    warn "$pc not found!"
+  done
+
 rm -f pg.core
 dumper pg $REPLY &
-until [ -s pg.core ]; do sleep 1; done
+
+until [ -s pg.core ]
+  do
+    sleep 1
+  done
+
 mapfile vids < <(grep -aoz "<video [^>]*>" pg.core | sort | uniq -w123)
 rm pg.core
 
-for i in "${!vids[@]}"; do
-  IFS=\" read -a vid <<< "${vids[i]}"
-  j=0
-  while [[ "${vid[j]}" =~ \ ([^=]*) ]]; do
-    read ${BASH_REMATCH[1]//[-:]}[i] <<< "${vid[j+1]}"
-    ((j+=2))
+for i in "${!vids[@]}"
+  do
+    IFS=\" read -a vid <<< "${vids[i]}"
+    j=0
+    while [[ "${vid[j]}" =~ \ ([^=]*) ]]
+      do
+        read ${BASH_REMATCH[1]//[-:]}[i] <<< "${vid[j+1]}"
+        ((j+=2))
+      done
+    printf "%2d\t%9s\t%s\n" "$i" "${filetype[i]}" "${cdn[i]}"
   done
-  printf "%2d\t%9s\t%s\n" "$i" "${filetype[i]}" "${cdn[i]}"
-done
 
 warn 'Make choice. Avoid level3.'
 set -x
