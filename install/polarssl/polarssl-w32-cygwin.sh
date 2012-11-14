42ec35761a5ea6a6cfa76e19babcde3ea6fc8b47
Steven Penny <svnpenn@gmail.com>
2012-11-13 22:52:04 -0600

    M install/polarssl/polarssl-w32-cygwin.sh  M install/rtmpdump/rtmpdump-w32-cygwin.sh

--- install/polarssl/polarssl-w32-cygwin.sh
+++ install/polarssl/polarssl-w32-cygwin.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
-HOST=i686-w64-mingw32
-
-# Install Cygwin packages
-./setup -nqs ftp://lug.mtu.edu/cygwin -P make,mingw64-i686-gcc-core,wget
-
-# Determine correct PolarSSL
-git clone git://git.ffmpeg.org/rtmpdump
-cd rtmpdump
-v='0.14.3'
-grep -r ciphersuite * && v='1.0.0'
-grep -r havege_random * && v='1.1.3'
-cd -
-
 # Install PolarSSL
-wget polarssl.org/code/releases/polarssl-$v-gpl.tgz
-tar xf polarssl*
-cd polarssl*
-# SYS_LDFLAGS=-lws2_32 is only needed for APPS, which we are not using.
-make APPS= AR="$HOST-ar" CC="$HOST-gcc"
-make install DESTDIR="/usr/$HOST/sys-root/mingw"
+
+wget polarssl.org/code/releases/polarssl-1.2.0-gpl.tgz
+tar xf polarssl-1.2.0-gpl.tgz
+cd polarssl-1.2.0
+make lib AR=i686-w64-mingw32-ar CC=i686-w64-mingw32-gcc OFLAGS=-Os
+make install DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw
--- install/rtmpdump/rtmpdump-w32-cygwin.sh
+++ install/rtmpdump/rtmpdump-w32-cygwin.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-# This is to build MY RtmpDump. I have dropped support for building "official"
-# RtmpDump.
-
-# Install PolarSSL
-wget polarssl.org/code/releases/polarssl-1.2.0-gpl.tgz
-tar xf polarssl-1.2.0-gpl.tgz
-cd polarssl-1.2.0
-make APPS= AR=i686-w64-mingw32-ar CC=i686-w64-mingw32-gcc
-make install DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw
-cd -
 
 # Install Zlib
 wget zlib.net/zlib-1.2.7.tar.bz2
 tar xf zlib-1.2.7.tar.bz2
 cd zlib-1.2.7
-make install -f win32/Makefile.gcc BINARY_PATH=/bin \
-  DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw INCLUDE_PATH=/include \
-  LIBRARY_PATH=/lib PREFIX=i686-w64-mingw32-
+make install -f win32/Makefile.gcc \
+  BINARY_PATH=/bin \
+  INCLUDE_PATH=/include \
+  LIBRARY_PATH=/lib \
+  DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw \
+  PREFIX=i686-w64-mingw32- \
+  CFLAGS=-Os
+cd -
+
+# Install PolarSSL
+wget polarssl.org/code/releases/polarssl-1.2.0-gpl.tgz
+tar xf polarssl-1.2.0-gpl.tgz
+cd polarssl-1.2.0
+make lib \
+  AR=i686-w64-mingw32-ar \
+  CC=i686-w64-mingw32-gcc \
+  OFLAGS=-Os
+make install \
+  DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw
 cd -
 
 # Install RtmpDump
@@ -24,8 +30,17 @@ git clone git://github.com/svnpenn/rtmpdump.git
 cd rtmpdump
 git checkout svnpenn
 read < <(git describe --tags)
-make CROSS_COMPILE=i686-w64-mingw32- CRYPTO=POLARSSL SYS=mingw SHARED= \
-  XLDFLAGS=-static VERSION=$REPLY
-
+make \
+  SYS=mingw \
+  CRYPTO=POLARSSL \
+  CROSS_COMPILE=i686-w64-mingw32- \
+  SHARED= \
+  XLDFLAGS=-static \
+  VERSION=$REPLY \
+  OPT=-Os
 # Build librtmp.dll
-make CROSS_COMPILE=i686-w64-mingw32- CRYPTO=POLARSSL SYS=mingw
+make \
+  SYS=mingw \
+  CRYPTO=POLARSSL \
+  CROSS_COMPILE=i686-w64-mingw32- \
+  OPT=-Os
