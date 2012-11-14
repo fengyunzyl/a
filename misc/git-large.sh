00f1a4df608acccd12ee5869ae5d0c6767e86973
Steven Penny <svnpenn@gmail.com>
2012-11-13 20:22:02 -0600

    M misc/git-large.sh

--- misc/git-large.sh
+++ misc/git-large.sh
@@ -15,15 +15,18 @@ while read commit
   do
     while read bits type sha size path
       do
-        if [ $size -gt 100000 ]
-          then
+        [ $size -gt 10000 ] &&
+          {
             big_files[$sha]="$sha $size $path"
-        fi
+          }
       done < <(git ls-tree --abbrev -rl $commit)
   done < <(git rev-list HEAD)
 
 for file in "${big_files[@]}"
   do
     read sha size path <<< "$file"
-    git ls-tree -r HEAD | grep -q $sha || echo $file
+    git ls-tree -r HEAD | grep -q $sha ||
+      {
+        echo $file
+      }
   done
