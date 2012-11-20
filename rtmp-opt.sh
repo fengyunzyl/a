21c89e01253ea1fcfe55f79087ca147c8d9129f4
Steven Penny <svnpenn@gmail.com>
2012-11-20 09:00:10 -0600

    M rtmp-opt.sh

--- rtmp-opt.sh
+++ rtmp-opt.sh
@@ -16,7 +16,7 @@ warn 'Enter full RtmpDump command.'
 read
 declare -a aa="($REPLY)"
 
-while getopts "C:W:a:f:o:p:r:vy:" opt "${aa[@]:1}"
+while getopts "C:RW:a:f:o:p:r:vy:" opt "${aa[@]:1}"
   do
     declare _$opt="$OPTARG"
   done
@@ -25,9 +25,13 @@ while getopts "C:W:a:f:o:p:r:vy:" opt "${aa[@]:1}"
 _p=${_p%/*}
 _p=${_p/www.}
 # strip -r
+_r=${_r/:1935\//\/}
 _r=${_r%/}
+# strip -y
+_y=${_y%.mp4}
 
 try rtmpdump -o a.flv -i "$_r/$_y" ||
-try rtmpdump -o a.flv -i "\"$_r/$_y live=1\"" ||
 try rtmpdump -o a.flv -i "\"$_r/$_y app=$_a\"" ||
-try rtmpdump -o a.flv -i "\"$_r/$_y pageUrl=$_p\""
+try rtmpdump -o a.flv -i "\"$_r/$_y live=1\"" ||
+try rtmpdump -o a.flv -i "\"$_r/$_y pageUrl=$_p\"" ||
+try rtmpdump -o a.flv -i "\"$_r playpath=$_y\""
