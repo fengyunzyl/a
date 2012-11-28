e2d2222dc9e3d3d2167e8c74f2c949324c9fcacf
Steven Penny <svnpenn@gmail.com>
2012-11-28 14:28:56 -0600

    M misc/git-merge-strategy.sh

--- misc/git-merge-strategy.sh
+++ misc/git-merge-strategy.sh
@@ -6,17 +6,14 @@ cd rtmpdump
 
 # Patch with KSV code
 git apply -p0 ../Patch.diff
-git add -A
-git commit -m KSV
+git commit -am ksv
 
-# Patch with Xeebo code
-git checkout 603f
-git branch xeebo
-git checkout xeebo
+# Patch with Handshake 10
+git checkout -b handshake-10 603f
 git apply ../0001-Handshake-10.patch
-git add -A
-git commit -m Xeebo
+git add .
+git commit -m 'Handshake 10'
 
 # Merge branches
 git checkout master
-git merge -s ours xeebo
+git merge -s ours handshake-10
