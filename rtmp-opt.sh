b00c4bbd35d7693b8d0789c6ad8839ae51305a78
Steven Penny <svnpenn@gmail.com>
2012-11-27 19:25:16 -0600

    M rtmp-opt.sh

--- rtmp-opt.sh
+++ rtmp-opt.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Optimize RTMP string
 
-warn()
+warn ()
 {
   echo -e "\e[1;35m$@\e[m"
 }
 
-try()
+try ()
 {
   warn "$@"
   eval "$@"
@@ -21,18 +21,16 @@ while getopts "C:RT:W:a:f:o:p:r:vy:" opt "${aa[@]:1}"
     declare _$opt="$OPTARG"
   done
 
-# strip -p
 _p=${_p%/*}
 _p=${_p/www.}
-# strip -r
 _r=${_r/:1935\//\/}
 _r=${_r%/}
-# strip -y
 _y=${_y%.mp4}
 
+# If you use live flag on non-live, it takes forever to time out.
 try rtmpdump -o a.flv -i "$_r/$_y" ||
 try rtmpdump -o a.flv -i "\"$_r/$_y app=$_a\"" ||
-try rtmpdump -o a.flv -i "\"$_r/$_y live=1\"" ||
 try rtmpdump -o a.flv -i "\"$_r/$_y pageUrl=$_p\"" ||
 try rtmpdump -o a.flv -i "\"$_r playpath=$_y\"" ||
-try rtmpdump -o a.flv -i "\"$_r/$_y token=$_T\""
+try rtmpdump -o a.flv -i "\"$_r/$_y token=$_T\"" ||
+try rtmpdump -o a.flv -i "\"$_r/$_y live=1\""
