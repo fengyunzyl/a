4aef0eeddc9542bc5301c107e5d1c58aa3d0c90d
Steven Penny <svnpenn@gmail.com>
2012-11-29 11:20:37 -0600

    ?? sbin/git-describe.sh

---
+++ sbin/git-describe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Fix broken git describe
+# http://article.gmane.org/gmane.comp.version-control.git/210835
+
+read < <(git describe)
+
+tag=${REPLY%-*-*}
+
+sha=${REPLY##*-}
+
+read < <(git log --oneline $tag..HEAD | wc -l)
+
+echo $tag-$REPLY-$sha
