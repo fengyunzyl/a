48ae085f9af7f349f77bc71e88c73b902fc9a794
Steven Penny <svnpenn@gmail.com>
2012-11-29 23:53:09 -0600

    M sbin/git-describe-remote.sh

--- sbin/git-describe-remote.sh
+++ sbin/git-describe-remote.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-# Should be v1.8.0.1-260-g226dcb5
+# Should be v1.8.0.1-343-gf94c325
+
+warn ()
+{
+  echo -e "\e[1;35m$@\e[m"
+}
+
+usage ()
+{
+  warn "Usage:  ${0##*/} torvalds/linux"
+  exit
+}
+
+[ $1 ] || usage
+git ls-remote git://github.com/$1.git > j
 
 # Get last tag
-# ed9fe755130891fc878bb2433204faffb534697b        refs/tags/v1.8.0.1^{}
-git ls-remote -t git://github.com/git/git.git |
-  tail -1 |
-  tr / ^ |
-  cut -d^ -f3 |
-  tee tg
+read tag < <(tac j | tr / ^ | cut -d^ -f3)
+
+# Get HEAD SHA
+read sha < <(cut -c-7 j)
 
 # Get commits to HEAD
-wget -qO- https://api.github.com/repos/git/git/compare/$REPLY...HEAD |
-  grep total_commits
+wget -Oj https://api.github.com/repos/$1/compare/$tag...HEAD
+read commits < <(grep total_commits j | grep -o '[0-9]*')
+warn "$tag-$commits-g$sha"
+rm j
