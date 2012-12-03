c91c5db6a22aecfe12f309357e02cb1ab91f0e13
Steven Penny <svnpenn@gmail.com>
2012-12-03 16:22:17 -0600

    M install/gitk/gitk-dist.sh

--- install/gitk/gitk-dist.sh
+++ install/gitk/gitk-dist.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+# We must build the Windows native Tcl/Tk because Cygwin version requires X11.
+# To that end, we must use native paths.
 rm -rf /tmp
 
 # Install Tcl
 wget downloads.sf.net/tcl/tcl8.5.12-src.tar.gz
 tar xf tcl8.5.12-src.tar.gz
 cd tcl8.5.12/win
-./configure --host i686-w64-mingw32 --prefix /tmp/usr/local --disable-shared
+./configure --host i686-w64-mingw32 --disable-shared \
+  --prefix $HOMEDRIVE/usr/local
 make install
 cd -
 
@@ -13,12 +16,13 @@ cd -
 wget downloads.sf.net/tcl/tk8.5.12-src.tar.gz
 tar xf tk8.5.12-src.tar.gz
 cd tk8.5.12/win
-./configure --host i686-w64-mingw32 --prefix /tmp/usr/local --disable-shared
+./configure --host i686-w64-mingw32 --disable-shared \
+  --prefix $HOMEDRIVE/usr/local
 make install-binaries install-libraries
 cd -
 
 # Install gitk
-cd /tmp/usr/local/bin
+cd $HOMEDRIVE/usr/local/bin
 wget raw.github.com/git/git/master/gitk-git/gitk
 wget raw.github.com/svnpenn/dotfiles/master/bin/wish
 chmod +x gitk wish
