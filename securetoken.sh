bc8e5776651f46e1e13aec79787ccb592a508834
Steven Penny <svnpenn@gmail.com>
2012-12-08 09:02:32 -0600

    D rtmp-test.sh ?? scripts/rtmp-test.sh ?? securetoken.sh

---
+++ securetoken.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Get SecureToken to be used by RtmpDump
+
+pgrep ()
+{
+  ps -W | awk /$1/'{print$4;exit}'
+}
+
+pkill ()
+{
+  pgrep $1 | xargs kill -f
+}
+
+warn ()
+{
+  echo -e "\e[1;35m$@\e[m"
+}
+
+pc=plugin-container
+pkill $pc
+echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
+warn 'Killed flash player for clean dump.
+Restart video then press enter here.'
+read
+
+until read < <(pgrep $pc)
+do
+  warn "$pc not found!"
+  read
+done
+
+rm -f pg.core
+dumper pg $REPLY &
+
+until [ -s pg.core ]
+do
+  sleep 1
+done
+
+tr "[:cntrl:]" "\n" < pg.core |
+  grep -1m1 secureTokenResponse |
+  tac > tp
+
+read dt < tp
+rm pg.core tp
+[ $dt ] && echo $dt || echo 'SecureToken not found.'
