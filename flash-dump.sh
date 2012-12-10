94d6c7b0ccc630b7868f99529591c54682a8e210
Steven Penny <svnpenn@gmail.com>
2012-12-09 22:46:46 -0600

    ?? flash-dump.sh

---
+++ flash-dump.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pgrep ()
+{
+  ps -W | awk /$1/'{print$4;exit}'
+}
+
+pkill ()
+{
+  pgrep $1 | xargs kill -f
+}
+
+warn ()
+{
+  echo -e "\e[1;35m$@\e[m"
+}
+
+pc=plugin-container
+pkill $pc
+echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
+warn 'Killed flash player for clean dump.
+Restart video then press enter here.'
+read
+
+until read < <(pgrep $pc)
+do
+  warn "$pc not found!"
+  read
+done
+
+rm -f pg.core
+dumper pg $REPLY &
+
+until [ -s pg.core ]
+do
+  sleep 1
+done
+
+pkill $pc
