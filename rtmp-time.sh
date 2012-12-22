29063219d43eb21488265a0af4e9f882986f5cec
Steven Penny <svnpenn@gmail.com>
2012-12-22 09:59:16 -0600

    M rtmp-time.sh

--- rtmp-time.sh
+++ rtmp-time.sh
@@ -6,12 +6,20 @@ warn ()
   echo -e "\e[1;35m$@\e[m"
 }
 
+log ()
+{
+  warn "$@"
+  eval "$@"
+}
+
 read -d! f < <(rtmpsrv -i)
 
+q=1
 while read j < <(date)
 do
   warn $j
   eval $f -B .1
   [ $? = 1 ] && break
-  sleep 10
+  log sleep $q
+  (( q *= 2 ))
 done
