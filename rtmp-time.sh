b606c2ab43038e63d77a05c7369eea727b5739ed
Steven Penny <svnpenn@gmail.com>
2012-12-22 02:42:36 -0600

    M rtmp-time.sh

--- rtmp-time.sh
+++ rtmp-time.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # See how long command stays alive
 
-read -d! < <(rtmpsrv -i)
+warn ()
+{
+  echo -e "\e[1;35m$@\e[m"
+}
 
-while date
-do eval $REPLY -B .1
+read -d! f < <(rtmpsrv -i)
+
+while read j < <(date)
+do
+  warn $j
+  eval $f -B .1
   [ $? = 1 ] && break
   sleep 10
 done
