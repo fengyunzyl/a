4844bd52423ef81ab84e7dc67d7ccc00801507d5
Steven Penny <svnpenn@gmail.com>
2012-12-27 03:30:42 -0600

    D scripts/BBCtv_Live_Akamai.bat  D scripts/BBCtv_Live_Limelight.bat  M wrapper/open ?? scripts/thumbnail.sh ?? wrapper/ffmpeg

---
+++ scripts/thumbnail.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Set thumbnail for MP4 video
+
+warn ()
+{
+  printf "\e[1;35m%s \e[m" "$@"
+  printf "\n"
+}
+
+usage ()
+{
+  echo "Usage:  $0 FILE"
+  exit
+}
+
+quote ()
+{
+  [[ ${!1} =~ \\ ]] && read -r $1 <<< \"${!1}\"
+}
+
+log ()
+{
+  local gh
+  for gg
+  do
+    quote gg
+    gh+=("$gg")
+  done
+  warn "${gh[@]}"
+  eval "${gh[@]}"
+}
+
+[ $1 ] || usage
+rm -r /tmp
+
+j=0
+while log ffmpeg -ss $j -i "$1" -frames:v 1 -v warning /tmp/$j.png
+do
+  [ -a /tmp/$j.png ] || break
+  (( j += 30 ))
+done
+
+open /tmp
+log atomicparsley "$1" --artwork REMOVE_ALL --overWrite || exit
+warn 'Drag picture here, then press enter (backslash ok)'
+read -r
+log atomicparsley "$1" --artwork "$REPLY" --overWrite
