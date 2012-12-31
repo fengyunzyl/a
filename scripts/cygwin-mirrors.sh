bbf7d2e8816ae1c66f0bce8c1128bf5714370f62
Steven Penny <svnpenn@gmail.com>
2012-12-31 04:04:21 -0600

    mirr

--- scripts/cygwin-mirrors.sh
+++ scripts/cygwin-mirrors.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Print Cygwin mirrors
 
-IFS=/ read hh pp <<< "sources.redhat.com/cygwin/mirrors.lst"
+IFS=/ read hh pp <<< "sourceware.org/cygwin/mirrors.lst"
 exec 3<>/dev/tcp/$hh/80
 
 echo "GET /$pp
-Connection:close
+Connection: close
 Host: $hh" >&3
 
 grep "United States" <&3 |
