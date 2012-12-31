52b9c61c7e633476177b04d909f014d0870747d1
Steven Penny <svnpenn@gmail.com>
2012-12-31 03:46:45 -0600

    mirrors

--- scripts/cygwin-mirrors.sh
+++ scripts/cygwin-mirrors.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 # Print Cygwin mirrors
-r=sources.redhat.com/cygwin/mirrors.lst
 
-exec 3<>/dev/tcp/${r%%/*}/80
-echo -e "GET /${r#*/}\nConnection:close\nHost:${r%%/*}" >&3
+IFS=/ read hh pp <<< sources.redhat.com/cygwin/mirrors.lst
+exec 3<>/dev/tcp/$hh/80
 
-grep "United States" <&3 \
-  | grep "ftp" \
-  | cut -d\; -f1 \
-  | while read r; do echo "${#r} $r"; done \
-  | sort
+echo "GET /$pp
+Connection:close
+Host: $hh" >&3
+
+grep "United States" <&3 |
+  grep "ftp" |
+  cut -d\; -f1 |
+  while read r
+  do
+    echo "${#r} $r"
+  done |
+  sort
