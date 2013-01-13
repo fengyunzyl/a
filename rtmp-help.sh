ca09b93b4efca5c981be17cd8528cf0998de4f3a
Steven Penny <svnpenn@gmail.com>
2013-01-13 16:20:27 -0600

    M rtmp-help.sh

--- rtmp-help.sh
+++ rtmp-help.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# FIXME check for RtmpSrv RtmpDumpHelper on PATH
-# FIXME NetStream.Play.Reset..description..(Playing and resetting
-#   mosaiktv/20121210
 
 warn ()
 {
   printf "\e[1;35m%s\e[m\n" "$*"
+}
+
+clean ()
+{
+  rm -f a.core a.txt
 }
 
 pgrep ()
@@ -13,42 +15,46 @@ pgrep ()
   ps -W | awk /$1/'{print$4;exit}'
 }
 
+coredump ()
+{
+  pkill $2
+  warn Killed $2 for clean dump.
+  warn Script will automatically continue after $2 is restarted.
+  until read < <(pgrep $2)
+  do
+    sleep 1
+  done
+  sleep $1
+  clean
+  dumper a $REPLY &
+  until [ -s a.core ]
+  do
+    sleep 1
+  done
+  kill -13 %%
+}
+
+usage ()
+{
+  echo "usage: $0 DELAY"
+  exit
+}
+
 pkill ()
 {
   pgrep $1 | xargs kill -f
 }
 
-pc=plugin-container
-pkill $pc
-pkill rtmpdumphelper
+[ $1 ] || usage
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
-warn 'This script requires RtmpSrv v2.4-46 or higher.
-Killed flash player for clean dump.
-Restart video then press enter here.'
-read
-
-until read < <(pgrep $pc)
-do
-  warn "$pc not found!"
-  read
-done
-
-rm -f a.core
-dumper a $REPLY &
-
-until [ -s a.core ]
-do
-  sleep 1
-done
-
-kill %%
+coredump $1 plugin-container
 warn 'Press enter to start RtmpDumpHelper, then restart video.'
 read
 
 LANG= grep -Eaom1 '(RTMP|rtmp).{0,2}://[-.0-z]+' a.core |
-  cut -d: -f3 > tp
+  cut -d: -f3 > a.txt
 
-read < tp
+read < a.txt
 # Be careful adding ports here, can block RTMPT
 echo "[general]
 autorunproxyserver=0
@@ -59,8 +65,8 @@ read da < rtmpsuck.txt
 
 tr "[:cntrl:]" "\n" < a.core |
   grep -1m1 secureTokenResponse |
-  tac > tp
+  tac > a.txt
 
-read dt < tp
-rm a.core tp
+read dt < a.txt
+rm a.core a.txt
 eval rtmp-opt.sh $da ${dt:+-T $dt}
