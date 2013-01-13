d5a234f6662a1335aefbc723d42ab6b7835f5bfd
Steven Penny <svnpenn@gmail.com>
2013-01-13 16:14:52 -0600

    M flash-dump.sh

--- flash-dump.sh
+++ flash-dump.sh
@@ -10,9 +10,14 @@ pkill ()
   pgrep $1 | xargs kill -f
 }
 
+clean ()
+{
+  rm -f a.core
+}
+
 warn ()
 {
-  printf "\e[36m%s\e[m\n" "$*"
+  printf '\e[36m%s\e[m\n' "$*"
 }
 
 usage ()
@@ -21,25 +26,25 @@ usage ()
   exit
 }
 
+coredump ()
+{
+  pkill $2
+  warn Killed $2 for clean dump.
+  warn Script will automatically continue after $2 is restarted.
+  until read < <(pgrep $2)
+  do
+    sleep 1
+  done
+  sleep $1
+  clean
+  dumper a $REPLY &
+  until [ -s a.core ]
+  do
+    sleep 1
+  done
+  kill -13 %%
+}
+
 [ $1 ] || usage
-pc=plugin-container
-pkill $pc
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
-warn 'Killed flash player for clean dump.
-Script will automatically continue after video is restarted.'
-
-until read < <(pgrep $pc)
-do
-  sleep 1
-done
-
-sleep $1
-rm -f a.core
-dumper a $REPLY &
-
-until [ -s a.core ]
-do
-  sleep 1
-done
-
-kill %%
+coredump $1 plugin-container
