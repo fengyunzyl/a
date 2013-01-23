eb0da96abd1cbeed35fe5271588d726fec7c1198
Steven Penny <svnpenn@gmail.com>
2013-01-23 01:01:10 -0600

    hulu.sh

--- hulu.sh
+++ hulu.sh
@@ -28,7 +28,6 @@ log ()
     quote oo
     pp+=($oo)
   done
-  echo ${pp[*]} >> a.txt
   warn ${pp[*]}
   eval ${pp[*]}
 }
@@ -40,17 +39,19 @@ bsplit ()
 
 usage ()
 {
-  echo "usage: $0 DELAY CDN FILETYPE URL"
+  echo "usage: $0 DELAY [CDN FILETYPE] URL"
   echo
-  echo "To see available CDNs and filetypes run script with just DELAY."
+  echo "CDN       content delivery network"
+  echo "DELAY     wait time before coredump"
+  echo "FILETYPE  quality"
   exit
 }
 
 clean ()
 {
-  rm -f a.core a.flv a.txt
-  echo 'user_pref("browser.startup.page",3);' \
-    >$APPDATA/moonch~1/palemo~1/profiles/default/user.js
+  rm -f a.core
+  set $APPDATA/moonch~1/palemo~1/profiles/default/user.js
+  echo 'user_pref("browser.startup.page",3);' >$1
 }
 
 serialize_xml ()
@@ -72,7 +73,7 @@ coredump ()
   do
     sleep 1
   done
-  sleep $1
+  sleep ${!1}
   clean
   dumper a $REPLY &
   until [ -s a.core ]
@@ -82,33 +83,41 @@ coredump ()
   kill -13 %%
 }
 
-[ $4 ] || usage
+[ $2 ] || usage
+[ $4 ] || set $1 '' '' $2
+arg_delay=$1
+arg_cdn=$2
+arg_filetype=$3
+arg_url=$4
 echo ProtectedMode=0 2>/dev/null >$WINDIR/system32/macromed/flash/mms.cfg
-echo 'user_pref("browser.startup.page",1);' \
-  >$APPDATA/moonch~1/palemo~1/profiles/default/user.js
+set $APPDATA/moonch~1/palemo~1/profiles/default/user.js
+echo 'user_pref("browser.startup.page",1);' >$1
 pkill palemoon
-open $4
-coredump $1 plugin-container
+open $arg_url
+coredump arg_delay plugin-container
 
 while read video
 do
   serialize_xml video
-  if ! [ $1 ]
+  if ! [ $arg_cdn ]
   then
     printf '%-9s  %9s\n' $cdn $filetype
-  elif [ $cdn$filetype = $2$3 ]
+  elif [ $cdn$filetype = $arg_cdn$arg_filetype ]
   then
     break
   fi
 done < <(grep -ao '<video [^>]*>' a.core | sort | uniq -w123)
 
-if ! [ $1 ]
+if ! [ $arg_cdn ]
 then
   clean
   exit
 fi
 
 read app < <(cut -d/ -f4- <<< ${server}?${token//amp;})
+clean
+
+# parse JSON to get file name
 
 log rtmpdump \
   -W http://download.hulu.com/huludesktop.swf \
@@ -116,6 +125,3 @@ log rtmpdump \
   -a $app \
   -r $server \
   -y $stream
-
-log ffmpeg -i a.flv -c copy -v warning a.mp4
-clean
