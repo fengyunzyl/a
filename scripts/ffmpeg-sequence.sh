6e8d8ced68a09d199e6af0cf7801ca3af8b06115
Steven Penny <svnpenn@gmail.com>
2013-01-22 23:15:29 -0600

    sequence

---
+++ scripts/ffmpeg-sequence.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+usage ()
+{
+  echo make an image sequence from a video
+  echo
+  echo usage: $0 START DURATION FILE
+  exit
+}
+
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*"
+}
+
+quote ()
+{
+  [[ ${!1} =~ \  ]] && read $1 <<< \"${!1}\"
+}
+
+log ()
+{
+  local pp
+  for oo
+  do
+    quote oo
+    pp+=($oo)
+  done
+  warn ${pp[*]}
+  eval ${pp[*]}
+}
+
+[ $2 ] || usage
+log ffmpeg -ss $1 -i $3 -t $2 %d.png
