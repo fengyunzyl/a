62ab3f2c9d4e30355e881450fa89f9a99395b399
Steven Penny <svnpenn@gmail.com>
2013-01-25 23:09:31 -0600

    swf

---
+++ swf.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# decompile and deobfuscate SWF
+# http://github.com/whitequark/furnace-avm2
+# http://canaldosconcursos.com.br/playercpf/player.swf
+# http://www.zonytvcom.info/you/src.swf
+# http://supremecater.com/player/src.swf
+
+usage()
+{
+  echo usage: $0 FILE
+  exit
+}
+
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*" >&2
+}
+
+quote ()
+{
+  [[ ${!1/*[ #&;\\]*} ]] || read -r $1 <<< \"${!1}\"
+}
+
+log ()
+{
+  for oo
+  do
+    quote oo
+    set -- "$@" $oo
+    shift
+  done
+  warn $*
+  eval $*
+}
+
+clean ()
+{
+  rm a.abc b.abc
+}
+
+[ $1 ] || usage
+log furnace-swf -i $1 abclist
+# assume name is '' for now
+log furnace-swf -i $1 abcextract -n '' -o a.abc
+log furnace-avm2 -i a.abc -d -o b.abc
+log furnace-avm2-decompiler -i b.abc -d -D funids > a.as
+clean
