a5e2b0b5a23c4eb627fd9e9da08ed662c9f6c9f5
Steven Penny <svnpenn@gmail.com>
2013-01-28 12:29:47 -0600

    baby-php.sh

---
+++ baby-php.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+bin=$PWD/php-5.4.11-nts-Win32-VC9-x86
+mkdir dist
+cd dist
+
+# /
+cat > cmd.bat <<q
+path %path%;%~dp0/bin
+cd root
+start cmd.exe
+q
+read DATE < <(date)
+PATH=$bin:$PATH
+read PHP_VERSION < <(php -v | grep -o '[.0-9]*')
+cat > README.txt <<q
+Baby PHP by
+  Steven Penny
+
+Steven’s Home Page
+  http://svnpenn.github.com
+
+Today’s date
+  $DATE
+
+The build script for this build can be found at
+  http://github.com/svnpenn/a/blob/master/baby-php.sh
+
+Included with this package
+  PHP $PHP_VERSION
+
+OPERATING INSTRUCTIONS
+  Put any scripts into "root" folder
+  Double click cmd.bat
+  Use command like "php foo.php"
+q
+u2d README.txt
+
+# bin
+deps=(
+  $bin/ext/php_curl.dll
+  $bin/php.exe
+)
+mkdir bin
+cd bin
+cp ${deps[*]} .
+ldd ${deps[*]} |
+  grep " $bin" |
+  cut -d\  -f3 |
+  sort -u |
+  xargs cp -t .
+echo extension=./php_curl.dll > php.ini
+cd -
+
+# root
+mkdir root
+
+# archive
+cd ${0%/*}
+read BABY_VERSION < <(git log --oneline $0 | wc -l)
+cd -
+set cmd.bat README.txt bin root
+tar acf baby-php-$BABY_VERSION.tar.gz $*
+rm -r $*
