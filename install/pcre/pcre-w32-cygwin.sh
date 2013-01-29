53b1e4e70da63d8f64e9188b7c1d8c3cf81bab6d
Steven Penny <svnpenn@gmail.com>
2013-01-29 15:09:30 -0600

    pcre-w32-cygwin.sh

--- install/pcre/pcre-w32-cygwin.sh
+++ install/pcre/pcre-w32-cygwin.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-# Build PCRE
+set i686-w64-mingw32
+set $1 /usr/$1/sys-root/mingw
 
+# install PCRE
 wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.31.tar.bz2
-tar xf pcre*
-cd pcre*
-: i686-w64-mingw32
-./configure --disable-cpp --disable-shared --host=$_ \
-  --prefix=/usr/$_/sys-root/mingw
+tar xf pcre-8.31.tar.bz2
+cd pcre-8.31
+./configure --disable-cpp --disable-shared --host $1 --prefix $2
 make install
+cd -
+
+# install regex.h
+# http://pcre.org/readme.txt
+# http://kemovitra.blogspot.com/2009/07/mingw-porting-gnu-regex-to-windows.html
+cd $2/include
+cp pcreposix.h regex.h
+cd -
