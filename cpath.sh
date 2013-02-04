320dbc94c483d025428fa09cab663503a8189e74
Steven Penny <svnpenn@gmail.com>
2013-02-04 08:51:50 -0600

    cpath

--- cpath.sh
+++ cpath.sh
@@ -7,21 +7,28 @@
 
 usage ()
 {
-  echo usage: $0 PATH
+  echo usage: $0 [WINDOWS] PATH
   exit
 }
 
 [ $1 ] || usage
+[ $2 ] || set '' "$1"
 
-if [ -a "$1" ]
+if [ -a "$2" ]
 then
-  mv "$1" /tmp
+  mv "$2" /tmp
 fi
 
-mkdir -p "$1"
-cd "$1"
-# unix path
-pwd
+mkdir -p "$2"
+cd "$2"
+
+if [ $1 ]
+then
+  $WINDIR/system32/cmd /c cd
+else
+  pwd
+fi
+
 cd ~-
-rmdir "$1"
-mv "/tmp/$1" .
+rmdir "$2"
+mv "/tmp/$2" .
