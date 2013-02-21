dbf59f0727c45d3d7e53de08dffdd0c69f3a92e4
Steven Penny <svnpenn@gmail.com>
2013-02-21 01:26:05 -0600

    flash-dump

--- scripts/flash-dump.sh
+++ scripts/flash-dump.sh
@@ -28,8 +28,11 @@ coredump ()
 arg_pwd=$PWD
 cd $WINDIR
 echo ProtectedMode=0 > system32/macromed/flash/mms.cfg
+# copy cookies
 rm -r /tmp
 mkdir /tmp
+cd $APPDATA
+find -name cookies.sqlite -exec cp -t /tmp {} +
 cd /tmp
 MOZ_DISABLE_OOP_PLUGINS=1 firefox -no-remote -profile . &
 cd $arg_pwd
