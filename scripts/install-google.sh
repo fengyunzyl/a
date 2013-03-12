023da0caa0550e521869ead82b9dfb548bd2fdd3
Steven Penny <svnpenn@gmail.com>
2013-03-12 08:36:18 -0500

    google

--- scripts/install-google.sh
+++ scripts/install-google.sh
@@ -2,8 +2,8 @@
 # http://googlecl.googlecode.com
 
 cd
-wget googlecl.googlecode.com/files/googlecl-win32-0.9.14.zip
+rm -r googlecl
+wget -nc googlecl.googlecode.com/files/googlecl-win32-0.9.14.zip
 unzip googlecl-win32-0.9.14.zip
 cd googlecl
-chmod +x google.exe python26.dll
-google --version
+chmod +x google.exe python26.dll pyexpat.pyd _socket.pyd _ssl.pyd
