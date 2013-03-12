48ef235ef3edd02a77ba7b9fc719aa5d854a5c3a
Steven Penny <svnpenn@gmail.com>
2013-03-12 06:03:17 -0500

    screen-buffer

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -11,8 +11,23 @@ warn ()
   printf '\e[36m%s\e[m\n' "$*"
 }
 
+quote ()
+{
+  yy='[ #&;\]'
+  if [[ ${!1} =~ $yy ]]
+  then
+    read -r $1 <<< \"${!1}\"
+  fi
+}
+
 log ()
 {
+  for oo
+  do
+    quote oo
+    set -- "$@" $oo
+    shift
+  done
   warn $*
   eval $*
 }
@@ -22,5 +37,7 @@ log ()
 # convert to hex
 printf -v rows %04x $1
 printf -v columns %04x $2
-log regtool set /user/Console/ScreenBufferSize 0x$rows$columns
+set -- -f -t reg_dword
+log reg add 'hkcu\console' -v WindowSize -d 0x0019$columns "$@"
+log reg add 'hkcu\console' -v ScreenBufferSize -d 0x$rows$columns "$@"
 echo Restart console to see changes.
