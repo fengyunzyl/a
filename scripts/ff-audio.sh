16297cc6a21fdb22ede8afb666edc03fb1196a7e
Steven Penny <svnpenn@gmail.com>
2013-03-22 12:45:21 -0500

    M scripts/ff-audio.sh

--- scripts/ff-audio.sh
+++ scripts/ff-audio.sh
@@ -2,7 +2,7 @@
 
 warn ()
 {
-  printf '\e[36m%s\e[m\n' "$*"
+  printf '\e[36m%s\e[m\n' "$*" >&2
 }
 
 log ()
@@ -16,16 +16,48 @@ log ()
 
 usage ()
 {
-  echo "script will search the current directory for FLAC files and JPG file."
-  echo "It will use these files to create high quality videos."
+  echo "Script will search the current directory for JPG file and either FLAC"
+  echo "or MP3 files. It will use these files to create high quality videos."
   echo "Finally it will upload videos to YouTube."
   echo "Once you are sure environment is correct run"
   echo "$0 -f"
   exit
 }
 
+json ()
+{
+  read $1 < <(jq -r .$2 jq.json)
+}
+
+metadata ()
+{
+  log fpcalc "$1" | sed 's/ //g' > fp.sh
+  . fp.sh
+  set "client=8XaBELgH&duration=${DURATION}&fingerprint=${FINGERPRINT}"
+  curl -s "api.acoustid.org/v2/lookup?${1}&meta=recordings+releaseids" |
+    jq .results[0].recordings[0] > jq.json
+  json TITLE title
+  json ID releases[0].id
+  set 'fmt=json&inc=artist-credits+labels'
+  log curl -s "musicbrainz.org/ws/2/release/${ID}?${1}" > jq.json
+  json ALBUM title
+  json ARTIST '["artist-credit"][0].name'
+  json LABEL '["label-info"][0].label.name'
+  json DATE date
+  rm -f fp.sh jq.json sr.txt
+  echo "Title: $TITLE" >> sr.txt
+  echo "Album: $ALBUM" >> sr.txt
+  echo "Artist: $ARTIST" >> sr.txt
+  echo "Label: $LABEL" >> sr.txt
+  echo "Date: $DATE" >> sr.txt
+  cat sr.txt
+  warn 'enter "y" if metadata is ok'
+  read bb
+  [ $bb ] || exit
+}
+
 [ $1 ] || usage
-mapfile -t songs < <(find -name '*.flac')
+mapfile -t songs < <(find -name '*.flac' -o -name '*.mp3')
 read img < <(find -name '*.jpg')
 
 if ! [ $img ]
@@ -41,18 +73,8 @@ do
   # make sure input picture is at least 720
   log ffmpeg -loop 1 -r 1 -i "$img" -i "$song" -shortest -qp 0 \
     -c:a aac -strict -2 -b:a 495263 -v warning -stats "$video"
-  # upload
-  ffprobe -show_format -print_format flat=s=% -v error "$song" |
-    sed 's/\r//; s/.*%//' > metadata.sh
-  . metadata.sh
-  set summary.txt
-  > $1
-  echo "Title: $TITLE" >> $1
-  echo "Album: $ALBUM" >> $1
-  echo "Artist: $ARTIST" >> $1
-  echo "Label: $LABEL" >> $1
-  echo "Date: $DATE" >> $1
+  metadata "$song"
   # category is case sensitive
-  log google youtube post -c Music -n "$ARTIST, $TITLE" -s $1 \
+  log google youtube post -c Music -n "$ARTIST, $TITLE" -s sr.txt \
     -t "$ALBUM, $ARTIST" -u svnpenn "$video"
 done
