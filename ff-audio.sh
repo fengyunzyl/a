7899570329a64bbf8f484c8867848e72b539c1ad
Steven Penny <svnpenn@gmail.com>
2013-03-24 03:22:51 -0500

    M ff-audio.sh

--- ff-audio.sh
+++ ff-audio.sh
@@ -26,57 +26,68 @@ usage ()
 
 json ()
 {
-  read $1 < <(jq -r .$2 jq.json)
+  read $1 < <(jq -r $2 jq.json)
 }
 
 metadata ()
 {
-  log fpcalc "$1" | sed 's/ //g' > fp.sh
+  ss=$1
+  tt=${ss%.*}.txt
+  log fpcalc "$ss" | sed '1d' > fp.sh
   . fp.sh
+  rm fp.sh
   set "client=8XaBELgH&duration=${DURATION}&fingerprint=${FINGERPRINT}"
-  curl -s "api.acoustid.org/v2/lookup?${1}&meta=recordings+releaseids" |
-    jq .results[0].recordings[0] > jq.json
-  json TITLE title
-  json ID releases[0].id
-  set 'fmt=json&inc=artist-credits+labels'
-  log curl -s "musicbrainz.org/ws/2/release/${ID}?${1}" > jq.json
-  json ALBUM title
-  json ARTIST '["artist-credit"][0].name'
-  json LABEL '["label-info"][0].label.name'
-  json DATE date
-  rm -f fp.sh jq.json sr.txt
-  echo "Title: $TITLE" >> sr.txt
-  echo "Album: $ALBUM" >> sr.txt
-  echo "Artist: $ARTIST" >> sr.txt
-  echo "Label: $LABEL" >> sr.txt
-  echo "Date: $DATE" >> sr.txt
-  cat sr.txt
+  wget -qO jq.json "api.acoustid.org/v2/lookup?meta=recordings+releaseids&${1}"
+  json title '.results[0].recordings[0].title'
+  json id '.results[0].recordings[0].releases[0].id'
+  if ! [[ $album ]]
+  then
+    set 'fmt=json&inc=artist-credits+labels'
+    log wget -qO jq.json "musicbrainz.org/ws/2/release/${id}?${1}"
+    json album '.title'
+    json artist '.["artist-credit"][0].name'
+    json label '.["label-info"][0].label.name'
+    json date '.date'
+  fi
+  rm -f jq.json "$tt"
+  echo "Title: $title" >> "$tt"
+  echo "Album: $album" >> "$tt"
+  echo "Artist: $artist" >> "$tt"
+  echo "Label: $label" >> "$tt"
+  echo "Date: $date" >> "$tt"
+  cat "$tt"
   warn 'enter "y" if metadata is ok'
-  read bb
-  [ $bb ] || exit
+  read uu
+  [ $uu ] || exit
+  titles[$ss]=$title
 }
 
 [ $1 ] || usage
-mapfile -t songs < <(find -name '*.flac' -o -name '*.mp3')
-read img < <(find -name '*.jpg')
-
-if ! [ $img ]
+if ! read img < <(find -name '*.jpg')
 then
   echo no jpg found
   exit
 fi
 
+mapfile -t songs < <(find -name '*.flac' -o -name '*.mp3')
+declare -A titles
+
 for song in "${songs[@]}"
 do
-  # remove bad tags
-  log mp3gain -s d "$song"
   metadata "$song"
+done
+
+for song in "${songs[@]}"
+do
+  mp3gain -s d "$song"
   video=${song%.*}.mp4
+  meta=${song%.*}.txt
   # adding "-preset" would only make small difference in size or speed
   # make sure input picture is at least 720
   log ffmpeg -loop 1 -r 1 -i "$img" -i "$song" -shortest -qp 0 \
     -c:a aac -strict -2 -b:a 495263 -v warning -stats "$video"
   # category is case sensitive
-  log google youtube post -c Music -n "$ARTIST, $TITLE" -s sr.txt \
-    -t "$ALBUM, $ARTIST" -u svnpenn "$video"
+  log google youtube post -c Music -n "${artist}, ${titles[$song]}" \
+    -s "${meta}" -t "${album}, ${artist}" \
+    -u svnpenn "${video}"
 done
