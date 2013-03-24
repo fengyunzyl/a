89c1ad5b84be6f3b394e0110cbc314b7d1306728
Steven Penny <svnpenn@gmail.com>
2013-03-24 04:30:33 -0500

    M scripts/screen-buffer.sh

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -21,11 +21,11 @@ log ()
 }
 
 [ $1 ] || usage
-
+REG=$WINDIR/system32/reg
 # convert to hex
 printf -v rows %04x $1
 printf -v columns %04x $2
 set -- -f -t reg_dword
-log reg add 'hkcu\console' -v WindowSize -d 0x0019$columns "$@"
-log reg add 'hkcu\console' -v ScreenBufferSize -d 0x$rows$columns "$@"
+log $REG add 'hkcu\console' -v WindowSize -d 0x0019$columns "$@"
+log $REG add 'hkcu\console' -v ScreenBufferSize -d 0x$rows$columns "$@"
 echo Restart console to see changes.
