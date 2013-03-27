46ec987dabf94b7746adafc8786b30068fabeab7
Steven Penny <svnpenn@gmail.com>
2013-03-27 13:24:55 -0500

    M scripts/screen-buffer.sh

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+# get/set size of the terminal
+REG=$WINDIR/system32/reg
 
 usage ()
 {
   echo usage: $0 ROWS COLUMNS
+  set '3!d; s/....$/ 0x&/'
+  set $(($REG query 'hkcu\console' -v ScreenBufferSize | sed "$1") )
+  echo current buffer rows $(($3))
+  echo current buffer columns $(($4))
   exit
 }
 
@@ -14,14 +19,13 @@ warn ()
 log ()
 {
   unset PS4
-  coproc yy (set -x; : "$@") 2>&1
-  read zz <&$yy
-  warn ${zz:2}
-  "$@"
+  set $((set -x; : "$@") 2>&1)
+  shift
+  warn $*
+  eval $*
 }
 
 [ $1 ] || usage
-REG=$WINDIR/system32/reg
 # convert to hex
 printf -v rows %04x $1
 printf -v columns %04x $2
