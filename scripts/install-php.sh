05be7c1093331ea41f143c1d6875855f6e2a163e
Steven Penny <svnpenn@gmail.com>
2013-03-29 15:39:18 -0500

    M scripts/install-php.sh

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/php
 cd ~/php
-wget windows.php.net/downloads/releases/php-5.4.12-nts-Win32-VC9-x86.zip
-unzip php-5.4.12-nts-Win32-VC9-x86.zip
+wget windows.php.net/downloads/releases/php-5.4.13-nts-Win32-VC9-x86.zip
+unzip php-5.4.13-nts-Win32-VC9-x86.zip
 chmod +x php.exe php5.dll libeay32.dll ssleay32.dll ext/php_curl.dll
 echo extension=ext/php_curl.dll > php.ini
