bbb11ff82c0d4a9174deeabbd4432801c5f4fbe9
Steven Penny <svnpenn@gmail.com>
2013-03-29 01:38:49 -0500

    M scripts/baby-wdk.sh

--- scripts/baby-wdk.sh
+++ scripts/baby-wdk.sh
@@ -8,6 +8,20 @@ else
   P7ZIP="${ProgramW6432}/7-zip/7z"
 fi
 
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*"
+}
+
+log ()
+{
+  unset PS4
+  set $((set -x; : "$@") 2>&1)
+  shift
+  warn $*
+  eval $*
+}
+
 deps=(
   wdk/buildtools_x86fre.msi
   wdk/buildtools_x86fre_cab001.cab
@@ -17,10 +31,10 @@ deps=(
   wdk/libs_x64fre_cab001.cab
 )
 
-"$P7ZIP" x GRMWDK_EN_7600_1.ISO ${deps[*]}
+log "$P7ZIP" x $HOMEDRIVE/steven/public/wdk/GRMWDK_EN_7600_1.ISO ${deps[*]}
 mv wdk baby-wdk
 cd ${0%/*}
 set $(git log --follow --oneline $0 | wc -l)
 cd -
-tar acf baby-wdk-${1}.tar.gz baby-wdk
+log tar acf baby-wdk-${1}.tar.gz baby-wdk
 rm -r baby-wdk
