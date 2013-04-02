6358eba56c8141d5bb9d503e95839956046ce379
Steven Penny <svnpenn@gmail.com>
2013-04-02 12:43:34 -0500

    jq

--- scripts/install-jq.sh
+++ scripts/install-jq.sh
@@ -1,6 +1,12 @@
-
+# cygwin jq
 setup -nqP bison,flex,gcc4-core,git,make,mpfr,python
-
-git clone git://github.com/stedolan/jq.git
+git clone --depth 1 git://github.com/stedolan/jq.git
 cd jq
 make
+
+# archive
+cd ${0%/*}
+BABY_VERSION=$(git log --follow --oneline $0 | wc -l)
+cd -
+P7ZIP="${ProgramW6432}/7-zip/7z"
+"$P7ZIP" a -mx=9 jq-cygwin-${BABY_VERSION}.zip jq.exe
