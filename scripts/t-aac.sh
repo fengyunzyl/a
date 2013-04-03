8b24f3d6de49b54b53bacaf5754cd8f3d29890f5
Steven Penny <svnpenn@gmail.com>
2013-04-02 21:25:16 -0500

    t-aac

--- scripts/t-aac.sh
+++ scripts/t-aac.sh
@@ -1,5 +1,5 @@
 # test ffmpeg aac encoder
-# 498795
+# 496814
 
 usage ()
 {
@@ -7,25 +7,35 @@ usage ()
   exit
 }
 
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*"
+}
+
+log ()
+{
+  unset PS4
+  set $((set -x; : "$@") 2>&1)
+  shift
+  warn $*
+  eval $*
+}
+
+ff ()
+{
+  log timeout 10 ffmpeg -i $HOMEDRIVE/steven/music/autechre/$1 -c:a aac \
+    -strict -2 -v warning -stats -cutoff 17000 -map a -t 10 -b:a $arg_rate \
+    -ss $2 -y $3
+  [ $? = 0 ] || exit
+}
+
 [ $1 ] || usage
 arg_rate=$1
-arg_dir=$HOMEDRIVE/steven/music/autechre
 
-timeout 10 ffmpeg \
-  -i $arg_dir/exai/Exai-001-Autechre-Fleure.flac \
-  -c:a aac -strict -2 -v warning -stats -cutoff 17000 -map a -t 10 \
-  -b:a $arg_rate -y fleure.m4a
+ff oversteps/Oversteps-002-Autechre-ilanders.flac 00:03:45 ilanders.m4a
 
-[ $? = 0 ] || exit
+ff exai/Exai-001-Autechre-Fleure.flac 0 fleure.m4a
 
-timeout 10 ffmpeg \
-  -i $arg_dir/incunabula-flac/Incunabula-009-Autechre-Windwind.flac \
-  -c:a aac -strict -2 -v warning -stats -cutoff 17000 -map a -t 10 \
-  -ss 00:03:00 -b:a $arg_rate -y wind.m4a
+ff incunabula-flac/Incunabula-009-Autechre-Windwind.flac 00:03:00 wind.m4a
 
-[ $? = 0 ] || exit
-
-timeout 10 ffmpeg \
-  -i $arg_dir/oversteps/Oversteps-014-Autechre-Yuop.flac \
-  -c:a aac -strict -2 -v warning -stats -cutoff 17000 -map a -t 10 \
-  -ss 00:03:10 -b:a $arg_rate -y yuop.m4a
+ff oversteps/Oversteps-014-Autechre-Yuop.flac 00:03:10 yuop.m4a
