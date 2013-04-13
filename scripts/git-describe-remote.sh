40c93bcd070ba288f6b6e9aff32b0172a138da1f
Steven Penny <svnpenn@gmail.com>
2013-04-13 11:48:12 -0500

    git-describe-remote

--- scripts/git-describe-remote.sh
+++ scripts/git-describe-remote.sh
@@ -21,16 +21,18 @@ log ()
 }
 
 [ $1 ] || usage
-git ls-remote git://github.com/$1.git > k
+arg_rpo=$1
+git ls-remote git://github.com/$arg_rpo.git > k
 
 # Get last tag
-tag=$(sed '$!d; s,.*/,,; s,\^.*,,' k)
+set $(sed '$!d; y./^.  .' k)
+tag=$4
 
 # Get HEAD SHA
 sha=$(sed '1!d; s/.//8g' k)
 
 # Get commits to HEAD
-log wget -qOk https://api.github.com/repos/$1/compare/$tag...HEAD
+log wget -qOk https://api.github.com/repos/$arg_rpo/compare/$tag...HEAD
 commits=$(sed '/total_commits/!d; s/[^0-9]//g' k)
 echo "$tag-$commits-g$sha"
 rm k
