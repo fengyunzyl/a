f98f780861e5117560ad64e4608b41581b191a7e
Steven Penny <svnpenn@gmail.com>
2013-04-17 23:58:48 -0500

    opt-http

--- scripts/opt-http.sh
+++ scripts/opt-http.sh
@@ -29,52 +29,40 @@ log ()
   eval $*
 }
 
-[ $2 ] || usage
-arg_search=$1
-shift
+[[ $1 ]] || usage
+[[ $1 = curl ]] && usage
 
 for hh
 do
-  if ! [ "$hh" = "-O" ]
+  bb[aa++]=$hh
+done
+
+arg_search=$bb
+unset bb[0]
+unset bb[1]
+
+for hh in ${!bb[*]}
+do
+  if [[ ${bb[hh]} = -o ]]
   then
-    bb[aa++]=$hh
+    arg_url=${bb[hh-1]}
+    unset bb[hh-1]
+    unset bb[hh]
+    unset bb[hh+1]
   fi
 done
 
-
-for ((hh = 1; hh < aa; hh++))
+for ((hh = 0; hh < aa; hh++))
 do
   one=${bb[hh]}
   unset bb[hh]
   two=${bb[hh+1]}
   [[ $two =~ ^- ]] && unset two || unset bb[hh+1]
-  if ! log "${bb[@]}" -s | grep --color $arg_search
+  if log curl "${bb[@]}" "$arg_url" | grep --color -m1 "$arg_search"
   then
+    # restore if download failed
     bb[hh]=$one
     bb[hh+1]=$two
   fi
   [[ $two ]] && (( hh++ ))
 done
-
-for hh in ${!bb[@]}
-do
-  # break up cookies
-  IFS=':' read name vs <<< "${bb[hh]}"
-  bsplit va vs
-  for ff in ${!va[@]}
-  do
-    one=${va[ff]}
-    unset va[ff]
-    bjoin vs va
-    bb[hh]=${name}${vs:+:$vs}
-    if ! log "${bb[@]}" -s | grep --color $arg_search
-    then
-      echo FAIL
-      va[ff]=$one
-    fi
-  done
-  bjoin vs va
-  bb[hh]=${name}${vs:+:$vs}
-done
-
-cut -b6- <<< $((set -x; : "${bb[@]}") 2>&1)
