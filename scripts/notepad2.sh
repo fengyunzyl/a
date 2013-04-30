29bc1c804fa8fd07c0a5ea837d33983c679fa3cb
Steven Penny <svnpenn@gmail.com>
2013-04-30 13:28:12 -0500

    notepad2

--- scripts/notepad2.sh
+++ scripts/notepad2.sh
@@ -1,7 +1,1 @@
-mapfile u </etc/mtab
-for (( v=${#u[*]}-1; v>=0; v-- ))
-do
-  set "$1" ${u[v]}
-  set "${1/#$3/$2/}"
-done
-"$ProgramW6432/notepad2/notepad2" "$1"
+"$ProgramW6432/notepad2/notepad2" "${1/${TMP%/Users*}/$HOMEDRIVE}"
