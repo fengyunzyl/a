41271dd44ec0feb34bc3a8de9cb7f56ffdff250d
Steven Penny <svnpenn@gmail.com>
2013-05-01 22:46:26 -0500

    cygwin-mirrors

--- scripts/cygwin-mirrors.sh
+++ scripts/cygwin-mirrors.sh
@@ -1,18 +1,5 @@
 # Print Cygwin mirrors
 
-IFS=/ read ee ff <<< sourceware.org/cygwin/mirrors.lst
-exec 3< /dev/tcp/$ee/80
-{
-  echo GET /$ff
-  echo connection: close
-  echo host: $ee
-} >&3
-
-grep 'United States' <&3 |
-  grep http |
-  cut -d';' -f1 |
-  while read gg
-  do
-    echo "${#gg} $gg"
-  done |
+curl -s sourceware.org/cygwin/mirrors.lst |
+  awk '/http/ && /United States/ {print length($1), $1}' FS=';' |
   sort
