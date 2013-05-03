60d9516bdc07df0205a672c7a97b3d8afe969f2c
Steven Penny <svnpenn@gmail.com>
2013-05-03 06:45:17 -0500

    M scripts/install-ffmpeg.sh

--- scripts/install-ffmpeg.sh
+++ scripts/install-ffmpeg.sh
@@ -1,7 +1,32 @@
-# zeranoe ffmpeg
+# Cygwin FFmpeg with libfdk_aac
+setup -nqP libtool
+setup -nqP mingw64-i686-gcc-g++
+setup -nqP yasm
 
-wget ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.7z
-P7ZIP="${ProgramW6432}/7-zip/7z"
-"$P7ZIP" x ffmpeg-latest-win64-static.7z
-cd ffmpeg-*-win64-static
-cp -r bin /usr/local
+# libfdk_aac
+git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
+cd fdk-aac
+autoreconf -fiv
+./configure \
+  --host i686-w64-mingw32 \
+  --prefix /usr/i686-w64-mingw32/sys-root/mingw
+make -j5 install
+cd -
+
+# install ffmpeg
+# --enable-libx264
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git
+cd ffmpeg
+./configure \
+  --enable-libfdk-aac \
+  --arch=x86 \
+  --target-os=mingw32 \
+  --cross-prefix=i686-w64-mingw32- \
+  --disable-indevs \
+  --disable-doc \
+  --extra-ldflags=-static
+make -j5
+
+# test
+set $HOMEDRIVE/$USERNAME/music/autechre/amber-flac/Amber-007-Autechre-Nine.flac
+./ffmpeg -i $1 -c:a libfdk_aac -map a outfile.m4a
