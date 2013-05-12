a456a0ed5eb5b558ffa0e842e174c4841585278f
Steven Penny <svnpenn@gmail.com>
2013-05-12 09:21:43 -0500

    screen-buffer

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -1,11 +1,10 @@
 # get/set size of the terminal
-REG=$WINDIR/system32/reg
 
 usage ()
 {
   echo usage: $0 ROWS COLUMNS
   set '3!d; s/....$/ 0x&/'
-  set $($REG query 'hkcu\console' -v ScreenBufferSize | sed "$1")
+  set $(reg query 'hkcu\console' -v ScreenBufferSize | sed "$1")
   echo current buffer rows $(($3))
   echo current buffer columns $(($4))
   exit
@@ -30,6 +29,6 @@ log ()
 printf -v rows %04x $1
 printf -v columns %04x $2
 set -- -f -t reg_dword
-log $REG add 'hkcu\console' -v WindowSize -d 0x0019$columns "$@"
-log $REG add 'hkcu\console' -v ScreenBufferSize -d 0x$rows$columns "$@"
+log reg add 'hkcu\console' -v WindowSize -d 0x0019$columns "$@"
+log reg add 'hkcu\console' -v ScreenBufferSize -d 0x$rows$columns "$@"
 echo Restart console to see changes.
