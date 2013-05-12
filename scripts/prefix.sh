8f768b75279f35bf5b8f5584df4c81aa7d8d8a9f
Steven Penny <svnpenn@gmail.com>
2013-05-12 09:22:22 -0500

    prefix

--- scripts/prefix.sh
+++ scripts/prefix.sh
@@ -6,14 +6,32 @@ usage ()
   exit
 }
 
-[ $1 ] || usage
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*"
+}
 
+[ $1 ] || usage
 touch foo.c
 
+warn INCLUDE
 $1 -v foo.c |& grep '^ [^ ]*$' | while read -r b
 do
-  cd $b
-  pwd
+  if [ -a $b ] 2>&-
+  then
+    cd $b
+    pwd
+  fi
+done | sed '/\./d; s./[^/]*$..; s/^/--prefix /'
+
+warn LIB
+$1 -\#\#\# foo.c |& sed '/LIBRARY_PATH=/!d; s///; y/;/\n/' | while read -r b
+do
+  if [ -a $b ] 2>&-
+  then
+    cd $b
+    pwd
+  fi
 done | sed '/\./d; s./[^/]*$..; s/^/--prefix /'
 
 rm -f foo.c foo.o
