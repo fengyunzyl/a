6b62640a9afee938083298d617776806a4d83819
Steven Penny <svnpenn@gmail.com>
2013-05-13 20:51:51 -0500

    hulu.sh

--- hulu.sh
+++ hulu.sh
@@ -97,7 +97,7 @@ MOZ_DISABLE_OOP_PLUGINS=1 "$FIREFOX" -no-remote -profile . $arg_url &
 PID=$!
 debug firefox started
 echo waiting for firefox to load...
-: ${S1=12}
+: ${S1=13}
 sleep $S1
 echo dumping firefox...
 read WINPID </proc/$PID/winpid
