22e11e20ca876e3e65edd07322d911e7549b2e54
Steven Penny <svnpenn@gmail.com>
2013-05-23 01:55:56 -0500

    t-ffmpeg

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -56,8 +56,3 @@ cd ffmpeg
   --cross-prefix=$HOST- \
   --host-cc=$HOST-gcc
 make -j5 install
-
-# test
-set 'Breaking Bad - S03E12 - Half Measures.mp4'
-./ffmpeg -i "c:/steven/videos/breaking bad/season 3/$1" -t 10 -y outfile.mp4
-./ffmpeg -v warning -codecs | grep png
