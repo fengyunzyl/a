56d9a2f1112ed2c96fb252f00e131bafbbb00d72
Steven Penny <svnpenn@gmail.com>
2013-05-24 07:48:47 -0500

    M ff-audio.sh

--- ff-audio.sh
+++ ff-audio.sh
@@ -21,31 +21,42 @@ warn ()
 log ()
 {
   unset PS4
-  set $((set -x; : "$@") 2>&1)
-  shift
-  warn $*
-  eval $*
+  qq=$((set -x; : "$@") 2>&1)
+  warn "${qq:2}"
+  eval "${qq:2}"
 }
 
 usage ()
 {
-  echo "Script will search the current directory for JPG file and either FLAC"
-  echo "or MP3 files. It will use these files to create high quality videos."
-  echo "Finally it will upload videos to YouTube."
-  echo "Once you are sure environment is correct run"
+  echo "Script will search current directory for FLAC, MP3, PNG or JPG files."
+  echo "It will use these files to create high quality videos. Finally it will"
+  echo "upload videos to YouTube. Once you are sure environment is correct run"
   echo "$0 -f"
   exit
 }
 
 querystring ()
 {
-  sed 's/ /\&/g' <<< "${qs[*]}"
+  sed 's/ /\&/g' <<< ${qs[*]}
 }
 
+exten ()
+{
+  sed "s/[^.]*$/$2/;s/[^.[:alnum:]]/-/g" <<< ${!1}
+}
+
+if ! [ -a ~/googlecl ]
+then
+  echo google not found
+  exit
+fi
+
 [ $1 ] || usage
-img=(*.jpg)
-[ -a $img ] || usage
 shopt -s extglob
+img=(
+  @(*.png|*.jpg)
+)
+[ -a $img ] || usage
 songs=(
   @(*.flac|*.mp3)
 )
@@ -54,18 +65,17 @@ declare -A titles
 for song in "${songs[@]}"
 do
   warn $song
-  eval $(fpcalc "$song" | sed 1d)
+  . <(fpcalc "$song" | sed 1d)
   qs=(
     client=8XaBELgH
     duration=$DURATION
     fingerprint=$FINGERPRINT
-    meta=recordingids
+    meta=recordings
   )
   warn connect to acoustid.org...
-  curl -s api.acoustid.org/v2/lookup?`querystring` |
-    jq '.results[0]' > .json
-  warn $(JQ '.id')
-  rid=$(JQ '.recordings[0].id')
+  curl -s api.acoustid.org/v2/lookup?`querystring` | jq .results[0] > .json
+  warn `JQ .id`
+  rid=$(JQ ".recordings | min_by($DURATION - (.duration // 0) | . * .).id")
   # hit musicbrainz API for entire album
   if ! [ $date ]
   then
@@ -76,24 +86,22 @@ do
     )
     warn connect to musicbrainz.org...
     curl -s musicbrainz.org/ws/2/release?`querystring` |
-      jq '.releases | sort_by(.["cover-art-archive"]) | .[length - 1]' > .json
-    cp .json release.json
-    album=$(JQ '.title')
+      jq '.releases | max_by(.["cover-art-archive"])' > .json
+    cp .json rls.json
+    album=`JQ .title`
     label=$(JQ '.["label-info"][0].label.name')
-    date=$(JQ '.date')
+    date=`JQ .date`
   fi
-  cat release.json |
-    jq ".media[0].tracks[] | select(.recording.id == \"$rid\")" > .json
-  title=$(JQ '.title')
+  jq ".media[0].tracks[] | select(.recording.id == \"$rid\")" rls.json > .json
+  title=`JQ .title`
   artist=$(JQ '.["artist-credit"][] | .name, .joinphrase')
-  meta=${song%.*}.txt
   {
     echo Title: $title
     echo Album: $album
     echo Artist: $artist
     echo Label: $label
     echo Date: $date
-  } | tee "$meta"
+  } | tee `exten song txt`
   warn 'enter "y" if metadata is ok'
   read uu
   [ $uu ] || exit
@@ -103,14 +111,13 @@ done
 for song in "${songs[@]}"
 do
   mp3gain -s d "$song"
-  video=${song%.*}.mp4
   ffprobe -v error -show_format -print_format json "$song" > .json
-  # Adding "-preset" would only make small difference in size or speed. Make
-  # sure input picture is at least 720. "-shortest" can mess up duration. Adding
-  # "-analyzeduration" would only suppress warning, not change file.
-  log ffmpeg -loop 1 -r 1 -i "$img" -i "$song" -t `JQ .format.duration` \
-    -qp 0 -filter:v 'crop=trunc(iw/2)*2' \
-    -c:a aac -strict -2 -b:a 384k -v error -stats "$video"
+  # Adding "-preset" would only make small difference in size or speed.
+  # "-shortest" can mess up duration. Adding "-analyzeduration" would only
+  # suppress warning, not change file.
+  log ffmpeg -loop 1 -r 1 -i "$img" -i "$song" -t `JQ .format.duration` -qp 0 \
+    -filter:v 'scale=trunc(oh*a/2)*2:720' -b:a 384k -v error \
+    -stats `exten song mp4`
 done
 
 if (( ${#album} > 30 ))
@@ -120,9 +127,7 @@ fi
 
 for song in "${songs[@]}"
 do
-  meta=${song%.*}.txt
-  video=${song%.*}.mp4
   # category is case sensitive
-  log google youtube post -c Music -n "$artist, ${titles[$song]}" -s "$meta" \
-    -t "$album, $artist" -u svnpenn "$video"
+  log google youtube post -c Music -n "$artist, ${titles[$song]}" \
+    -s `exten song txt` -t "$album, $artist" -u svnpenn `exten song mp4`
 done
