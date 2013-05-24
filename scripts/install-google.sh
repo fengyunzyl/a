1c496014c001d062836a8304d8ac3737b6da1d04
Steven Penny <svnpenn@gmail.com>
2013-05-24 07:05:57 -0500

    Revert "google"
    This reverts commit cfacef2b06c5f3fe2f5c64b47b0990b37187e326.

---
+++ scripts/install-google.sh
@@ -0,0 +1,8 @@
+# install google
+# http://googlecl.googlecode.com
+
+cd
+wget googlecl.googlecode.com/files/googlecl-win32-0.9.14.zip
+unzip googlecl-win32-0.9.14.zip
+cd googlecl
+chmod +x google.exe python26.dll pyexpat.pyd _socket.pyd _ssl.pyd
