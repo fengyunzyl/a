9db270e0e37ba96f08896ef80d696e36aa79609b
Steven Penny <svnpenn@gmail.com>
2013-05-26 15:37:02 -0500

    polarssl

--- scripts/install-polarssl.sh
+++
@@ -1,9 +0,0 @@
-# Install PolarSSL
-wget polarssl.org/download/polarssl-1.2.0-gpl.tgz
-tar xf polarssl-1.2.0-gpl.tgz
-cd polarssl-1.2.0
-make lib \
-  AR=i686-w64-mingw32-ar \
-  CC=i686-w64-mingw32-gcc
-make install \
-  DESTDIR=/usr/i686-w64-mingw32/sys-root/mingw
