290bc9c9ace0017a63976aaabe5c445d48b99ea0
Steven Penny <svnpenn@gmail.com>
2013-06-12 01:12:51 -0500

    install-php

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -2,7 +2,9 @@
 
 mkdir -p ~/php
 cd ~/php
-wget windows.php.net/downloads/releases/php-5.4.14-nts-Win32-VC9-x86.zip
-unzip php-5.4.14-nts-Win32-VC9-x86.zip
+wget windows.php.net/downloads/releases/sha1sum.txt
+set $(awk 'END {print $2}' RS='\n\n' FS=- sha1sum.txt)
+wget windows.php.net/downloads/releases/php-$1-Win32-VC9-x86.zip
+unzip php-$1-Win32-VC9-x86.zip
 chmod +x php.exe php5.dll libeay32.dll ssleay32.dll ext/php_curl.dll
 echo extension=ext/php_curl.dll > php.ini
