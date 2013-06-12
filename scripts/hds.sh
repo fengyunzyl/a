f95ade07b2dfc64788e7cb5fe59d29862821651b
Steven Penny <svnpenn@gmail.com>
2013-06-12 02:43:52 -0500

    hds

--- scripts/hds.sh
+++ scripts/hds.sh
@@ -1,3 +1,35 @@
-# run AdodeHDS.php from any directory
+# seek with Adobe HTTP Dynamic Streaming
+
+usage ()
+{
+  echo usage: $0 COMMAND
+  exit
+}
+
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*" >&2
+}
+
+(( $# )) || usage
 shift 2
-php /cygwin/opt/scripts/adobehds.php $*
+
+while :
+do
+  warn '[#,q]?'
+  read line
+  if [[ $line =~ ^[0-9]+$ ]]
+  then
+    php /opt/scripts/adobehds.php --play --start $line $* |
+      ffplay -v warning - &
+    sleep 6
+    [ $OLD ] && kill -7 $OLD
+    OLD=$!
+  elif [[ $line = q ]]
+  then
+    exit
+  else
+    warn '# - start fragment'
+    warn 'q - quit'
+  fi
+done
