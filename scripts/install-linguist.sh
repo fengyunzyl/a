309cc1b68a510a1b67cdbfeac75ec7e34d02bdc8
Steven Penny <svnpenn@gmail.com>
2013-06-16 22:19:37 -0500

    linguist

--- scripts/install-linguist.sh
+++ scripts/install-linguist.sh
@@ -4,11 +4,6 @@ setup -nqP libicu-devel,patch,ruby,zlib-devel
 # charlock_holmes
 git clone --depth 1 git://github.com/brianmario/charlock_holmes.git
 cd charlock_holmes
-# github.com/brianmario/charlock_holmes/issues/32
-sed -i '
-50 s/^/have_library "z" or abort "libz missing"/
-51 s/^/have_library "icuuc" or abort "libicuuc missing"/
-' ext/charlock_holmes/extconf.rb
 gem build charlock_holmes.gemspec
 gem install charlock_holmes
 cd -
