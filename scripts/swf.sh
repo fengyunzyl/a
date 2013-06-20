6f0db00fd03634920a18d48eca9c9cc16eb6037d
Steven Penny <svnpenn@gmail.com>
2013-06-20 11:45:37 -0500

    swf.sh

--- scripts/swf.sh
+++ scripts/swf.sh
@@ -20,23 +20,19 @@ log ()
   eval "${qq:2}"
 }
 
+gemi ()
+{
+  git clone --depth 1 git@github.com:$1/$2
+  cd $2
+  gem build $2.gemspec
+  gem install $2
+}
+
 [ $1 ] || usage
 arg_file=$1
-
-if ! [ -a /bin/gem ]
-then
-  log setup -nqP ruby
-fi
-
-if ! [ -a /bin/furnace-swf ]
-then
-  log gem install furnace-swf
-fi
-
-if ! [ -a /bin/furnace-avm2 ]
-then
-  log gem install furnace-avm2
-fi
+hash gem || log setup -nqP ruby
+hash furnace-swf || gemi whitequark furnace-swf
+hash furnace-avm2 || gemi whitequark furnace-avm2
 
 log furnace-swf -i $arg_file abclist |
   tee /dev/tty | cut -s -d'"' -f2 | while read aa
