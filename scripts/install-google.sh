c9e465aacc6a0e10121b5c90fa5b8e31ef098487
Steven Penny <svnpenn@gmail.com>
2013-06-23 19:27:52 -0500

    install-google

--- scripts/install-google.sh
+++ scripts/install-google.sh
@@ -1,5 +1,9 @@
-# install google
-# http://googlecl.googlecode.com
+# this version is better
+# - dont have to install python
+# - properly opens browser
 
-setup -nqP googlecl,python-gdata
-mkdir -p ~/.local/share/googlecl
+cd
+wget googlecl.googlecode.com/files/googlecl-win32-0.9.14.zip
+unzip googlecl-win32-0.9.14.zip
+cd googlecl
+chmod +x google.exe python26.dll pyexpat.pyd _socket.pyd _ssl.pyd
