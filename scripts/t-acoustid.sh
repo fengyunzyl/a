58adb0fb48ac144ee6457100a2123ada3874d756
Steven Penny <svnpenn@gmail.com>
2013-06-24 20:21:14 -0500

    t-acoustid.sh

--- scripts/t-acoustid.sh
+++ scripts/t-acoustid.sh
@@ -26,3 +26,6 @@ try 308 b5dcad58-6e4e-453e-828e-c789e6a515fd unfinished-sympathy.json
 
 # acoustid.org/track/b2e48096-2183-4482-8492-b33b09760f4c
 try 471 b2e48096-2183-4482-8492-b33b09760f4c protection.json
+
+# acoustid.org/track/f3e95cce-2899-49f8-a95c-3de761235e34
+try 497 f3e95cce-2899-49f8-a95c-3de761235e34 antistar.json
