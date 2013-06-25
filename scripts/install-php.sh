a7630d9ecd49fd51db439525fa1aba26418dac79
Steven Penny <svnpenn@gmail.com>
2013-06-25 09:42:56 -0500

    M scripts/install-php.sh

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/php
 cd ~/php
 wget windows.php.net/downloads/releases/sha1sum.txt
-set $(awk '/php-[^-]*-Win32/ {a=$2} END {print a}' sha1sum.txt)
+set $(awk '/VC9/ {a=$6} END {print a}' RS='\n\n' sha1sum.txt)
 wget windows.php.net/downloads/releases/$1
 unzip $1
 chmod +x php.exe php5ts.dll libeay32.dll ssleay32.dll ext/php_curl.dll
