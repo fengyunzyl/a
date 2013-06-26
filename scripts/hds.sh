28a6fd57ae259c40524652f75aa2857d31170989
Steven Penny <svnpenn@gmail.com>
2013-06-25 20:31:55 -0500

    M scripts/hds.sh  M scripts/t-musicbrainz.sh

--- scripts/hds.sh
+++ scripts/hds.sh
@@ -1,35 +1,3 @@
-# seek with Adobe HTTP Dynamic Streaming
-
-usage ()
-{
-  echo usage: $0 COMMAND
-  exit
-}
-
-warn ()
-{
-  printf '\e[36m%s\e[m\n' "$*" >&2
-}
-
-(( $# )) || usage
+# run AdodeHDS.php from any directory
 shift 2
-
-while :
-do
-  warn '[#,q]?'
-  read line
-  if [[ $line =~ ^[0-9]+$ ]]
-  then
-    php /opt/scripts/adobehds.php --play --start $line $* |
-      ffplay -v warning - &
-    sleep 6
-    [ $OLD ] && kill -13 $OLD
-    OLD=$!
-  elif [[ $line = q ]]
-  then
-    exit
-  else
-    warn '# - start fragment'
-    warn 'q - quit'
-  fi
-done
+php $PUBLIC/opt/scripts/adobehds.php $*
