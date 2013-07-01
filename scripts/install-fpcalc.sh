c8e5a4bff88a4ec200fa5f7a200a2e43892d06f9
Steven Penny <svnpenn@gmail.com>
2013-07-01 03:45:50 -0500

    fpcalc

--- scripts/install-fpcalc.sh
+++ scripts/install-fpcalc.sh
@@ -1,6 +1,7 @@
 # also known as chromaprint
 
-wget bitbucket.org/acoustid/chromaprint/downloads/chromaprint-fpcalc-0.6-win32.zip
-unzip chromaprint-fpcalc-0.6-win32.zip
-cd chromaprint-fpcalc-0.6-win32
+set chromaprint-fpcalc-0.6-win32
+wget bitbucket.org/acoustid/chromaprint/downloads/$1.zip
+unzip $1.zip
+cd $1
 cp fpcalc /usr/local/bin
