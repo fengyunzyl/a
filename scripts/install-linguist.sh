dcf06372e8277a6b579e3b5c563dc8d09f542594
Steven Penny <svnpenn@gmail.com>
2013-07-04 23:56:14 -0500

    M scripts/install-linguist.sh

--- scripts/install-linguist.sh
+++ scripts/install-linguist.sh
@@ -1,20 +1,17 @@
 # Cygwin packages
-setup -nqP libicu-devel,patch,ruby,zlib-devel
+setup -nqP libicu-devel,patch,ruby
 
 # charlock_holmes
-git clone --depth 1 git://github.com/brianmario/charlock_holmes.git
+git clone --single-branch git@github.com:brianmario/charlock_holmes
 cd charlock_holmes
 # github.com/brianmario/charlock_holmes/issues/32
-sed -i '
-50 s/^/have_library "z" or abort "libz missing"/
-51 s/^/have_library "icuuc" or abort "libicuuc missing"/
-' ext/charlock_holmes/extconf.rb
+sed -i '51 s/^/have_library "icuuc"/' ext/charlock_holmes/extconf.rb
 gem build charlock_holmes.gemspec
 gem install charlock_holmes
 cd -
 
 # Install posix-spawn
-git clone --depth 1 git://github.com/rtomayko/posix-spawn.git
+git clone git@github.com:rtomayko/posix-spawn
 cd posix-spawn
 gem build posix-spawn.gemspec
 gem install posix-spawn
