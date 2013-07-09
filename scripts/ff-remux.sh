2195d170621804fb12db729c9d8868d472e08d11
Steven Penny <svnpenn@gmail.com>
2013-07-08 22:55:09 -0500

    ff-remux.sh

--- scripts/ff-remux.sh
+++ scripts/ff-remux.sh
@@ -23,8 +23,9 @@ usage ()
 
 buffer ()
 {
+  set $1 $(reg query 'hkcu\console' | grep ScreenBufferSize)
+  (( $(( $4 & 0xffff )) == $1 )) && return
   set $(printf '%04x ' $1 2000 25)
-  [ $(reg query 'hkcu\console' | awk /nB/,NF=1 FPAT='....$') = $1 ] && return
   reg add 'hkcu\console' -f -t reg_dword -v ScreenBufferSize -d 0x$2$1
   reg add 'hkcu\console' -f -t reg_dword -v WindowSize -d 0x$3$1
   cygstart bash -l
