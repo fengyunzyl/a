c11990118cd105c480d947d10632eb1358b7b536
Steven Penny <svnpenn@gmail.com>
2013-07-10 20:34:02 -0500

    M scripts/install-notepad2.sh

--- scripts/install-notepad2.sh
+++ scripts/install-notepad2.sh
@@ -9,7 +9,7 @@ sed -bi 's/sc.Match("$((")/& || sc.Match("$(")/' scintilla/lexers/lexbash.cxx
 # build
 cd build
 export WDKBASEDIR="$HOMEDRIVE/winddk/7600.16385.win7_wdk.100208-1538"
-$WINDIR/system32/cmd /c build_wdk.bat Build x64
+cmd /c build_wdk.bat Build x64
 
 # install
 cd "$ProgramW6432"
