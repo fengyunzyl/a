6cb67b5cc4874694b13988f8a19815b96d81bf1e
Steven Penny <svnpenn@gmail.com>
2013-07-18 18:56:38 -0500

    thumbnail.sh

--- scripts/thumbnail.sh
+++ scripts/thumbnail.sh
@@ -32,7 +32,7 @@ awk "BEGIN {
   d = $stream_0_duration
   ar = w / h
   pics = ar > 2 ? 36 : 30
-  a = .07 * d
+  a = .08 * d
   b = d - a
   interval = (b - a) / (pics - 1)
   for (ss = a; pics-- > 0; ss += interval)
