b060fc219e65d0a09cbd958479a63079073da127
Steven Penny <svnpenn@gmail.com>
2013-11-09 02:36:52 -0600

    cygwin-mirrors

--- scripts/cygwin-mirrors.sh
+++ scripts/cygwin-mirrors.sh
@@ -1,5 +1,31 @@
 # Print Cygwin mirrors
 
+warn ()
+{
+  printf '\e[36m%s\e[m\n' "$*"
+}
+
+log ()
+{
+  warn $*
+  eval $*
+}
+
 curl -s sourceware.org/cygwin/mirrors.lst |
-  awk '/http/ && /United States/ {print length($1), $1}' FS=';' |
-  sort
+  awk '/http/ && /United States/ && NF=1' FS=';' > mirrors.lst
+
+while read ee
+do
+  if log wget --spider -t1 -T1 -q $ee
+  then
+    ff+=($ee)
+  fi
+done < mirrors.lst
+
+for gg in ${ff[*]}
+do
+  echo "${#gg} $gg"
+done |
+sort
+
+rm mirrors.lst
