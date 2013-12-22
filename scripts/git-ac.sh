ff3782d57a3565e475f7ad368098b880d84c3a04
Steven Penny <svnpenn@gmail.com>
2013-12-21 20:31:16 -0600

    git-ac

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -3,6 +3,6 @@
 # git add -A     modified, new and deleted files
 # git commit -a  modified and deleted files
 
-set "$(git status -s)"
+set -- "$(git diff | awk '/^[+-][^+-]/ {print;exit}')"
 git add -A
 git commit -m "$1"
