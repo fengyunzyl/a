592b674cbade805a19179bc338791cbb749e0573
Steven Penny <svnpenn@gmail.com>
2013-12-22 13:30:33 -0600

    print first added line if found, else print first removed line

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -1,4 +1,3 @@
-
 # we need a catch in case $1 is nonsense
 if (( $# ))
 then
@@ -7,5 +6,11 @@ else
   git add -A
 fi
 
-b=$(git diff --cached | awk '/^[+-][^+-]/{print;exit}')
+# print first added line if found, else print first removed line
+b=$(git diff --cached | awk '
+  /^+[^+]/       {m=$0;exit}
+  /^-[^-]/ && !m {m=$0}
+  END            {sub(/^[-+# ]+/, "", m); print m}
+')
+
 git commit -m "$b"
