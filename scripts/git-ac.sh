cd785935fd50cc187b3f0d117e6dae5f3271f2c8
Steven Penny <svnpenn@gmail.com>
2013-12-23 01:08:27 -0600

    y=$(git diff --cached --color | awk '

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -1,11 +1,18 @@
-
 git add -A "$1"
 
 # print first added line if found, else print first removed line
-y=$(git diff --cached | awk '
-  /^+[^+]/       {z=$0;exit}
-  /^-[^-]/ && !z {z=$0}
-  END            {sub(/^[-+# ]+/, "", z); print z}
-')
+y=$(git diff --cached --color | awk '
+/\033.3[12]/ {
+  c=$4
+  if (c==1 && m) next
+  gsub(/\033[^m]+m/, "")
+  sub(/^[-+# ]+/, "")
+  m=$0
+  if (c==2 && m) exit
+}
+END {
+  print m
+}
+' FPAT=.)
 
 git commit -m "$y"
