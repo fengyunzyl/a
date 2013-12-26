e0ee92ddac397b2bbf5a0eafe65b947ecadbc0ca
Steven Penny <svnpenn@gmail.com>
2013-12-25 18:31:34 -0600

    fix svnpenn/dotfiles#2

--- scripts/cygpath.sh
+++ scripts/cygpath.sh
@@ -1,2 +1,7 @@
-# run Windows native programs
+# programs that produce linux paths
+# \C-x\C-e  edit-and-execute-command
+# Git
+
+# programs that require windows paths
+# Notepad2
 $(cygpath -m $*)
