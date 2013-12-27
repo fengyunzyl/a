6d4b258f46832bdb45edd31701b8bf45841517a4
Steven Penny <svnpenn@gmail.com>
2013-12-27 10:55:17 -0600

    ./configure --host x86_64-w64-mingw32

--- scripts/install-jq.sh
+++ scripts/install-jq.sh
@@ -4,11 +4,5 @@ setup-x86 -nqP bison,flex,libtool
 git clone --depth 1 git://github.com/stedolan/jq
 cd jq
 autoreconf -i
-./configure --host i686-w64-mingw32
+./configure --host x86_64-w64-mingw32
 make
-
-# archive
-cd ${0%/*}
-VERSION=$(git log --follow --oneline $0 | wc -l)
-cd -
-zip jq-${VERSION}.zip jq.exe
