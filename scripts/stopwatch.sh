e3b9a2835b42408b2cdcc2353b2319e7605f9f09
Steven Penny <svnpenn@gmail.com>
2013-12-29 01:45:44 -0600

    x=$(date +%s.%N)

--- scripts/stopwatch.sh
+++ scripts/stopwatch.sh
@@ -1,9 +1,8 @@
 # http://superuser.com/q/611538
 
-x=$(date +%s)
+x=$(date +%s.%N)
 while :
 do
-  y=$(date +%s)
-  printf '%s\r' $(date +%T -ud@$((y-x)))
-  sleep 1
+  y=$(date +%s.%N)
+  printf '%.11s\r' $(date +%T.%N -ud@$(awk "BEGIN{print($y-$x)}"))
 done
