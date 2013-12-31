fb611283df1b6d58f4b0aa5924e4c47b2ca2650d
Steven Penny <svnpenn@gmail.com>
2013-12-30 19:27:28 -0600

    the script will count number of files with good extensions.

--- scripts/path.sh
+++ scripts/path.sh
@@ -1,3 +1,38 @@
+# the script will count number of files with good extensions.
+# the script will also list the "bad" extensions found.
+# this way i can add to the list if necessary.
+# we are not going to count scripts with no extension. those should be
+# symbolic links. Good thing about extension is we dont have to worry if the
+# file is executable.
 
-echo FOLDER "'$1'"
-echo $(find $1 -maxdepth 1 -executable -type f | wc -l) files
+usage ()
+{
+  echo "usage: ${0##*/} FOLDER"
+  exit
+}
+
+(( $# )) || usage
+good='\.(exe|sh)$'
+declare -A bad
+
+for item in "$1"/*
+do
+  # need to filter out directories
+  [ -d "$item" ] && continue
+  if [[ $item =~ $good ]]
+  then
+    (( ct++ ))
+  else
+    # make note of the extension
+    exn=${item##*.}
+    bad[$exn]=
+  fi
+done
+
+printf '%s\n  %s\n\n' 'good extensions'      "$ct"
+printf '%s\n'         'discarded extensions'
+
+for item in "${!bad[@]}"
+do
+  printf '  %s\n' $item
+done
