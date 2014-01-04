a02161b6a7aea591d8757398b52e5ad557e80f49
Steven Penny <svnpenn@gmail.com>
2014-01-03 22:47:04 -0600

    echo usage: ${0##*/} ROWS COLUMNS

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -2,17 +2,17 @@
 
 usage ()
 {
-  echo usage: $0 ROWS COLUMNS
+  echo usage: ${0##*/} ROWS COLUMNS
   set $(reg query 'hkcu\console' | grep ScreenBufferSize)
   echo current buffer rows $(( $3 >> 16 ))
   echo current buffer columns $(( $3 & 0xffff ))
   exit
 }
 
-[ $1 ] || usage
+(( $# )) || usage
 # convert to hex
 set $(printf '%04x ' $1 $2 25)
 reg add 'hkcu\console' -f -t reg_dword -v ScreenBufferSize -d 0x$1$2
 reg add 'hkcu\console' -f -t reg_dword -v WindowSize -d 0x$3$2
-cygstart bash -l
+cmd /c start bash -l
 kill -7 $PPID
