96d55886c7a1f07edd8f80a4fda42dcf604c5532
Steven Penny <svnpenn@gmail.com>
2014-01-05 20:59:30 -0600

    type xdg-open &>/dev/null || xdg-open () {

--- scripts/art.sh
+++ scripts/art.sh
@@ -1,19 +1,20 @@
 # get cover art
 
-usage ()
-{
+type xdg-open &>/dev/null || xdg-open () {
+  cmd /c start '' "$1"
+}
+
+usage () {
   echo usage: $0 ARTIST ALBUM
   exit
 }
 
-PATH=/bin:/usr/local/bin:${TMP%U*}progra~2/mozill~1
-hash firefox || exit
-[[ $2 ]] || usage
+(( $# < 2 )) && usage
 ARTIST=$1
 ALBUM=$2
 
-firefox "musicbrainz.org/search?type=release&query=$ARTIST $ALBUM"
-firefox "google.com/search?tbm=isch&q=$ARTIST $ALBUM"
-firefox "fanart.tv/api/getdata.php?type=2&s=$ARTIST"
-firefox "discogs.com/search?q=$ARTIST $ALBUM"
-firefox "wikipedia.org/w/index.php?search=${ARTIST// /+}+${ALBUM// /+}"
+xdg-open "http://google.com/search?tbm=isch&q=$ARTIST $ALBUM"
+xdg-open "http://fanart.tv/api/getdata.php?type=2&s=$ARTIST "
+xdg-open "http://discogs.com/search?q=$ARTIST $ALBUM"
+xdg-open "http://wikipedia.org/w/index.php?search=${ARTIST// /+}+${ALBUM// /+}"
+xdg-open "http://musicbrainz.org/search?type=release&query=$ARTIST $ALBUM"
