7874c778baa34d2c43aa2fef29f5269a5df7f20d
Steven Penny <svnpenn@gmail.com>
2014-01-05 21:00:21 -0600

    usage () {

--- scripts/swf.sh
+++ scripts/swf.sh
@@ -1,27 +1,24 @@
 # decompile and deobfuscate SWF
 # http://github.com/whitequark/furnace-avm2
 
-usage ()
-{
+usage () {
   echo usage: $0 FILE
   exit
 }
 
-warn ()
-{
+warn () {
   printf '\e[36m%s\e[m\n' "$*" >&2
 }
 
-log ()
-{
+log () {
   unset PS4
-  qq=$((set -x; : "$@") 2>&1)
+  qq=$(( set -x
+         : "$@" ) 2>&1)
   warn "${qq:2}"
   eval "${qq:2}"
 }
 
-gemi ()
-{
+gemi () {
   git clone --depth 1 git://github.com/$1/$2
   cd $2
   gem build $2.gemspec
@@ -30,9 +27,9 @@ gemi ()
 
 [ $1 ] || usage
 arg_file=$1
-hash gem || log setup -nqP ruby
-hash furnace-swf || gemi whitequark furnace-swf
-hash furnace-avm2 || gemi whitequark furnace-avm2
+type gem || exit
+type furnace-swf || gemi whitequark furnace-swf
+type furnace-avm2 || gemi whitequark furnace-avm2
 
 log furnace-swf -i $arg_file abclist |
   tee /dev/tty | cut -s -d'"' -f2 | while read aa
