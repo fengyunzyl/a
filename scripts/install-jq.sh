5b12b4ea36fe22f6b6ccc4ac7268f4586ec94bfa
Steven Penny <svnpenn@gmail.com>
2014-01-06 18:42:12 -0600

    ./configure --host i686-w64-mingw32

--- scripts/install-jq.sh
+++ scripts/install-jq.sh
@@ -4,5 +4,5 @@ setup-x86 -nqP bison,flex,libtool
 git clone --depth 1 git://github.com/stedolan/jq
 cd jq
 autoreconf -i
-./configure --host x86_64-w64-mingw32
+./configure --host i686-w64-mingw32
 make
