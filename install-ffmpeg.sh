a7c6b2d72a243187236815d684a6fa01407aa7cd
Steven Penny <svnpenn@gmail.com>
2014-01-06 20:05:51 -0600

    fix #3

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -1,30 +1,20 @@
-# Cygwin FFmpeg with libfdk_aac
-setup -nqP libtool,yasm
+# FFmpeg with libfdk_aac
 HOST=i686-w64-mingw32
-PREFIX=/mingw32/i686-w64-mingw32
+PREFIX=/usr/i686-w64-mingw32/sys-root/mingw
+setup-x86 -nqP libtool,mingw64-i686-gcc,yasm
 
-# GCC
-wget downloads.sf.net/mingw-w64/i686-w64-mingw32-gcc-4.8.0-cygwin_rubenvb.tar.xz
-tar xf i686-w64-mingw32-gcc-4.8.0-cygwin_rubenvb.tar.xz
-mv mingw32 /
-
-# fdk_aac
-git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
+# fdk-aac
+git clone --depth 1 git://github.com/mstorsjo/fdk-aac
 cd fdk-aac
-autoreconf -iv
-./configure \
-  --host=$HOST \
-  --prefix=$PREFIX
+autoreconf -i
+./configure --host=$HOST --prefix=$PREFIX
 make -j5 install
 cd -
 
 # x264
-git clone --depth 1 git://git.videolan.org/x264.git
+git clone --depth 1 git://git.videolan.org/x264
 cd x264
-./configure \
-  --enable-static \
-  --enable-win32thread \
-  --cross-prefix=$HOST- \
+./configure --enable-static --enable-win32thread --cross-prefix=$HOST- \
   --prefix=$PREFIX
 make -j5 install
 cd -
@@ -33,26 +23,14 @@ cd -
 wget zlib.net/zlib-1.2.8.tar.xz
 tar xf zlib-1.2.8.tar.xz
 cd zlib-1.2.8
-make -f win32/Makefile.gcc install \
-  PREFIX=$HOST- \
-  DESTDIR=$PREFIX \
-  BINARY_PATH=/bin \
-  INCLUDE_PATH=/include \
-  LIBRARY_PATH=/lib
+make -f win32/Makefile.gcc PREFIX=$HOST- DESTDIR=$PREFIX BINARY_PATH=/bin \
+  INCLUDE_PATH=/include LIBRARY_PATH=/lib install
 cd -
 
 # ffmpeg
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-./configure \
-  --enable-gpl \
-  --enable-libx264 \
-  --enable-nonfree \
-  --enable-libfdk-aac \
-  --arch=x86 \
-  --target-os=mingw32 \
-  --logfile=/dev/stdout \
-  --extra-ldflags=-static \
-  --cross-prefix=$HOST- \
-  --host-cc=$HOST-gcc
+./configure --enable-gpl --enable-libx264 --enable-nonfree --enable-libfdk-aac \
+  --arch=x86 --target-os=mingw32 --logfile=/dev/stdout --extra-ldflags=-static \
+  --cross-prefix=$HOST- --host-cc=$HOST-gcc
 make -j5 install
--- scripts/t-ffmpeg.sh
+++ scripts/t-ffmpeg.sh
@@ -1,5 +1,5 @@
 # test FFmpeg
 
-set 'Breaking Bad - S03E12 - Half Measures.mp4'
+set 'Breaking Bad S3E12 Half Measures.mp4'
 ./ffmpeg -i "c:/steven/videos/breaking bad/season 3/$1" -t 10 -y outfile.mp4
 ./ffmpeg -v warning -codecs | grep png
