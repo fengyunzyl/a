ee2d28f65c9a1e454c8ecd0093f0bf162a263efe
Steven Penny <svnpenn@gmail.com>
2014-01-12 17:04:35 -0600

    echo usage: ${0##*/} WORD FILE

--- scripts/variable.sh
+++ scripts/variable.sh
@@ -1,53 +1,24 @@
 # find unused variable names
 
 usage () {
-  echo usage: $0 NUMBER [FILE]
+  echo usage: ${0##*/} WORD FILE
   exit
 }
 
-build () {
-  printf -v ccc "%$1s"
-  bbb=()
-  for aaa in {a..z}
-  do
-    ccc=${ccc//?/$aaa}
-    if [[ $arg_file =~ $ccc ]]
-    then
-      bbb=()
-    elif [[ ${bad[*]} =~ $ccc ]]
-    then
-      bbb=()
-    else
-      bbb+=($ccc)
-    fi
-    if (( ${#bbb[*]} == arg_num ))
-    then
-      echo ${bbb[*]}
-      exit
-    fi
-  done
-}
+(( $# < 2 )) && usage
+read -d '' arg_file < "$2"
+b=$(awk NF=NF OFS=, FPAT=[^aeilou] <<< ${1:1})
+eval set ${1::1}{$b}
+declare -A cans
 
-(( $# )) || usage
-arg_num=$1
+for foo
+do
+  [[ $arg_file =~ $foo  ]] && continue
+  [[ $foo      =~ ar|at ]] && continue
+  cans[$foo]=
+done
 
-if (( ${#2} ))
-then
-  read -d '' arg_file < "$2"
-else
-  read -d '' arg_file
-fi
-
-arg_file=${arg_file,,}
-
-# one character
-bad=( i j l )
-build 1
-
-# two characters
-bad=( cc dd ii jj ll )
-build 2
-
-# three characters
-bad=( iii jjj lll )
-build 3
+for can in "${!cans[@]}"
+do
+  echo $can
+done
