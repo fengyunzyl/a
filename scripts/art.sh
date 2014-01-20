740672ca901d5968f91db677a529fd0689295ffa
Steven Penny <svnpenn@gmail.com>
2014-01-20 17:55:32 -0600

    there are one or more whitespace characters

--- scripts/art.sh
+++ scripts/art.sh
@@ -1,7 +1,9 @@
 # get cover art
 
 type xdg-open &>/dev/null || xdg-open () {
-  cmd /c start '' "$1"
+  # there are one or more whitespace characters
+  # between the two quote characters
+  cmd /c start '' "$1 "
 }
 
 usage () {
@@ -14,7 +16,7 @@ ARTIST=$1
 ALBUM=$2
 
 xdg-open "http://google.com/search?tbm=isch&q=$ARTIST $ALBUM"
-xdg-open "http://fanart.tv/api/getdata.php?type=2&s=$ARTIST "
+xdg-open "http://fanart.tv/api/getdata.php?type=2&s=$ARTIST"
 xdg-open "http://discogs.com/search?q=$ARTIST $ALBUM"
 xdg-open "http://wikipedia.org/w/index.php?search=${ARTIST// /+}+${ALBUM// /+}"
 xdg-open "http://musicbrainz.org/search?type=release&query=$ARTIST $ALBUM"
