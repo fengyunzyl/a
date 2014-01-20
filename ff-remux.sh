65c49b9a8a834d9d1201605bb1380b5196d36e18
Steven Penny <svnpenn@gmail.com>
2014-01-20 17:23:05 -0600

    cmd /c start bash

--- ff-remux.sh
+++ ff-remux.sh
@@ -25,7 +25,7 @@ buffer () {
   set $(printf '%04x ' $1 2000 25)
   reg add 'hkcu\console' -f -t reg_dword -v ScreenBufferSize -d 0x$2$1
   reg add 'hkcu\console' -f -t reg_dword -v WindowSize -d 0x$3$1
-  cmd /c start bash -l
+  cmd /c start bash
   kill -7 $PPID
 }
 
