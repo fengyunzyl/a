09dc6d1a8af4f22c59365e82b411a8891ee1e9f3
Steven Penny <svnpenn@gmail.com>
2014-01-30 18:20:33 -0600

    echo "usage: ${0##*/} SEARCH SORT CATEGORY"

--- scripts/torrent.sh
+++ scripts/torrent.sh
@@ -4,12 +4,18 @@
 }
 
 usage () {
-  echo "usage: ${0##*/} CATEGORY SEARCH"
-  echo "207  HD - Movies"
-  echo "208  HD - TV shows"
+  echo "usage: ${0##*/} SEARCH SORT CATEGORY"
+  echo
+  echo "SORT"
+  echo "3  date descending"
+  echo "6  size ascending"
+  echo
+  echo "CATEGORY"
+  echo "207  HD Movies"
+  echo "208  HD TV shows"
+  echo "301  Applications Windows"
   exit
 }
 
-(( $# < 2 )) && usage
-# 6  size ascending
-xdg-open "http://thepiratebay.se/search/$2/0/6/$1"
+(( $# == 3 )) || usage
+xdg-open "http://thepiratebay.se/search/$1/0/$2/$3"
