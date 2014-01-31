60b704ecc8305b591f34f99b99fb7c54bfa5b524
Steven Penny <svnpenn@gmail.com>
2014-01-30 21:41:43 -0600

    set "$HOMEDRIVE\program files" "$HOMEDRIVE\program files (x86)"

--- scripts/program-files.sh
+++ scripts/program-files.sh
@@ -1,15 +1,40 @@
-# clean up program files to recycle bin
-ds=$PWD
+set "$HOMEDRIVE\program files" "$HOMEDRIVE\program files (x86)"
+echo these items can be removed
 
-for ac in 'program files' 'program files (x86)'
+# clean up program files
+for ac
 do
-  cd "$HOMEDRIVE/$ac"
-  mkdir -p "$ds/$ac"
+  cd "$ac"
   for pg in *
   do
     if ! find "$pg" -iname '*.exe' | read
     then
-      mv "$pg" "$ds/$ac"
+      printf '\e[1;31m%s\e[m\n' "$ac\\$pg"
     fi
   done
+done
+
+# clean up local appdata
+cd "$LOCALAPPDATA"
+for pg in *
+do
+  [ -f "$pg" -o -h "$pg" ] && continue
+  if ! find "$@" -maxdepth 1 -iname "$pg*" | read
+  then
+    printf '\e[1;31m%s\e[m\n' "$LOCALAPPDATA\\$pg"
+  fi
+done
+
+# clean up appdata
+cd "$APPDATA"
+for pg in *
+do
+  [ -f "$pg" -o -h "$pg" ] && continue
+  if ! find "$@" -maxdepth 1 -iname "$pg*" | read
+  then
+    if ! find "$pg" -iname '*.exe' | read
+    then
+      printf '\e[1;31m%s\e[m\n' "$APPDATA\\$pg"
+    fi
+  fi
 done
