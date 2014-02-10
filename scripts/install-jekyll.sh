7ab4ce6f5c519022070cbdc01ff53087969aa3d9
Steven Penny <svnpenn@gmail.com>
2014-02-09 22:09:38 -0600

    packages

--- scripts/install-jekyll.sh
+++ scripts/install-jekyll.sh
@@ -1,14 +1,16 @@
-# cygwin packages
-setup-x86_64 -nqP gcc4-core,ruby
+# packages
+pacman -S gcc libcrypt-devel libffi-devel pkg-config ruby
 
-# gems
-gem install jekyll kramdown coderay
+# faster_require
+gem i faster_require
+sed -i "1crequire '$(gem w faster_require)'" $(gem w rubygems)
 
-# install coderay_bash
-set raw.github.com/rubychan/coderay/master/lib/coderay/scanners/bash.rb
-if ! wget --spider $1
-then
-  gem install coderay_bash
-  set $(find /lib/ruby -name scanners)
-  cp $2/bash.rb $1
-fi
+# v1.4.3
+git clone git://github.com/jekyll/jekyll
+cd jekyll
+gem b jekyll.gemspec
+gem i jekyll
+
+# rouge
+gem i rouge
+rougify style > rouge.css
