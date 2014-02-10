76748e0c7931b33eb3ca577301b085af5ad29891
Steven Penny <svnpenn@gmail.com>
2014-02-10 01:58:17 -0600

    export WDKBASEDIR=$HOMEDRIVE/winddk/7600.16385.win7_wdk.100208-1538

--- scripts/install-notepad2.sh
+++ scripts/install-notepad2.sh
@@ -3,22 +3,21 @@
 # number on about page
 git clone git://github.com/XhmikosR/notepad2-mod
 cd notepad2-mod
-SOURCE=$PWD
-
-# patch
 sed -bi 's/sc.Match("$((")/& || sc.Match("$(")/' scintilla/lexers/lexbash.cxx
 
 # build
 cd build
-export WDKBASEDIR="$HOMEDRIVE/winddk/7600.16385.win7_wdk.100208-1538"
-cygstart build_wdk.bat Build x64
+export WDKBASEDIR=$HOMEDRIVE/winddk/7600.16385.win7_wdk.100208-1538
+# cygstart build_wdk.bat Build x64
+start build_wdk build x64
+cd -
 
 # install
-cd "$ProgramW6432"
-mkdir -p notepad2
-cd notepad2
-cp $SOURCE/bin/wdk/release_x64/notepad2 .
-cat > notepad2.ini <<q
-[notepad2]
-notepad2.ini=%appdata%/notepad2/notepad2.ini
-q
+cd bin/wdk/release_x64
+set "$PROGRAMFILES"/notepad2
+mkdir -p "$1"
+cp notepad2 "$1"
+{
+  echo [notepad2]
+  echo notepad2.ini=%appdata%/notepad2/notepad2.ini
+} > "$1"/notepad2.ini
