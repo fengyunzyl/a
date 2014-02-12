0629ebf2fea4e339eb8be528e43b8a13b1948b8a
Steven Penny <svnpenn@gmail.com>
2014-02-12 00:02:17 -0600

    mg=$(git diff --cached --color | perl -F -ane '

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -2,18 +2,17 @@ git reset -q
 git add -A "$@"
 
 # print first added line if found, else print first removed line
-mg=$(git diff --cached --color | awk '
-/^\033\[3[12]m/ {
-  co=$4
-  if (co==1 && rd) next
-  gsub(/\033[^m]+m/, "")
-  sub(/^[+-] *(#+ )*/, "")
-  rd=$0
-  if (co==2 && rd) exit
-}
+mg=$(git diff --cached --color | perl -F -ane '
+/^\033\[3[12]m/ || next;
+$co=$F[3];
+$co==1 && $rd && next;
+s.\033[^m]+m..g;
+s.^[+-] *(#+ )*..;
+$rd=$_;
+$co==2 && $rd && last;
 END {
-  print rd
+  print $rd
 }
-' FPAT=.)
+')
 
 git commit -m "$mg"
