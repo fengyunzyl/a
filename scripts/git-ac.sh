8c38081847e33beb9d0604a7231315e41e46e4d6
Steven Penny <svnpenn@gmail.com>
2014-02-12 00:52:58 -0600

    $F[3]==1 && $rd && next;

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -4,12 +4,11 @@ git add -A "$@"
 # print first added line if found, else print first removed line
 mg=$(git diff --cached --color | perl -F -ane '
 /^\033\[3[12]m/ || next;
-$co=$F[3];
-$co==1 && $rd && next;
+$F[3]==1 && $rd && next;
 s.\033[^m]+m..g;
-s.^[+-] *(#+ )*..;
+s.^[+-]\s*(#+ )*..;
 $rd=$_;
-$co==2 && $rd && last;
+$F[3]==2 && $rd && last;
 END {
   print $rd
 }
