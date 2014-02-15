381d02f7075e72b554f2e7e83f1d6c9c11073799
Steven Penny <svnpenn@gmail.com>
2014-02-14 21:58:01 -0600

    autoconf  need for autoreconf

--- scripts/install-jq.sh
+++ scripts/install-jq.sh
@@ -1,8 +1,14 @@
 # up to date Windows native jq
-setup-x86 -nqP bison,flex,libtool
+
+# autoconf  need for autoreconf
+# automake  need for autoreconf > aclocal
+# libtool   need for autoreconf > libtool
+# bison     need for make
+# flex      need for make
+pacman -S autoconf automake bison flex libtool
 
 git clone --depth 1 git://github.com/stedolan/jq
 cd jq
 autoreconf -i
-./configure --host i686-w64-mingw32
-make
+./configure --host x86_64-w64-mingw32
+make -j4
