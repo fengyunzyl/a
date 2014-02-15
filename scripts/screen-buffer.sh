5c9b03efb6543a8047cd5ba445eab4a6f8844348
Steven Penny <svnpenn@gmail.com>
2014-02-15 13:45:53 -0600

    echo ${0##*/} ROWS COLUMNS

--- scripts/screen-buffer.sh
+++ scripts/screen-buffer.sh
@@ -1,7 +1,7 @@
 # get/set size of the terminal
 
 usage () {
-  echo usage: ${0##*/} ROWS COLUMNS
+  echo ${0##*/} ROWS COLUMNS
   set $(reg query 'hkcu\console' | grep ScreenBufferSize)
   echo current buffer rows $(( $3 >> 16 ))
   echo current buffer columns $(( $3 & 0xffff ))
@@ -13,5 +13,5 @@ usage () {
 set $(printf '%04x ' $1 $2 25)
 reg add 'hkcu\console' -f -t reg_dword -v ScreenBufferSize -d 0x$1$2
 reg add 'hkcu\console' -f -t reg_dword -v WindowSize -d 0x$3$2
-start bash
+powershell saps bash
 kill -7 $PPID
