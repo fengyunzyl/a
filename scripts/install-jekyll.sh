2296f21e10ff7970ad49ab82d31f7ef8eef59d95
Steven Penny <svnpenn@gmail.com>
2014-02-18 21:52:34 -0600

    2.0.0.alpha.1

--- scripts/install-jekyll.sh
+++ scripts/install-jekyll.sh
@@ -5,8 +5,8 @@ pacman -S gcc libcrypt-devel libffi-devel pkg-config ruby
 gem i faster_require
 sed -i "1crequire '$(gem w faster_require)'" $(gem w rubygems)
 
-# jekyll
-gem i jekyll
+# 2.0.0.alpha.1
+gem i jekyll --pr
 
 # rougify style > rouge.css
 gem i rouge
