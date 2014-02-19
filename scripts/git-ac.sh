58e1b257f32f20ffc6ffcc1ebe6b5a47ae525846
Steven Penny <svnpenn@gmail.com>
2014-02-18 22:03:15 -0600

    cr=$4

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -4,11 +4,12 @@ git add -A "$@"
 # print first added line if found, else print first removed line
 mg=$(git diff --cached --color | awk '
 /^\033\[3[12]m/ {
-  if ($4==1 && rd) next
+  cr=$4
+  if (cr==1 && rd) next
   gsub(/\033[^m]+m/, "")
   sub(/^[+-] *(#+ )*/, "")
   rd=$0
-  if ($4==2 && rd) exit
+  if (cr==2 && rd) exit
 }
 END {
   print rd
