0c0cc70ed10f51392aae3ebc9968fd25f8801d0f
Steven Penny <svnpenn@gmail.com>
2014-02-18 21:56:26 -0600

    ' FPAT=.)

--- scripts/git-ac.sh
+++ scripts/git-ac.sh
@@ -2,16 +2,17 @@ git reset -q
 git add -A "$@"
 
 # print first added line if found, else print first removed line
-mg=$(git diff --cached --color | perl -F -ane '
-/^\033\[3[12]m/ || next;
-$F[3]==1 && $rd && next;
-s.\033[^m]+m..g;
-s.^[+-]\s*(#+ )*..;
-$rd=$_;
-$F[3]==2 && $rd && last;
-END {
-  print $rd
+mg=$(git diff --cached --color | awk '
+/^\033\[3[12]m/ {
+  if ($4==1 && rd) next
+  gsub(/\033[^m]+m/, "")
+  sub(/^[+-] *(#+ )*/, "")
+  rd=$0
+  if ($4==2 && rd) exit
 }
-')
+END {
+  print rd
+}
+' FPAT=.)
 
 git commit -m "$mg"
