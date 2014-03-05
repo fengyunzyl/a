43045567b4750de2a670cfb31aeb346971fed9d4
Steven Penny <svnpenn@gmail.com>
2014-03-04 19:37:51 -0600

    if (( $# != 2 ))

--- scripts/printf.sh
+++ scripts/printf.sh
@@ -1,11 +1,19 @@
-usage () {
-  echo ${0##*/} INPUT
+if (( $# != 2 ))
+then
+  echo "${0##*/} <E|S> INPUT"
+  echo "E expression"
+  echo "S string"
   exit
-}
+fi
 
-(( $# )) || usage
+ag=$2
+[ $1 = S ] && ag=\"$2\"
 
-for foo in {a..z}
+bar=(
+  %a %b %d %e %.7f %g %h %i %j %k %l %m %n %o %p %q %r %s %t %u %v %w %x %y %z
+)
+
+for foo in ${bar[*]}
 do
-  awk "BEGIN {printf \"%%$foo\t%$foo\n\", \"$1\"}"
+  awk "BEGIN {printf \"%$foo\t$foo\n\", $ag}"
 done
