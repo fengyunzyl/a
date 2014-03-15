b5f215f806555793b601309fc1ecefbd81bdcaa5
Steven Penny <svnpenn@gmail.com>
2014-03-15 11:14:09 -0500

    Revert "Use gitk on Cygwin without X11"
    This reverts commit 26d3cf5e51a422be497849d1ca250b6829ef9635.

---
+++ scripts/install-gitk.sh
@@ -0,0 +1,22 @@
+# Use gitk on Cygwin without X11
+
+# Tcl
+wget downloads.sf.net/tcl/tcl8.6.0-src.tar.gz
+tar xf tcl8.6.0-src.tar.gz
+cd tcl8.6.0/win
+./configure --host i686-w64-mingw32 --disable-shared
+make -j5 binaries install-libraries TCL_EXE=:
+cd -
+
+# Tk
+wget downloads.sf.net/tcl/tk8.6.0-src.tar.gz
+tar xf tk8.6.0-src.tar.gz
+cd tk8.6.0/win
+./configure --host i686-w64-mingw32 --disable-shared
+make -j5 install-binaries install-libraries
+
+# gitk
+cd /usr/local/bin
+echo 'wish86s /cygwin~$*' > wish
+wget raw.github.com/git/git/master/gitk-git/gitk
+chmod +x gitk
