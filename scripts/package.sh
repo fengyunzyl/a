4873ba425bbb8477d90b3695bbd5bd6832c0e689
Steven Penny <svnpenn@gmail.com>
2014-04-18 15:27:54 -0500

    # check usage of a package by searching local repos

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,34 +1,11 @@
-if (( $# != 2 ))
+# check usage of a package by searching local repos
+
+if (( $# != 1 ))
 then
-  rw=(
-    "${0##*/} MODE REGEX"
-    ""
-    "MODES"
-    "   local     check usage of a package by searching local repos"
-    "   require   print packages that require REGEX"
-  )
-  printf '%s\n' "${rw[@]}"
+  echo ${0##*/} REGEX
+  echo check usage of a package by searching local repos
   exit
 fi
 
-mode=$1
-regex=$2
-
-case $mode in
-local)
-  # commented results ok
-  egrep -r --color --exclude-dir .git "^(|.*[^.\"])$regex" /srv/{a,dotfiles}
-  ;;
-require)
-  cd /usr/local/bin
-  set $(du -d1 | sort -nr | awk 'NR==2 {print $2}')
-  awk '
-  /^@ / {
-    foo=$2
-  }
-  $0 ~ "^requires:.*"bar {
-    print foo
-  }
-  ' bar=$regex $1/x86_64/setup.ini
-  ;;
-esac
+# commented results ok
+egrep -r --color --exclude-dir .git "^(|.*[^.\"])$1" /srv/{a,dotfiles}
