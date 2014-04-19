333dbf6212753d90d825ba1c37efcda6aa1a3f0b
Steven Penny <svnpenn@gmail.com>
2014-04-19 18:03:54 -0500

    # site:musicbrainz.org acoustid -site:forums.musicbrainz.org

--- ff-song.sh
+++ ff-song.sh
@@ -1,4 +1,12 @@
 # create high quality video from song and picture
+# site:musicbrainz.org acoustid -site:forums.musicbrainz.org
+: '
+blog.musicbrainz.org/2013/03/21/
+puids-are-deprecated-and-will-be-removed-on-15-october-2013
+
+blog.musicbrainz.org/2013/09/03/
+changes-for-upcoming-schema-change-release-2013-10-14
+'
 
 JQ () {
   jq -r "$@" .json | sed 's/\r//'
