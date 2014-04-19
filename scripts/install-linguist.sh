f9d5991f7d2dfa513645c3d93fd77a5bfe6066f6
Steven Penny <svnpenn@gmail.com>
2014-04-19 17:40:26 -0500

    set gcc-core gcc-g++ libcrypt-devel libicu-devel patch ruby zlib-devel

--- scripts/install-linguist.sh
+++ scripts/install-linguist.sh
@@ -1,20 +1,20 @@
 # packages
-pacman -S gcc icu-devel libcrypt-devel patch ruby zlib-devel
+set gcc-core gcc-g++ libcrypt-devel libicu-devel patch ruby zlib-devel
+apt-cyg install $*
 
 # faster_require
 gem i faster_require
+cd $(find /lib -type d -name 'faster_require*')
+curl -L github.com/rdp/faster_require/commit/dd680bb.diff | git apply
 sed -i "1crequire '$(gem w faster_require)'" $(gem w rubygems)
+cd -
 
 # charlock_holmes
 git clone --single-branch git://github.com/brianmario/charlock_holmes
 cd charlock_holmes
-# github.com/brianmario/charlock_holmes/issues/32
-sed -i '51s/^/have_library "icuuc"/' ext/charlock_holmes/extconf.rb
 gem b charlock_holmes.gemspec
-echo ac_cv_build=x86_64-unknown-cygwin > ~/config.site
-export CONFIG_SITE=~/config.site
 gem i charlock_holmes
 
 # linguist
 gem i github-linguist
-linguist /lib/ruby/2.0.0/csv.rb
+linguist $(find /lib -name csv.rb)
