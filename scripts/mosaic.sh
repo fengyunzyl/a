f95a1dedbff48eae815c7a59003bc37c5fee5d4d
Steven Penny <svnpenn@gmail.com>
2014-04-27 03:44:20 -0500

    if (( $# < 3 ))

--- scripts/mosaic.sh
+++ scripts/mosaic.sh
@@ -2,12 +2,15 @@
 # in some tessellation.
 type magick >/dev/null || exit
 
-if (( $# < 2 ))
+if (( $# < 3 ))
 then
-  echo ${0##*/} [-shave WxH] WIDTHS FILES
+  echo ${0##*/} [-shave WxH] WIDTHS GRAVITIES FILES
   echo
-  echo 'WIDTHS  comma separated list of output image widths'
-  echo '        example  640,1280,960,960'
+  echo 'WIDTHS     comma separated list of output widths'
+  echo '           example  640,1280,960,960'
+  echo
+  echo 'GRAVITIES  comma separated list of output gravities'
+  echo '           example  center,center,north,east'
   exit
 fi
 
@@ -20,12 +23,14 @@ fi
 # crop images
 wd=(${1//,/ })
 shift
+gv=(${1//,/ })
+shift
 ia=("$@")
 
 for ((o = 0; o < $#; o++))
 do
   magick "${ia[o]}" $shave \
-    -resize ${wd[o]}x1080^ -gravity north \
+    -resize ${wd[o]}x1080^ -gravity ${gv[o]} \
     -extent ${wd[o]}x1080 outfile-$o.png
 done
 
