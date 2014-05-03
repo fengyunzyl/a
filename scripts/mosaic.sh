0a830243e49f81f3b742960d53cadb9b69d7810f
Steven Penny <svnpenn@gmail.com>
2014-05-03 13:36:05 -0500

    if (( ! $# ))

--- scripts/mosaic.sh
+++ scripts/mosaic.sh
@@ -2,31 +2,50 @@
 # in some tessellation.
 type magick >/dev/null || exit
 
-if (( $# < 3 ))
+if (( ! $# ))
 then
-  echo ${0##*/} [-shave WxH] WIDTHS GRAVITIES FILES
+  echo ${0##*/} [-s shave] [-g gravity] FILES
   echo
-  echo 'WIDTHS     comma separated list of output widths'
-  echo '           example  640,1280,960,960'
+  echo '-s   how much to shave from width and height'
+  echo '     example   10x10'
   echo
-  echo 'GRAVITIES  comma separated list of output gravities'
-  echo '           example  center,center,north,east'
+  echo '-g   comma separated list of output gravities'
+  echo '     example   center,center,north,east'
   exit
 fi
 
-if [[ $1 = -shave ]]
+if [[ $1 = -s ]]
 then
   shave="-shave $2"
   shift 2
 fi
 
-# crop images
-wd=(${1//,/ })
-shift
-gv=(${1//,/ })
-shift
+if [[ $1 = -g ]]
+then
+  gv=(${2//,/ })
+  shift 2
+else
+  gv=(center center center center center center)
+fi
+
 ia=("$@")
 
+case $(identify -format '%[fx:w/h>1]' "${ia[@]}") in
+  0110) wd=(640 1280 640 1280) ;;
+  0110) wd=(640 1280 1280 640) ;;
+  1001) wd=(1280 640 640 1280) ;;
+  1010) wd=(1280 640 1280 640) ;;
+  1111) wd=(960 960 960 960) ;;
+  00001) wd=(640 640 640 640 1280) ;;
+  00010) wd=(640 640 640 1280 640) ;;
+  01000) wd=(640 1280 640 640 640) ;;
+  10000) wd=(1280 640 640 640 640) ;;
+  00011) wd=(640 640 640 960 960) ;;
+  11000) wd=(960 960 640 640 640) ;;
+  000000) wd=(640 640 640 640 640 640) ;;
+esac
+
+# crop images
 for ((o = 0; o < $#; o++))
 do
   magick "${ia[o]}" $shave \
