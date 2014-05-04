70c5a4529bbc1386f7ad9688f46dad5fb4e1ea50
Steven Penny <svnpenn@gmail.com>
2014-05-03 22:07:17 -0500

    # fix audio stream using FFmpeg

--- scripts/ff-audio.sh
+++ scripts/ff-audio.sh
@@ -1,7 +1,10 @@
-# increase video volume
+# fix audio stream using FFmpeg
 
-# detect
+# volume
 ffmpeg -i a.mkv -filter volumedetect -vn -f null -
-
-# increase
 ffmpeg -i a.mkv -c:v copy -af volume=3dB -b:a 384k out.mp4
+
+# pan
+ffmpeg -i *.mp4 -c:v copy -b:a 256k -af 'pan=stereo|\
+  FL < FL + 1.414FC + .5BL + .5SL|\
+  FR < FR + 1.414FC + .5BR + .5SR' pan.mp4
