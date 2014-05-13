9328ee420c900477ef8b3b4b5726a84d17d7ce07
Steven Penny <svnpenn@gmail.com>
2014-05-12 20:54:00 -0500

    # Invalid UTF-8 in decoded subtitles text; maybe missing -sub_charenc option

--- scripts/ff-subs.sh
+++ scripts/ff-subs.sh
@@ -40,3 +40,11 @@ ib=${1%.*}
 
 export -f buffer log say warn
 buffer bf.sh
+
+# Invalid UTF-8 in decoded subtitles text; maybe missing -sub_charenc option
+
+# Character encoding subtitles conversion needs a libavcodec built with iconv
+# support for this codec
+
+# ffmpeg -i *.mkv -sub_charenc asdf -i *.srt -c:v copy -c:a copy \
+#   -c:s mov_text outfile.mp4
