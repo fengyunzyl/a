0c1bf84d5c8beb5d814c25b8d33e873c05c675dc
Steven Penny <svnpenn@gmail.com>
2014-05-12 20:54:21 -0500

    apt-cyg install gcc-core libcrypt-devel libffi-devel pkg-config ruby

--- scripts/install-jekyll.sh
+++ scripts/install-jekyll.sh
@@ -1,12 +1,12 @@
 # packages
-apt-cyg gcc-core libcrypt-devel libffi-devel pkg-config ruby
+apt-cyg install gcc-core libcrypt-devel libffi-devel pkg-config ruby
 
 # faster_require
 gem i faster_require
 sed -i "1crequire '$(gem w faster_require)'" $(gem w rubygems)
 
-# 2.0.0.alpha.1
-gem i jekyll --pr
+# 2.0.3
+gem i jekyll
 
 # rougify style > rouge.css
 gem i rouge
