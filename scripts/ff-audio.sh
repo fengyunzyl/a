6b6619ac0a627f70e748f4dff73f816de25918f1
Steven Penny <svnpenn@gmail.com>
2014-05-13 00:02:09 -0500

    if (( ! $# ))

--- scripts/ff-audio.sh
+++ scripts/ff-audio.sh
@@ -1,10 +1,17 @@
 # fix audio stream using FFmpeg
+if (( ! $# ))
+then
+  echo ${0##*/} FILES
+  exit
+fi
 
-# volume
-ffmpeg -i a.mkv -filter volumedetect -vn -f null -
-ffmpeg -i a.mkv -c:v copy -af volume=3dB -b:a 384k out.mp4
+for each
+do
+  # Apply max noclip gain
+  aacgain -r -k -m 10 "$each"
+  echo
+done
 
-# pan
-ffmpeg -i *.mp4 -c:v copy -b:a 256k -af 'pan=stereo|\
+: ffmpeg -i *.mp4 -c:v copy -b:a 256k -af 'pan=stereo|\
   FL < FL + 1.414FC + .5BL + .5SL|\
   FR < FR + 1.414FC + .5BR + .5SR' pan.mp4
