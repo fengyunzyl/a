9fc433ba46e33e4327bc6cd4552b4544e52d3d84
Steven Penny <svnpenn@gmail.com>
2014-05-19 00:02:19 -0500

    ﻿if ($args.length -ne 3) {

---
+++ misc/torrent.ps1
@@ -0,0 +1,19 @@
+﻿if ($args.length -ne 3) {
+  '{0} SEARCH SORT CATEGORY' -f $MyInvocation.MyCommand.Name
+  ''
+  'SORT'
+  '3  date ↓'
+  '6  size ↑'
+  '7  seeders ↓'
+  ''
+  'CATEGORY'
+  '100  Audio'
+  '104  Audio FLAC'
+  '205  Video TV shows'
+  '207  Video HD Movies'
+  '208  Video HD TV shows'
+  '301  Applications Windows'
+  exit
+}
+
+Start-Process ('http://thepiratebay.se/search/{0}/0/{1}/{2}' -f $args)
