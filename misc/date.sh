4ecab8593ca433119197996374e1529e331c468d
Steven Penny <svnpenn@gmail.com>
2014-06-01 10:46:18 -0500

    sq=(%H%M%S '%b %-d %Y'

--- misc/date.sh
+++ misc/date.sh
@@ -1,4 +1,4 @@
-sq=(%H%M%S '%b %d %Y'
+sq=(%H%M%S '%b %-d %Y'
     %a %b %c %d %e    %g %h    %j %k %l %m    %p %r %s    %u    %w %x %y %z
     %A %B %C %D    %F %G %H %I          %M %N %P %R %S %T %U %V %W %X %Y %Z
     %:z %::z %:::z)
