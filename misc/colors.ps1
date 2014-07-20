bca49ea814aa7e691bb53441803c8b4f35d86667
Steven Penny <svnpenn@gmail.com>
2014-07-20 05:18:42 -0500

    # black is invisible

---
+++ misc/colors.ps1
@@ -0,0 +1,19 @@
+# black is invisible
+
+'blue',
+'cyan',
+'darkblue',
+'darkcyan',
+'darkgray',
+'darkgreen',
+'darkmagenta',
+'darkred',
+'darkyellow',
+'gray',
+'green',
+'magenta',
+'red',
+'white',
+'yellow' | % {
+  Write-Host -ForegroundColor $_ $_
+}
