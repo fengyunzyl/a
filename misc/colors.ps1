764a2b2ddd2c14c173b9c44de6bcfb22ca841c48
Steven Penny <svnpenn@gmail.com>
2014-07-23 02:57:56 -0500

    & {

--- misc/colors.ps1
+++ misc/colors.ps1
@@ -1,19 +1,20 @@
 # black is invisible
-
-'blue',
-'cyan',
-'darkblue',
-'darkcyan',
-'darkgray',
-'darkgreen',
-'darkmagenta',
-'darkred',
-'darkyellow',
-'gray',
-'green',
-'magenta',
-'red',
-'white',
-'yellow' | % {
+& {
+  'blue'
+  'cyan'
+  'darkblue'
+  'darkcyan'
+  'darkgray'
+  'darkgreen'
+  'darkmagenta'
+  'darkred'
+  'darkyellow'
+  'gray'
+  'green'
+  'magenta'
+  'red'
+  'white'
+  'yellow'
+} | % {
   Write-Host -ForegroundColor $_ $_
 }
