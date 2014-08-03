7c39bcf059f34293374884946351b7040f984a15
Steven Penny <svnpenn@gmail.com>
2014-08-03 01:29:57 -0500

    function hr {

--- misc/torrent.sh
+++ misc/torrent.sh
@@ -1,24 +1,65 @@
+function hr {
+  sed '
+  1d
+  $d
+  s/  //
+  ' <<< "$1"
+}
+
 if (( $# != 3 ))
 then
-  rw=(
-    "${0##*/} SEARCH SORT CATEGORY"
-    ''
-    SORT
-    '3  date ↓'
-    '6  size ↑'
-    '7  seeders ↓'
-    ''
-    CATEGORY
-    '100  Audio'
-    '104  Audio FLAC'
-    '201  Video Movies'
-    '205  Video TV shows'
-    '207  Video HD Movies'
-    '208  Video HD TV shows'
-    '301  Applications Windows'
-  )
-  printf '%s\n' "${rw[@]}"
+  hr "
+  ${0##*/} SEARCH SORT CATEGORY
+
+  SORT
+  3  date ↓
+  6  size ↑
+  7  seeders ↓
+
+  CATEGORY
+  100  Audio
+  104  Audio FLAC
+  201  Video Movies
+  205  Video TV shows
+  207  Video HD Movies
+  208  Video HD TV shows
+  301  Applications Windows
+  "
   exit
 fi
 
-cygstart "http://thepiratebay.se/search/$1/0/$2/$3"
+sc=$1
+sr=$2
+cg=$3
+
+if (( cg != 207 ))
+then
+  cygstart "http://thepiratebay.se/search/$sc/0/$sr/$cg"
+  exit
+fi
+
+cd /tmp
+rm -f *.htm
+lower=$(awk 'BEGIN {print 1.5 * 1024^3}')
+upper=$(awk 'BEGIN {print 3 * 1024^3}')
+
+curl "http://thepiratebay.se/search/$sc/0/$sr/$cg" |
+awk '$2 == "torrent" {print $3}' FS=/ |
+while read each
+do
+  curl -o $each.htm http://thepiratebay.se/torrent/$each
+done
+
+for each in *.htm
+do
+  # check size
+  sz=$(awk '/Bytes/ {print $NF}' FPAT=[[:digit:]]+ $each)
+  (( sz < lower )) && continue
+  (( sz > upper )) && continue
+  # check kbps
+  grep -iq 'kb[p/]s' $each || continue
+  # check seeders
+  sd=$(awk '/Seeders/ {print RT}' RS=[[:digit:]]+ $each)
+  (( sd < 2 )) && continue
+  cygstart http://thepiratebay.se/torrent/$each
+done
