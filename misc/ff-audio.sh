1085a0a47097d45ebbcfbe0799cbfd96095256cf
Steven Penny <svnpenn@gmail.com>
2014-08-10 13:25:12 -0500

    function hr {

--- misc/ff-audio.sh
+++ misc/ff-audio.sh
@@ -1,13 +1,31 @@
 # fix audio stream using FFmpeg
+function hr {
+  sed '
+  1d
+  $d
+  s/  //
+  ' <<< "$1"
+}
+
 if (( ! $# ))
 then
-  echo ${0##*/} FILES
+  hr "
+  ${0##*/} FILES
+
+  Apply max noclip gain
+  "
   exit
 fi
 
 for each
 do
-  # Apply max noclip gain
-  aacgain -k -r -s s -m 10 "$each"
+  case ${each: -3} in
+  m4a)
+    aacgain -k -r -s s -m 10 "$each"
+  ;;
+  mp3)
+    mp3gain -k -r -s s -m 10 "$each"
+  ;;
+  esac
   echo
 done
