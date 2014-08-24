6ca79e306d8abe6bebb9413ff7034fa3fe63edc4
Steven Penny <svnpenn@gmail.com>
2014-08-24 18:56:40 -0500

    function hr {

--- misc/screen-buffer.sh
+++ misc/screen-buffer.sh
@@ -1,20 +1,28 @@
+function hr {
+  sed '
+  1d
+  $d
+  s/  //
+  ' <<< "$1"
+}
+
+function hx {
+  printf 0x%04x%04x $*
+}
+
 if (( $# != 2 ))
 then
-  echo ${0##*/} ROWS COLUMNS
-  echo max columns is 184
-  powershell '
-  $0 = (gp hkcu:console).ScreenBufferSize
-  "current buffer rows {0}" -f ($0 -shr 16)
-  "current buffer columns {0}" -f ($0 -band 0xffff)
-  '
+  set $(regtool get /user/console/ScreenBufferSize)
+  hr "
+  ${0##*/} ROWS COLUMNS
+  max columns is 184
+  current buffer rows    $(( $1 >> 0x0010 ))
+  current buffer columns $(( $1  & 0xFFFF ))
+  "
   exit
 fi
 
-powershell '&{
-param($rw,$cm)
-sp hkcu:console ScreenBufferSize ("0x{0:x}{1:x4}" -f $rw,$cm)
-sp hkcu:console WindowSize       ("0x{0:x}{1:x4}" -f  25,$cm)
-}' $*
-
+regtool set /user/console/ScreenBufferSize $(hx $1 $2)
+regtool set /user/console/WindowSize       $(hx 25 $2)
 cygstart bash
 kill -7 $PPID
