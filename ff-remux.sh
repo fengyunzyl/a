f83d407c99cc2a37b2b8b7c7ac9ea694e8b17275
Steven Penny <svnpenn@gmail.com>
2014-08-24 19:17:50 -0500

    function hx {

--- ff-remux.sh
+++ ff-remux.sh
@@ -10,14 +10,24 @@ function log {
   "$@"
 }
 
-function buffer {
-  powershell '&{
-  param($cm)
-  sp hkcu:console ScreenBufferSize ("0x{0:x}{1:x4}" -f 2000,$cm)
-  sp hkcu:console WindowSize       ("0x{0:x}{1:x4}" -f   25,$cm)
-  }' $(( ${#1} ? 88 : 80 ))
-  cygstart bash $1
+function hx {
+  printf 0x%04x%04x $*
+}
+
+function bf {
+  regtool set /user/console/ScreenBufferSize $(hx 2000 $1)
+  regtool set /user/console/WindowSize       $(hx   25 $1)
+  cygstart bash $2
   kill -7 $$ $PPID
+}
+
+function ug {
+  echo ${0##*/} [-c choice] [-t title] [-a artist] [files]
+  for each in "${ga[@]}"
+  do
+    printf '%2s  %s\n' $((++bar)) "$each"
+  done
+  exit
 }
 
 ga=(
@@ -30,42 +40,45 @@ ga=(
   'ffmpeg -i %q -c copy -vn %q.m4a'
   'ffmpeg -i %q -c copy -vn %q.mp3'
   'ffmpeg -i %q -c copy -movflags faststart %q.m4a'
-  'ffmpeg -i %q -c copy -vn -movflags faststart -metadata title=%q %q.m4a'
+  'ffmpeg -i %q -c copy -vn -movflags faststart -metadata title=%q \
+    -metadata artist=%q %q.m4a'
   'ffmpeg -i %q -b:a 256k -movflags faststart -metadata title=%q %q.m4a'
   'ffmpeg -i %q -vn -b:a 256k -movflags faststart %q.m4a'
   'ffmpeg -i %q -c:v copy -b:a 256k -ac 2 -clev 3dB -slev -6dB %q.mp4'
   'ffmpeg -i %q -b:a 256k -ac 2 -clev 3dB -slev -6dB %q.mp4'
 )
 
-if (( $# < 2 )) || ! seq ${#ga[*]} | grep -qx "$1"
-then
-  echo ${0##*/} CHOICE FILES
-  echo
-  echo this will not delete original file
-  echo
-  for each in "${ga[@]}"
-  do
-    printf '%2s  %s\n' $((++bar)) "$each"
-  done
-  exit
-fi
+while getopts c:t:a: name
+do
+  case $name in
+  c) ci=$OPTARG ;;
+  t) te=$OPTARG ;;
+  a) ai=$OPTARG ;;
+  esac
+done
+shift $((--OPTIND))
 
-choice=$1
-shift
-pm=("$@")
-up=${ga[choice-1]}
+(( $# )) || ug
+seq ${#ga[*]} | grep -qx "$ci" || ug
+up=${ga[ci-1]}
+[[ $up =~ title ]] && [[ ! $te ]] && ug
+[[ $up =~ artist ]] && [[ ! $ai ]] && ug
 
-for ip in "${pm[@]}"
+for ip
 do
   ib=${ip%.*}
   ie=${ip##*.}
   oe=${up##*.}
   am[0]=$ip
-  if [[ $up =~ metadata ]]
+  if [[ $up =~ title ]]
   then
-    am[1]=$(sed 's/-/ /g;s/  */ /g' <<< "$ib")
+    am[1]=$te
   fi
-  am[2]=$ib
+  if [[ $up =~ artist ]]
+  then
+    am[2]=$ai
+  fi
+  am[3]=$ib
   [[ $oe = $ie ]] && am[2]+='~'
   printf -v stage2 "$up" "${am[@]}"
   (( oc++ )) && ao+=("echo")
@@ -74,7 +87,7 @@ done
 ao+=("warn Press any key to continue . . .")
 ao+=("read")
 ao+=("rm rx.sh")
-ao+=("buffer")
+ao+=("bf 80")
 printf '%s\n' "${ao[@]}" > rx.sh
-export -f buffer log warn
-buffer rx.sh
+export -f bf hx log warn
+bf 88 rx.sh
