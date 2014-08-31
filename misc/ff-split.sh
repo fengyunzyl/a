9de8947a3ccc5602de8de3179a6b144a511e28b0
Steven Penny <svnpenn@gmail.com>
2014-08-31 04:19:57 -0500

    function warn {

--- misc/ff-split.sh
+++ misc/ff-split.sh
@@ -1,30 +1,30 @@
 # split album flac file
 
-usage () {
-  echo usage: $0 CUE AUDIO
-  exit
-}
-
-warn () {
+function warn {
   printf '\e[36m%s\e[m\n' "$*"
 }
 
-log () {
+function log {
   unset PS4
-  qq=$(( set -x
-         : "$@" )2>&1)
-  warn "${qq:2}"
-  eval "${qq:2}"
+  sx=$((set -x
+    : "$@") 2>&1)
+  warn "${sx:2}"
+  "$@"
 }
 
-(( $# < 2 )) && usage
+if (( $# != 2 ))
+then
+  echo ff-split.sh CUE AUDIO
+  exit
+fi
+
 arg_cue=$1
 arg_aud=$2
 
 # first we need to remux to wav
 otf=${arg_aud%.*}.wav
 log ffmpeg -i "$arg_aud" -v warning -stats "$otf"
-printf '\n'
+echo
 
 # fpcalc cannot read files with commas, good game
 log shntool split -f "$arg_cue" -t %n-%t -m ' -&-(-)-,-/-;-' "$otf"
