cdd818a251985c002cee0d82aa6c747982fa2040
Steven Penny <svnpenn@gmail.com>
2014-09-04 22:45:46 -0500

    function hr {

---
+++ misc/musicbrainz.sh
@@ -0,0 +1,105 @@
+function hr {
+  sed '
+  1d
+  $d
+  s/  //
+  ' <<< "$1"
+}
+
+function bw {
+  case $OSTYPE in
+  linux-gnu) xdg-open "$1" ;;
+  cygwin)    cygstart "$1" ;;
+  esac
+}
+
+function jn {
+  # parse json
+  awk '$1 ~ key {print $2}' RS='([{}]|"?, ?")' FS='": ?"?' key="$1" "$2"
+}
+
+function proxy {
+  local msg url dt pool px cn
+  msg=$1
+  url=$2
+  set --
+  dt=proxy.txt
+  printf 'request %s... ' "$msg" >&2
+  while :
+  do
+    if (( ! $# ))
+    then
+      touch $dt
+      mapfile -t pool < $dt
+      set -- "${pool[@]}"
+    fi
+    if (( ! $# ))
+    then
+      wget -q -O $dt txt.proxyspy.net/$dt
+    fi
+    read px cn <<< $1
+    if [[ ! $cn =~ US ]]
+    then
+      shift
+      continue
+    fi
+    if ! wget -q -T 1 -t 1 -O web.json -e http_proxy=$px "$url"
+    then
+      shift
+      continue
+    fi
+    if jn responseStatus web.json | grep -q 403
+    then
+      shift
+      continue
+    else
+      break
+    fi
+  done
+  printf '%s\n' "$px" >&2
+  printf '%s\n' "$@" > $dt
+}
+
+function jo {
+  jq -r "$@" | sed 's.\r..'
+}
+
+case "$1" in
+'')
+  hr '
+  musicbrainz.sh date-get <album>  <date>
+  musicbrainz.sh  img-get <artist> <album>
+  musicbrainz.sh  img-set <image>
+  
+  date must be this format: 1982-12
+  '
+  exit
+;;
+img-get)
+  artist=$2
+  album=$3
+  bw 'http://google.com/search?tbm=isch&q='"$artist $album"
+  bw 'http://fanart.tv/api/getdata.php?type=2&s='"$artist"
+  bw 'http://discogs.com/search?q='"$artist $album"
+  bw 'http://wikipedia.org/w/index.php?search='"${artist// /+}+${album// /+}"
+  bw 'http://musicbrainz.org/search?type=release&query='"$artist $album"
+;;
+img-set)
+  magick "$2" -resize x1000 -compress lossless 1000-"$2"
+;;
+date-get)
+  album=$2
+  date=$3
+  cd /tmp
+  seq -f %02g 1 31 |
+  while read each
+  do
+    datefull="$date-$each"
+    printf -v qs 'v=1.0&q="%s" %s' "$album" "$datefull"
+    proxy "$datefull" "ajax.googleapis.com/ajax/services/search/web?$qs"
+    count=$(jo .responseData.cursor.resultCount web.json)
+    printf '%s\t%s\n' $count "$datefull"
+  done |
+  sort -nr
+;;
+esac
