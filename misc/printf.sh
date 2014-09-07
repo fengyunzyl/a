60f875fb6e23cf699d03927af439792f86e04135
Steven Penny <svnpenn@gmail.com>
2014-09-07 17:15:28 -0500

    function hr {

--- misc/printf.sh
+++ misc/printf.sh
@@ -1,13 +1,26 @@
+function hr {
+  sed '
+  1d
+  $d
+  s/  //
+  ' <<< "$1"
+}
+
 if (( $# != 2 ))
 then
-  echo "${0##*/} <E|S> INPUT"
-  echo "E expression"
-  echo "S string"
+  hr '
+  printf.sh <E|S> INPUT
+  E expression
+  S string
+  '
   exit
 fi
 
 ag=$2
-[ $1 = S ] && ag=\"$2\"
+if [[ $1 = S ]]
+then
+  printf -v ag '"%s"' "$2"
+fi
 
 bar=(
   %a %b %d %e %.7f %g %h %i %j %k %l %m %n %o %p %q %r %s %t %u %v %w %x %y %z
