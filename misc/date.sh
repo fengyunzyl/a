f0e4c323a69cf338e8540df05ae7a3b2a8aadfe8
Steven Penny <svnpenn@gmail.com>
2014-11-04 21:29:02 -0600

    if (( $# != 1 ))

--- misc/date.sh
+++ misc/date.sh
@@ -1,3 +1,9 @@
+if (( $# != 1 ))
+then
+  echo date.sh DATE
+  exit
+fi
+
 sq=(%H%M%S '%b %-d %Y'
     %a %b %c %d %e    %g %h    %j %k %l %m    %p %r %s    %u    %w %x %y %z
     %A %B %C %D    %F %G %H %I          %M %N %P %R %S %T %U %V %W %X %Y %Z
@@ -6,5 +12,5 @@ sq=(%H%M%S '%b %-d %Y'
 for each in "${sq[@]}"
 do
   printf '%-11s' "$each"
-  date +"$each"
+  date -d "$1" +"$each"
 done
