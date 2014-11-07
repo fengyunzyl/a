054f18ecac2d515ba25ed5dd373136f661c371a0
Steven Penny <svnpenn@gmail.com>
2014-11-06 19:29:10 -0600

    %a %b %d %e %.0f %.7f

--- misc/printf.sh
+++ misc/printf.sh
@@ -23,7 +23,8 @@ then
 fi
 
 bar=(
-  %a %b %d %e %.7f %g %h %i %j %k %l %m %n %o %p %q %r %s %t %u %v %w %x %y %z
+  %a %b %d %e %.0f %.7f
+  %g %h %i %j %k %l %m %n %o %p %q %r %s %t %u %v %w %x %y %z
 )
 
 for foo in ${bar[*]}
