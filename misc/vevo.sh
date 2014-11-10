4f9902a9bfd7c0658b27337229cb3b90a51ec079
Steven Penny <svnpenn@gmail.com>
2014-11-10 13:46:52 -0600

    # get vevo highest quality

--- misc/vevo.sh
+++
@@ -1,6 +0,0 @@
-# get vevo highest quality
-
-rtmpdump \
--r rtmp://vevoodfs.fplive.net/vevood \
--y mp4:/Video/V2/SMIL/USRV81300282/usrv81300282_4392k_1920x1080_x264_4200_quicktime_192.mp4 \
--o a.flv
