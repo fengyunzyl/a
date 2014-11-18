24834c10f6900119404d257c8db2f3a5b56db793
Steven Penny <svnpenn@gmail.com>
2014-11-17 18:21:28 -0600

    for ts in a b c d e f g h L k p r s S t u w x O G N

--- misc/test.sh
+++ misc/test.sh
@@ -4,12 +4,24 @@ then
   exit
 fi
 
-for ts in a b c d e f g k p r s S t u w x G N
+for ts in a b c d e f g h L k p r s S t u w x O G N
 do
-  printf '%s  ' $ts
+  printf '  -%s  ' $ts
   for each
   do
     if [ -$ts "$each" ]
+    then
+      printf T
+    else
+      printf F
+    fi
+    printf '  '
+  done
+  echo
+  printf '! -%s  ' $ts
+  for each
+  do
+    if [ ! -$ts "$each" ] 2>/dev/null
     then
       printf T
     else
