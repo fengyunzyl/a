df5f95e9870263dc94c134eb77f37f45305f64ac
Steven Penny <svnpenn@gmail.com>
2015-04-18 13:18:32 -0500

    #!/bin/sh

---
+++ misc/reddit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ $# != 1 ]
+then
+  echo reddit.sh SUBREDDIT
+  exit
+fi
+
+lynx -dump -listonly -nonumbers reddit.com/r/$1 |
+sed 0,/Hidden/d |
+youtube-dl --format bestaudio --download-archive archive.txt --batch-file -
