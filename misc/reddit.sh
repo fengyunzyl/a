cb24823862e8906b9439a6a83c0148f68925ed54
Steven Penny <svnpenn@gmail.com>
2015-04-29 23:13:05 -0500

    if [ $# = 0 ]

--- misc/reddit.sh
+++ misc/reddit.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
-if [ $# != 1 ]
+if [ $# = 0 ]
 then
-  echo reddit.sh URL
+  echo reddit.sh URLS
   exit
 fi
 
-lynx -dump -listonly -nonumbers "$1" |
-sed 0,/Hidden/d |
-youtube-dl --format bestaudio --download-archive %.txt --batch-file -
+# download
+lynx -dump -listonly -nonumbers "$@" |
+awk '/Hidden/ {$1=""; print}' RS='\n\n' FS='\n' OFS='\n' |
+youtube-dl --format bestaudio --download-archive %.txt \
+--output '%(title)s.%(ext)s' --batch-file -
+
+# faststart
+for each in *.m4a
+do
+  ffmpeg -hide_banner -i "$each" -c copy -movflags faststart outfile.m4a
+  mv outfile.m4a "$each"
+done
+
+# gain
+for each in *.m4a *.mp3
+do
+  case "${each: -3}" in
+    m4a) gain=aacgain ;;
+    mp3) gain=mp3gain ;;
+  esac
+  "$gain" -k -r -s s -m 10 "$each"
+  echo
+done
