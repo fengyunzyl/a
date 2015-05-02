5ce94d3e23791f4587f47d71892c4746a1907d70
Steven Penny <svnpenn@gmail.com>
2015-05-02 18:24:56 -0500

    youtube-dl --format bestaudio --download-archive %-download.txt \

--- misc/reddit.sh
+++ misc/reddit.sh
@@ -8,23 +8,25 @@ fi
 # download
 lynx -dump -listonly -nonumbers "$@" |
 awk '/Hidden/ {$1=""; print}' RS='\n\n' FS='\n' OFS='\n' |
-youtube-dl --format bestaudio --download-archive %.txt \
+youtube-dl --format bestaudio --download-archive %-download.txt \
 --output '%(title)s.%(ext)s' --batch-file -
 
-# faststart
-for each in *.m4a
-do
-  ffmpeg -hide_banner -i "$each" -c copy -movflags faststart outfile.m4a
-  mv outfile.m4a "$each"
-done
-
-# gain
 for each in *.m4a *.mp3
 do
-  case "${each: -3}" in
-    m4a) gain=aacgain ;;
-    mp3) gain=mp3gain ;;
-  esac
-  "$gain" -k -r -s s -m 10 "$each"
-  echo
+  if fgrep "$each" %-fixup.txt
+  then
+    continue
+  fi
+
+  # gain
+  aacgain -k -r -s s -m 10 "$each"
+
+  # faststart
+  if file --brief --mime-type "$each" | grep audio/x-m4a
+  then
+    ffmpeg -hide_banner -i "$each" -c copy -movflags faststart outfile.m4a
+    mv outfile.m4a "$each"
+  fi
+
+  echo "$each" >> %-fixup.txt
 done
