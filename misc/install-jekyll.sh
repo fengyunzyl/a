d9a481b8518fbc9ae777edd6317e5947ebe479eb
Steven Penny <svnpenn@gmail.com>
2015-05-01 23:16:55 -0500

    #!/bin/sh

--- misc/install-jekyll.sh
+++ misc/install-jekyll.sh
@@ -1,11 +1,12 @@
+#!/bin/sh
 # packages
-apt-cyg install gcc-core libcrypt-devel libffi-devel pkg-config ruby
+apt-cyg install gcc-core libcrypt-devel libffi-devel make ruby
 
 # faster_require
 gem i faster_require
 sed -i "1crequire '$(gem w faster_require)'" $(gem w rubygems)
 
-# 2.0.3
+# jekyll
 gem i jekyll
 
 # rougify style > rouge.css
