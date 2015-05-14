9524a901324ab853559710175231a0f8e4c31832
Steven Penny <svnpenn@gmail.com>
2015-05-14 00:46:46 -0500

    mapfile -t usage <<+

--- misc/reddit.sh
+++ misc/reddit.sh
@@ -1,19 +1,52 @@
 #!/bin/sh
+mapfile -t usage <<+
+NAME
+  reddit.sh
+
+SYNOPSIS
+  reddit.sh [URLs]
+
+EXAMPLE URLs
+  http://youtube.com/watch?v=qjgnOP8f5NU
+  http://soundcloud.com/greco-roman/roosevelt-night-moves
+  http://reddit.com/r/stationalpha/new
++
+
+function pa {
+  printf '%s\n' "$@"
+}
+
+function bk {
+  awk '
+  BEGIN {
+    OFS = "░"
+    NF = 80
+    printf "\033[1;35m%s\033[m\n", $0
+  }
+  '
+}
+
 if [ $# = 0 ]
 then
-  echo reddit.sh URLS
+  pa "${usage[@]}"
   exit
 fi
 
-touch %-alpha.txt
+{
+  pa "$@" |
+    grep -v reddit
+  pa "$@" |
+    grep reddit |
+    lynx -dump -listonly -nonumbers - '' |
+    awk '/Hidden/ {z=1; next} ! $0 {z=0} z'
+} > /tmp/1.txt
 
-# download
-lynx -dump -listonly -nonumbers "$@" |
-awk '/Hidden/ {z=1; next} ! $0 {z=0} z' |
+touch %.txt
+
 while read golf
 do
-  let charlie++ && echo
-  kilo=$(awk '$1==golf {print $2}' FS='\t' golf="$golf" %-alpha.txt)
+  let bravo++ && bk
+  kilo=$(awk '$1==golf {print $2}' FS='\t' golf="$golf" %.txt)
   if [ "$kilo" ]
   then
     printf '%s\nhas already been recorded in archive\n' "$kilo"
@@ -21,20 +54,24 @@ do
   fi
 
   # download
-  youtube-dl --add-metadata --format m4a/mp3 --output '%(title)s.%(ext)s' \
-    --youtube-skip-dash-manifest "$golf" | tee %-bravo.txt
-  kilo=$(awk '/Destination/ {print $2}' FS=': ' %-bravo.txt)
-
-  # gain
-  aacgain -k -r -s s -m 10 "$kilo"
+  youtube-dl --add-metadata --format m4a/mp3 --youtube-skip-dash-manifest \
+    --output '%(upload_date)s %(title)s.%(ext)s' "$golf" |
+    iconv -f cp1252 | tee /tmp/2.txt
+  kilo=$(awk '/Destination/ {print $2}' FS=': ' /tmp/2.txt)
+  lima="${kilo/???? / }"
+  mv "$kilo" "$lima"
 
   # faststart
-  if file --brief --mime-type "$kilo" | grep -q audio/x-m4a
+  if [ "${lima##*.}" = m4a ]
   then
-    ffmpeg -nostdin -hide_banner -v warning -i "$kilo" -c copy \
-      -movflags faststart outfile.m4a
-    mv outfile.m4a "$kilo"
+    echo '[ffmpeg] moving the moov atom to the beginning of the file'
+    ffmpeg -nostdin -v warning -i "$lima" -c copy -movflags faststart \
+      -flags global_header outfile.m4a
+    mv outfile.m4a "$lima"
   fi
 
-  printf '%s\t%s\n' "$golf" "$kilo" >> %-alpha.txt
-done
+  # gain
+  aacgain -k -r -s s -m 10 "$lima"
+
+  printf '%s\t%s\n' "$golf" "$lima" >> %.txt
+done < /tmp/1.txt
