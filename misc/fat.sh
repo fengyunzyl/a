beb6dc1c5b6d65c9bf2f41680035850fed36df5a
Steven Penny <svnpenn@gmail.com>
2015-05-14 23:22:58 -0500

    new file: misc/fat.sh

---
+++ misc/fat.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+mapfile -t usage <<+
+NAME
+  fat.sh
+
+SYNOPSIS
+  fat.sh [drive]
+
+EXAMPLE
+  fat.sh E:
++
+
+function pa {
+  printf '%s\n' "$@"
+}
+
+if [ $# != 1 ]
+then
+  pa "${usage[@]}"
+  exit
+fi
+
+cd /tmp
+yafs -r -d "$1" -f 1.xml
+
+# FIXME support top level files too, not just directories
+awk '
+{
+  z[NR] = $0
+}
+/^...short_name/ {
+  y[$0]
+}
+END {
+  PROCINFO["sorted_in"] = "@ind_num_asc"
+  for (x in y)
+    y[x] = ++w
+  for (x in z)
+    if (z[x] ~ /^..file/)
+      printf "\t<file order=\"0\" volume=\"true\">\n"
+    else if (z[x] ~ /^..directory/)
+      printf "\t<directory order=\"%d\">\n", y[z[x+1]]
+    else
+      print z[x]
+}
+' 1.xml > 2.xml
+
+yafs -w -d "$1" -f 2.xml
