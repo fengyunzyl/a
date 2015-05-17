988241bfb508b0863cc67dcab3560d6bb3693bfa
Steven Penny <svnpenn@gmail.com>
2015-05-17 16:42:56 -0500

    bra=$(date -d '-1 year' +%Y%m%d)

--- misc/reddit.sh
+++ misc/reddit.sh
@@ -38,6 +38,7 @@ then
   exit
 fi
 
+bra=$(date -d '-1 year' +%Y%m%d)
 mkdir -p %-new %-old
 touch %-new/%.txt
 set -o igncr
@@ -51,50 +52,81 @@ cd /tmp
     lynx -dump -listonly -nonumbers - '' |
     awk '/Hidden/ {z=1; next} ! $0 {z=0} z'
 } |
-while read bra
+while read wu
 do
   let char++
   if [ $char -ge 2 ]
   then
     bk starting link $char
   fi
-  del=$(awk '$1==bra {print $2}' FS='\t' bra="$bra" ~-/%-new/%.txt)
-  if [ "$del" ]
+  awk '
+  func z(y) {
+    return gensub(/\W/, "\\\\&", "g", y)
+  }
+  $1 == wu {
+    print "upload_date=" z($2), "source=" z($3), "_filename=" z($4)
+  }
+  ' FS='\t' wu="$wu" ~-/%-new/%.txt > vars.sh
+  if [ -s vars.sh ]
   then
-    printf '%s\nhas already been recorded in archive\n' "$del"
-    continue
-  fi
-
-  # download
-  rm -f *.info.json
-  youtube-dl --add-metadata --format m4a/mp3 --output '%(title)s.%(ext)s' \
-    --write-info-json --youtube-skip-dash-manifest "$bra"
-  jq -r '
-  {upload_date, title, _filename, ext} |
-  to_entries |
-  map("\(.key)=\(.value | @sh)") |
-  .[]
-  ' *.info.json > info.sh
-  . info.sh
-
-  # faststart
-  if [ $ext = m4a ]
-  then
-    echo '[ffmpeg] moving the moov atom to the beginning of the file'
-    ffmpeg -nostdin -v warning -i "$_filename" -c copy -movflags faststart \
-      -flags global_header temp.m4a
-    mv temp.m4a "$_filename"
-  fi
-
-  # gain
-  aacgain -k -r -s s -m 10 "$_filename"
-
-  fox=$(date -d '-1 year' +%Y%m%d)
-  if [ $upload_date -gt $fox ]
-  then
-    mv "$_filename" ~-/%-new
+    . vars.sh
+    printf '%s\nhas already been recorded in archive\n' "$_filename"
+    if [ ! -e ~-/%-new/"$_filename" ]
+    then
+      continue
+    fi
+    if [ $upload_date -gt $bra ]
+    then
+      dest=1
+    else
+      dest=0
+    fi
+    if [ $source != $dest ]
+    then
+      echo '[mv] file is now old, moving'
+      mv ~-/%-new/"$_filename" ~-/%-old
+      awk -i inplace '
+      $1 == wu {
+        $3 = 0
+      }
+      1
+      ' FS='\t' OFS='\t' wu="$wu" ~-/%-new/%.txt
+    fi
   else
-    mv "$_filename" ~-/%-old
+    # download
+    rm -f *.info.json
+    youtube-dl --add-metadata --format m4a/mp3 --output '%(title)s.%(ext)s' \
+      --write-info-json --youtube-skip-dash-manifest "$wu"
+    jq -r '
+    {upload_date, _filename, ext} |
+    to_entries |
+    map("\(.key)=\(.value | @sh)") |
+    .[]
+    ' *.info.json > vars.sh
+    . vars.sh
+
+    # faststart
+    if [ $ext = m4a ]
+    then
+      echo '[ffmpeg] moving the moov atom to the beginning of the file'
+      ffmpeg -nostdin -v warning -i "$_filename" -c copy -movflags faststart \
+        -flags global_header temp.m4a
+      mv temp.m4a "$_filename"
+    fi
+
+    # gain
+    aacgain -k -r -s s -m 10 "$_filename"
+
+    if [ $upload_date -gt $bra ]
+    then
+      dest=1
+      delta=%-new
+    else
+      dest=0
+      delta=%-old
+    fi
+    mv "$_filename" ~-/"$delta"
+    set "$wu" "$upload_date" "$dest" "$_filename"
+    printf '%s\t%s\t%s\t%s\n' "$@" >> ~-/%-new/%.txt
   fi
-  printf '%s\t%s\n' "$bra" "$title" >> ~-/%-new/%.txt
 done
