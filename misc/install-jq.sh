0679475824bcd16d448cfa180566cb4288e5240a
Steven Penny <svnpenn@gmail.com>
2015-06-21 19:43:05 -0500

    #!/bin/sh

--- misc/install-jq.sh
+++ misc/install-jq.sh
@@ -1,14 +1,20 @@
-# up to date Windows native jq
-
-# autoconf  need for autoreconf
-# automake  need for autoreconf > aclocal
-# libtool   need for autoreconf > libtool
-# bison     need for make
-# flex      need for make
-pacman -S autoconf automake bison flex libtool
-
-git clone --depth 1 git://github.com/stedolan/jq
+#!/bin/sh
+type apt-cyg >/dev/null || exit
+# automake > autoreconf
+# diffutils > cmp > configure
+# libtool > autoreconf
+# mingw64-x86_64-gcc-core > x86_64-w64-mingw32-gcc > configure
+apt-cyg install automake curl diffutils libtool \
+  make mingw64-x86_64-gcc-core upx zip
+apt-cyg install --nodeps git
+# need full clone for autoreconf
+git clone --single-branch git://github.com/stedolan/jq
 cd jq
-autoreconf -i
-./configure
-make -j4 LDFLAGS=-all-static
+curl https://github.com/stedolan/jq/commit/97a2f34.diff | git apply
+autoreconf --install
+./configure --host x86_64-w64-mingw32
+make --jobs 4 LDFLAGS=-s
+upx -9 jq.exe
+set -o igncr
+vr=$(./jq --version)
+zip -9 "$vr".zip jq.exe
