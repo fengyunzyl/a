4af802f4ba80ddbad874e91d6ae5402b2080c31e
Steven Penny <svnpenn@gmail.com>
2015-08-22 21:42:11 -0500

    case "$1" in

--- misc/music.sh
+++ misc/music.sh
@@ -7,28 +7,44 @@ fi
 
 wget --output-document /tmp/alpha.htm "$1"
 printf '\e[1;33m'
-    
-awk '
-BEGIN {
-  RS = "\"?,\""
-  FS = "\":\"?"
-}
-/playback_count/ {
-  z = $2
-}
-/created_at/ {
-  y = $2
-}
-END {
-  # get age in seconds
-  x = systime() - mktime(gensub("[/:]", " ", "g", y))
 
+case "$1" in
+*soundcloud*)
+  v='
+  BEGIN {
+    RS = "\"?,\""
+    FS = "\":\"?"
+  }
+  /playback_count/ {
+    z = $2
+  }
+  /created_at/ {
+    y = gensub("[/:]", " ", "g", $2)
+  }
+  '
+;;
+*youtube*)
+  v='
+  BEGIN {
+    FS = "\""
+  }
+  /interactionCount/ {
+    z = $4
+  }
+  /datePublished/ {
+    y = gensub("-", " ", "g", $4) " 0 0 0"
+  }
+  '
+;;
+esac
+
+awk "$v"'
+END {
+  x = systime() - mktime(y)
   x /= 60 * 60 * 24
   printf "%d views / %d days = %d\n", z, x, z/x
-
   x *= 24
   printf "%d views / %d hours = %d\n", z, x, z/x
-
   x *= 60
   printf "%d views / %d minutes = %d\n", z, x, z/x
 }
