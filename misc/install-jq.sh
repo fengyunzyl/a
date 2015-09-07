262b87cfb3e13a1f10d555f3f0b62a0424530bca
Steven Penny <svnpenn@gmail.com>
2015-09-07 12:49:26 -0500

    # automake < autoreconf

--- misc/install-jq.sh
+++ misc/install-jq.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 type apt-cyg >/dev/null || exit
-# automake > autoreconf
-# diffutils > cmp > configure
-# libtool > autoreconf
-# mingw64-x86_64-gcc-core > x86_64-w64-mingw32-gcc > configure
-apt-cyg install automake curl diffutils libtool \
+# automake < autoreconf
+# bison < yacc < make
+# diffutils < cmp < configure
+# flex < make
+# libtool < autoreconf
+# mingw64-x86_64-gcc-core < x86_64-w64-mingw32-gcc < configure
+apt-cyg install automake bison diffutils flex libtool \
   make mingw64-x86_64-gcc-core upx zip
 apt-cyg install --nodeps git
 # need full clone for autoreconf
 git clone --single-branch git://github.com/stedolan/jq
 cd jq
-curl https://github.com/stedolan/jq/commit/97a2f34.diff | git apply
+git fetch origin pull/939/head:939
+git checkout 939
 autoreconf --install
 ./configure --host x86_64-w64-mingw32
-make --jobs 4 LDFLAGS=-s
+make --jobs 4 LDFLAGS='-s -lshlwapi'
 upx -9 jq.exe
-set -o igncr
-vr=$(./jq --version)
+vr=$(./jq --version | sed :)
 zip -9 "$vr".zip jq.exe
