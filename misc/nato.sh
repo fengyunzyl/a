51b0e6e696645329a8b5c2beaa0d63717ca05dc8
Steven Penny <svnpenn@gmail.com>
2015-12-16 23:44:11 -0600

    if [ $# != 2 ]

--- misc/nato.sh
+++ misc/nato.sh
@@ -1,20 +1,62 @@
 #!/bin/sh
-
-if [ $# = 0 ]
+if [ $# != 2 ]
 then
-  echo 'nato.sh [words per line]'
+  echo 'nato.sh [# of variables] [file]'
   exit
 fi
-
-z=$1
-
-y=(
-  al-pha bra-vo char-lie del-ta ech-o fox-trot golf ho-tel in-di-a ju-li-et
-  ki-lo li-ma mike no-vem-ber os-car pa-pa que-bec ro-me-o si-er-ra tan-go
-  u-ni-form vic-tor whis-key x-ray yan-kee zu-lu
-)
-
-for x in ${!y[*]}
+num_vars=$1
+inp_file=$2
+cat >/tmp/nfa_file <<+
+alpha
+bravo
+charlie
+delta
+echo
+foxtrot
+golf
+hotel
+india
+juliet
+kilo
+lima
+mike
+november
+oscar
+papa
+quebec
+romeo
+sierra
+tango
+uniform
+victor
+whiskey
+xray
+yankee
+zulu
++
+# length of variable name - november is longest
+# FIXME variable names need to be same length
+for var_leng in {1..8}
 do
-  echo ${y[*]:x:z}
+  # starting letter
+  for sta_lett in {1..26}
+  do
+    awk '
+    NR >= a && NR < a+b {
+      print substr($0, 1, c)
+    }
+    ' a=$sta_lett b=$num_vars c=$var_leng /tmp/nfa_file >/tmp/pat_file
+    if [ `wc -l </tmp/pat_file` -lt $num_vars ]
+    then
+      continue
+    fi
+    awk '{printf $0 FS}' /tmp/pat_file
+    if grep --quiet --file /tmp/pat_file "$inp_file"
+    then
+      echo BAD
+    else
+      echo GOOD
+      exit
+    fi
+  done
 done
