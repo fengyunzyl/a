3570ca8b655cdc8cf245f62e9cacd259eaf33467
Steven Penny <svnpenn@gmail.com>
2016-01-17 21:24:20 -0600

    mik="\

--- release.sh
+++ release.sh
@@ -1,39 +1,52 @@
 #!/bin/sh
+mik="\
+SYNOPSIS
+  release.sh [commit]
+
+LOCAL
+  1. commit program change
+  2. commit version change
+  3. tag new version
+
+REMOTE
+  1. push commits
+  2. push release
+"
 if [ $# != 1 ]
 then
-  echo 'release.sh [commit]'
+  printf "$mik"
   exit
 fi
-mik=$1
-nov=`git describe --tags --abbrev= $mik`
+nov=$1
+osc=`git describe --tags --abbrev= $nov`
 if [ $? = 0 ]
 then
-  echo "Last tag ‘$nov’"
+  echo "Last tag ‘$osc’"
 else
   echo 'No tags.'
   exit
 fi
 {
-  git diff --shortstat $nov $mik
-  git diff --shortstat `:|git mktree` $nov
+  git diff --shortstat $osc $nov
+  git diff --shortstat `:|git mktree` $osc
 } |
 awk '
 1
 NR == 1 {
-  osc = $4 > $6 ? $4 : $6
+  pap = $4 > $6 ? $4 : $6
 }
 NR == 2 {
-  pap = $4
+  que = $4
 }
 END {
   if (NR == 1) {
     print "No commits since last tag."
     exit
   }  
-  que = osc / pap * 100
-  if (que >= 100) rom = "major"
-  else if (que >= 10) rom = "minor"
-  else rom = "patch"
-  printf "%d / %d = %d% = %s\n", osc, pap, que, rom
+  rom = pap / que * 100
+  if (rom >= 100) sie = "major"
+  else if (rom >= 10) sie = "minor"
+  else sie = "patch"
+  printf "%d / %d = %d% = %s\n", pap, que, rom, sie
 }
 '
