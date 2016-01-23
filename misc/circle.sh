b981160af31f44e8fd65a02969f661b807a76909
Steven Penny <svnpenn@gmail.com>
2016-01-22 21:33:50 -0600

    then

---
+++ misc/circle.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ $# != 6 ]
+then
+  echo 'circle.sh [in radius] [x] [y] [in file] [out radius] [out file]'
+  exit
+fi
+if ! convert -version &>/dev/null
+then
+  echo 'convert not found'
+  exit
+fi
+
+qu=$(($1*2))
+ro=$(($5*2))
+
+convert "$4" -extent ${qu}x${qu}+${2}+${3} \
+  '(' +clone -alpha transparent -draw "circle $1,$1 $1,0" ')' \
+  -compose copyopacity -composite -resize ${ro}x${ro} "$6"
