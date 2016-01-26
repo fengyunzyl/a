88254a61d85774ec0b869728ea20f69574e6eef7
Steven Penny <svnpenn@gmail.com>
2016-01-25 18:25:31 -0600

    -define icon:auto-resize=16,48,256 -compress zip "$6"

--- misc/circle.sh
+++ misc/circle.sh
@@ -16,6 +16,6 @@ ro=$(($5*2))
 convert "$4" -extent ${qu}x${qu}+${2}+${3} \
   '(' +clone -alpha transparent -draw "circle $1,$1 $1,0" ')' \
   -compose copyopacity -composite -resize ${ro}x${ro} \
-  -define icon:auto-resize=16,256 -compress zip "$6"
+  -define icon:auto-resize=16,48,256 -compress zip "$6"
 
 ie4uinit -ClearIconCache
