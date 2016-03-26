0c5167f8cb7c45af6e99aac5733d41b4cb845620
Steven Penny <svnpenn@gmail.com>
2016-03-26 13:41:03 -0500

    then

--- misc/github.sh
+++ misc/github.sh
@@ -1,34 +1,38 @@
+#!/bin/dash
 # Binary search algorithm
 
-usage () {
-  echo usage: ${0##*/} torvalds/linux
+if [ $# != 2 ]
+then
+  echo 'github.sh [user] [repo]'
   exit
-}
+fi
 
-(( $# )) || usage
 # HTTPS is faster because no redirect
-url=https://github.com/$1/commit/HEAD~
-((up = 1))
+url=https://github.com/$1/$2/commits?page=
+up=1
 
 while wget -q --spider $url$up
 do
-  printf '%d\t' $up
-  printf '\e[1;32m%s\e[m\n' GOOD
-  ((lw = up))
-  ((up *= 2))
+  printf '%s%d\t' $url $up
+  echo OK
+  lw=$up
+  : $((up *= 2))
 done
 
 while :
 do
-  ((k = (lw + up) / 2))
-  ((k == lw)) && break
-  printf '%d\t' $k
+  : $((k = (lw + up) / 2))
+  if [ $k = $lw ]
+  then
+    break
+  fi
+  printf '%s%d\t' $url $k
   if wget -q --spider $url$k
   then
-    printf '\e[1;32m%s\e[m\n' GOOD
-    ((lw = k))
+    echo OK
+    lw=$k
   else
-    printf '\e[1;31m%s\e[m\n' BAD
-    ((up = k))
+    echo Not Found
+    up=$k
   fi
 done
