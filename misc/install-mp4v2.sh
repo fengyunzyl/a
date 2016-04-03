eb91dfa78dc0b82ded4138abeaed2b7f71a479d0
Steven Penny <svnpenn@gmail.com>
2016-04-03 01:10:09 -0500

    sage install automake git libtool make mingw64-i686-gcc-g++

---
+++ misc/install-mp4v2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sage install automake git libtool make mingw64-i686-gcc-g++
+git clone git://github.com/svnpenn/mp4v2
+cd mp4v2
+git clean -fdx
+git checkout .
+autoreconf --install --verbose
+./configure --host i686-w64-mingw32
+make --jobs 5 LDFLAGS=-all-static \
+  DEFS='-DMP4V2_USE_STATIC_LIB -D__STDC_CONSTANT_MACROS'
