4093fd1586d25ad65f14eddaa55eb50cf0e46dc2
Steven Penny <svnpenn@gmail.com>
2016-04-17 22:23:05 -0500

    mn() {

--- misc/mosaic.sh
+++ misc/mosaic.sh
@@ -22,27 +22,39 @@ mosaic.sh [options] [files]
                example  1920x1080,1280x1080,960x1080,640x1080
 "
 
-function mn {
+mn() {
   awk '{for (;NF-1;NF--) if ($1>$NF) $1=$NF} 1' RS=
 }
 
-function warn {
+warn() {
   printf '\e[36m%s\e[m\n' "$*"
 }
 
-function log {
-  sx=$(sh -xc ': "$@"' . "$@" 2>&1)
-  warn "${sx:4}"
+xc() {
+  awk '
+  BEGIN {
+    x = "\47"
+    printf "\33[36m"
+    while (++i < ARGC) {
+      y = split(ARGV[i], z, x)
+      for (j in z) {
+        printf z[j] ~ /[^[:alnum:]%+,./:=@_-]/ ? x z[j] x : z[j]
+        if (j < y) printf "\\" x
+      }
+      printf i == ARGC - 1 ? "\33[m\n" : FS
+    }
+  }
+  ' "$@"
   "$@"
 }
 
-if ! convert -version &>/dev/null
+if ! convert -version 2>&1 >/dev/null
 then
   echo convert not found
   exit
 fi
 
-if ! identify -version &>/dev/null
+if ! identify -version 2>&1 >/dev/null
 then
   echo identify not found
   exit
@@ -65,7 +77,7 @@ do
   m) IFS=, read -a dm <<< "$OPTARG" ;;
   esac
 done
-shift $((--OPTIND))
+shift $((OPTIND-1))
 
 sc=("$@")
 
@@ -80,35 +92,39 @@ if [ ${#dm} = 0 ]
 then
   ao=$(identify -format '%[fx:w/h>1]' "$@")
   case $ao in
-    11) dm=({1920,1920}x1080) ;;
-    001) dm=({960,960,1920}x1080) ;;
-    110) dm=({1920,1280,640}x1080) ;;
-    0001) dm=(640x1080{,,} 1920x1080) ;;
-    0101) dm=({640,1280,640,1280}x1080) ;;
-    0110) dm=({640,1280,1280,640}x1080) ;;
-    1000) dm=({1920,640,640,640}x1080) ;;
-    1001) dm=({1280,640,640,1280}x1080) ;;
-    1010) dm=({1280,640,1280,640}x1080) ;;
-    1111) dm=({960,960,960,960}x1080) ;;
-    0111) dm=({640,1280,960,960}x1080) ;;
-    1011) dm=({1280,640,960,960}x1080) ;;
-    1101) dm=({960,960,640,1280}x1080) ;;
-    1110) dm=({960,960,1280,640}x1080) ;;
-    00001) dm=({640,640,640,640,1280}x1080) ;;
-    00010) dm=({640,640,640,1280,640}x1080) ;;
-    01000) dm=({640,1280,640,640,640}x1080) ;;
-    10000) dm=({1280,640,640,640,640}x1080) ;;
-    00011) dm=({640,640,640,960,960}x1080) ;;
-    11000) dm=({960,960,640,640,640}x1080) ;;
-    000000) dm=({640,640,640,640,640,640}x1080) ;;
+    11) dm=(1920x1080 1920x1080) ;;
+    001) dm=(960x1080 960x1080 1920x1080) ;;
+    110) dm=(1920x1080 1280x1080 640x1080) ;;
+    0001) dm=(640x1080 640x1080 640x1080 1920x1080) ;;
+    0101) dm=(640x1080 1280x1080 640x1080 1280x1080) ;;
+    0110) dm=(640x1080 1280x1080 1280x1080 640x1080) ;;
+    1000) dm=(1920x1080 640x1080 640x1080 640x1080) ;;
+    1001) dm=(1280x1080 640x1080 640x1080 1280x1080) ;;
+    1010) dm=(1280x1080 640x1080 1280x1080 640x1080) ;;
+    1111) dm=(960x1080 960x1080 960x1080 960x1080) ;;
+    0111) dm=(640x1080 1280x1080 960x1080 960x1080) ;;
+    1011) dm=(1280x1080 640x1080 960x1080 960x1080) ;;
+    1101) dm=(960x1080 960x1080 640x1080 1280x1080) ;;
+    1110) dm=(960x1080 960x1080 1280x1080 640x1080) ;;
+    00001) dm=(640x1080 640x1080 640x1080 640x1080 1280x1080) ;;
+    00010) dm=(640x1080 640x1080 640x1080 1280x1080 640x1080) ;;
+    01000) dm=(640x1080 1280x1080 640x1080 640x1080 640x1080) ;;
+    10000) dm=(1280x1080 640x1080 640x1080 640x1080 640x1080) ;;
+    00011) dm=(640x1080 640x1080 640x1080 960x1080 960x1080) ;;
+    11000) dm=(960x1080 960x1080 640x1080 640x1080 640x1080) ;;
+    000000) dm=(640x1080 640x1080 640x1080 640x1080 640x1080 640x1080) ;;
     000011) dm=(640x1080 640x1080 640x1080 960x1080 960x540 960x540) ;;
     000110) dm=(640x1080 640x1080 640x1080 960x540 960x540 960x1080) ;;
-    011011) dm=(960x{1080,540,540,1080,540,540}) ;;
+    011011) dm=(960x1080 960x540 960x540 960x1080 960x540 960x540) ;;
     110000) dm=(960x540 960x540 960x1080 640x1080 640x1080 640x1080) ;;
     110011) dm=(960x540 960x540 960x1080 960x1080 960x540 960x540) ;;
-    110110) dm=(960x{540,540,1080,540,540,1080}) ;;
-    0000110) dm=(640x{1080,1080,1080,1080,540,540,1080}) ;;
-    11111111) dm=(960x540{,,,,,,,}) ;;
+    110110) dm=(960x540 960x540 960x1080 960x540 960x540 960x1080) ;;
+    0000110)
+      dm=(640x1080 640x1080 640x1080 640x1080 640x540 640x540 640x1080)
+    ;;
+    11111111)
+      dm=(960x540 960x540 960x540 960x540 960x540 960x540 960x540 960x540)
+    ;;
     *)
       echo cannot automatically set dimensions
       exit
@@ -117,15 +133,17 @@ then
 fi
 
 # extent must come after resize
-for ((o=0; o<$#; o++))
+go=0
+while [ $go -lt $# ]
 do
-  log convert -quality 100 \
+  xc convert -quality 100 \
   ${sv+-shave $sv} \
-  ${eg[o]:+-crop ${eg[o]}} \
-  ${rz[o]:+-resize ${dm[o]}^} \
-  -extent ${dm[o]} \
-  -gravity ${gv[o]:-center} \
-  {,=}"${sc[o]}"
+  ${eg[go]:+-crop ${eg[go]}} \
+  ${rz[go]:+-resize ${dm[go]}^} \
+  -extent ${dm[go]} \
+  -gravity ${gv[go]:-center} \
+  {,=}"${sc[go]}"
+  go=$((go+1))
 done
 
 # combine
@@ -134,27 +152,28 @@ ${dry+exit}
 ht=$(identify -format '%h\n' "$@" | mn)
 
 set =*
+br=`mktemp`
 case $ao in
-000011) log convert "$1" "$2" "$3" "$4" '(' "$5" "$6" -append ')' +append \
-  -quality 100 out ;;
-000110) log convert "$1" "$2" "$3" '(' "$4" "$5" -append ')' "$6" +append \
-  -quality 100 out ;;
-011011) log convert "$1" '(' "$2" "$3" -append ')' "$4" \
-  '(' "$5" "$6" -append ')' +append -quality 100 out ;;
-110000) log convert '(' "$1" "$2" -append ')' "$3" "$4" "$5" "$6" +append \
-  -quality 100 out ;;
-110011) log convert '(' "$1" "$2" -append ')' "$3" "$4" \
-  '(' "$5" "$6" -append ')' +append -quality 100 out ;;
-110110) log convert '(' "$1" "$2" -append ')' "$3" \
-  '(' "$4" "$5" -append ')' "$6" +append -quality 100 out ;;
-0000110) log convert "$1" "$2" "$3" "$4" \
-  '(' "$5" "$6" -append ')' "$7" +append -quality 100 out ;;
-11111111) log convert '(' "$1" "$2" -append ')' '(' "$3" "$4" -append ')' \
+000011) xc convert "$1" "$2" "$3" "$4" '(' "$5" "$6" -append ')' +append \
+  -quality 100 "$br" ;;
+000110) xc convert "$1" "$2" "$3" '(' "$4" "$5" -append ')' "$6" +append \
+  -quality 100 "$br" ;;
+011011) xc convert "$1" '(' "$2" "$3" -append ')' "$4" \
+  '(' "$5" "$6" -append ')' +append -quality 100 "$br" ;;
+110000) xc convert '(' "$1" "$2" -append ')' "$3" "$4" "$5" "$6" +append \
+  -quality 100 "$br" ;;
+110011) xc convert '(' "$1" "$2" -append ')' "$3" "$4" \
+  '(' "$5" "$6" -append ')' +append -quality 100 "$br" ;;
+110110) xc convert '(' "$1" "$2" -append ')' "$3" \
+  '(' "$4" "$5" -append ')' "$6" +append -quality 100 "$br" ;;
+0000110) xc convert "$1" "$2" "$3" "$4" \
+  '(' "$5" "$6" -append ')' "$7" +append -quality 100 "$br" ;;
+11111111) xc convert '(' "$1" "$2" -append ')' '(' "$3" "$4" -append ')' \
   '(' "$5" "$6" -append ')' '(' "$7" "$8" -append ')' +append \
-  -quality 100 out ;;
-*) log convert "$@" +append -quality 100 out ;;
+  -quality 100 "$br" ;;
+*) xc convert "$@" +append -quality 100 "$br" ;;
 esac
 
 sn=$(basename ~+)
-log mv out "s $sn h $ht".jpg
-log rm "$@"
+xc mv "$br" "s $sn h $ht".jpg
+xc rm "$@"
