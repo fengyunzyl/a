08eaf1068e7021f5d265ea695de1d4d0a8dec430
Steven Penny <svnpenn@gmail.com>
2016-05-02 00:27:06 -0500

    echo 'trending.sh [page] [language] [maxsize]'

--- etc/trending.sh
+++ etc/trending.sh
@@ -1,29 +1,35 @@
-#!/bin/sh
+#!/bin/dash
 if [ $# != 3 ]
 then
-  echo trending.sh PAGE LANGUAGE MAXSIZE
+  echo 'trending.sh [page] [language] [maxsize]'
+  exit
+fi
+if [ "$3" -ge 10000000000 ]
+then
+  echo 'maxsize too high'
   exit
 fi
 
 cd /tmp
 # For unauthenticated requests, the rate limit allows you to make up to 5
 # requests per minute.
-curl https://api.github.com/search/repositories?page=$1 -Gso repos.json \
-  --data-urlencode "q=size:<=$3 language:$2" \
-  --data-urlencode sort=stars \
-  --data-urlencode order=desc
+curl --get --remote-name \
+--data-urlencode page="$1" --data-urlencode q="size:<=$3 language:$2" \
+https://api.github.com/search/repositories
 
-printf '\e[1;32m%s\e[m\n' "star count and size of $2 repos under $3 kB"
+printf '\33[1;32m%s\33[m\n' "star count and size of $2 repos under $3 kB"
 
-jq -r '.items[] |
-  "\(.stargazers_count)\t\(.size)\t\(.language)\t\(.html_url)\t\(.description)"' repos.json |
-  awk '{
-  v = sprintf("%s %s %s \33[1;31m%s\33[m %s",
-    $1,
-    $2,
-    $3,
-    substr($4, 20),
-    $5)
+jq -r '
+.items[] |
+"\(.stargazers_count)\t\(.size)\t\(.language)\t\(.html_url)\t\(.description)"
+' repositories |
+awk '
+BEGIN {
+  FS = "\t"
+}
+{
+  v = sprintf("%s %s %s \33[1;31m%s\33[m %s", $1, $2, $3, substr($4, 20), $5)
   print substr(v, 0, 79 + 10)
-  }' FS='\t' |
-  sort -nr
+}
+' |
+sort -nr
