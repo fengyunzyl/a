2198002e5b5627c915d405c6c154c4848612f634
Steven Penny <svnpenn@gmail.com>
2016-05-26 12:25:48 -0500

    ff-sequence.sh [file]

--- ff/ff-sequence.sh
+++ ff/ff-sequence.sh
@@ -1,27 +1,9 @@
-#!/bin/sh
-xc() {
-  awk '
-  BEGIN {
-    x = "\47"
-    printf "\33[36m"
-    while (++i < ARGC) {
-      y = split(ARGV[i], z, x)
-      for (j in z) {
-        printf z[j] ~ /[^[:alnum:]%+,./:=@_-]/ ? x z[j] x : z[j]
-        if (j < y) printf "\\" x
-      }
-      printf i == ARGC - 1 ? "\33[m\n" : FS
-    }
-  }
-  ' "$@"
-  "$@"
-}
-
-if [ "$#" != 4 ]
+#!/bin/dash
+if [ "$#" != 1 ]
 then
   cat <<+
 SYNOPSIS
-  ff-sequence.sh [start] [duration] [file]
+  ff-sequence.sh [file]
 
 DESCRIPTION
   Make an image sequence from a video
@@ -29,4 +11,5 @@ DESCRIPTION
   exit
 fi
 
-xc ffmpeg -hide_banner -ss $1 -i "$3" -t $2 -q 1 %d.jpg
+ffmpeg -hide_banner -i "$1" -vf "select='eq(pict_type,I)'" -vsync vfr \
+-q 1 %d.jpg
