cc129a040b48cc8398a66d1d82eefbbc84373846
Steven Penny <svnpenn@gmail.com>
2016-05-30 18:15:37 -0500

    ju=$1

--- etc/highlight.sh
+++ etc/highlight.sh
@@ -1,19 +1,47 @@
-#!/bin/sh
+#!/bin/dash
 if [ "$#" != 1 ]
 then
   echo 'highlight.sh [file]'
   exit
 fi
-alfa=(
-  lang-sh lang-c lang-cs lang-clj lang-coffee lang-css lang-dart lang-pascal
-  lang-erlang lang-go lang-hs lang-html lang-java lang-javascript lang-json
-  lang-tex lang-lisp lang-lua lang-fs lang-pascal lang-perl lang-php lang-proto
-  lang-python lang-r lang-regex lang-ruby lang-rust lang-scala lang-sql
-  lang-vhdl lang-vb lang-xml
-)
-for bravo in ${alfa[*]}
+ju=$1
+while read ki
 do
-  printf '%s\n\n<!-- language: %s -->\n\n' $bravo{,}
-  cat "$1"
+  printf '%s\n\n<!-- language: %s -->\n\n' "$ki" "$ki"
+  cat "$ju"
   echo
-done
+done <<+
+lang-sh
+lang-c
+lang-cs
+lang-clj
+lang-coffee
+lang-css
+lang-dart
+lang-pascal
+lang-erlang
+lang-go
+lang-hs
+lang-html
+lang-java
+lang-javascript
+lang-json
+lang-tex
+lang-lisp
+lang-lua
+lang-fs
+lang-pascal
+lang-perl
+lang-php
+lang-proto
+lang-python
+lang-r
+lang-regex
+lang-ruby
+lang-rust
+lang-scala
+lang-sql
+lang-vhdl
+lang-vb
+lang-xml
++
