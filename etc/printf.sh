853b241eabc22e3677c68aa9c834468161ff969a
Steven Penny <svnpenn@gmail.com>
2016-05-30 18:21:20 -0500

    cat <<+

--- etc/printf.sh
+++ etc/printf.sh
@@ -1,21 +1,45 @@
-#!/bin/sh
+#!/bin/dash
 if [ "$#" = 0 ]
 then
-  echo 'printf.sh [-e] [input]'
-  echo '-e   treat input as expression instead of string'
+  cat <<+
+printf.sh [-e] [input]
+-e    treat input as expression instead of string
++
   exit
 fi
 
-z=(
-  %a %b %d %e %.0f %.7f
-  %g %h %i %j %k %l %m %n %o %p %q %r %s %t %u %v %w %x %y %z
-)
-
 if [ "$1" = -e ]
 then
-  printf -vy '{w=%s}' "$2"
+  al=$(printf '{br=%s}' "$2")
 else
-  printf -vy '{w="%s"}' "$1"
+  al=$(printf '{br="%s"}' "$1")
 fi
 
-printf '%s\n' "${z[@]}" | awk "$y"'{printf "%" $0 "\t" $0 "\n", w}'
+awk "$al"'{printf "%" $0 "\t" $0 "\n", br}' <<+
+%a
+%b
+%d
+%e
+%.0f
+%.7f
+%g
+%h
+%i
+%j
+%k
+%l
+%m
+%n
+%o
+%p
+%q
+%r
+%s
+%t
+%u
+%v
+%w
+%x
+%y
+%z
++
