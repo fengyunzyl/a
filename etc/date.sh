d32ffa1cec47d322583c257009e0212d1ba2f205
Steven Penny <svnpenn@gmail.com>
2016-05-30 18:15:00 -0500

    then

--- etc/date.sh
+++ etc/date.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-usage="\
+#!/bin/dash
+if [ "$#" != 1 ]
+then
+  cat <<+
 NAME
   date.sh
 
@@ -8,22 +10,60 @@ SYNOPSIS
 
 EXAMPLE
   date.sh 2015-5-15
-"
-
-if [ "$#" != 1 ]
-then
-  printf "$usage"
++
   exit
 fi
 
-sq=(
-  %a %b %c %d %e %g %h %j %k %l %m %p %r %s %u %w %x %y %z
-  %A %B %C %D %F %G %H %I %M %N %P %R %S %T %U %V %W %X %Y %Z %:z %::z %:::z
-  %Y%m%d %H%M%S '%b %-d %Y'
-)
+al=$1
 
-for each in "${sq[@]}"
+while read br
 do
-  printf '%-11s' "$each"
-  date -d "$1" +"$each"
-done
+  printf '%-11s' "$br"
+  date -d "$al" +"$br"
+done <<+
+%a
+%b
+%c
+%d
+%e
+%g
+%h
+%j
+%k
+%l
+%m
+%p
+%r
+%s
+%u
+%w
+%x
+%y
+%z
+%A
+%B
+%C
+%D
+%F
+%G
+%H
+%I
+%M
+%N
+%P
+%R
+%S
+%T
+%U
+%V
+%W
+%X
+%Y
+%Z
+%:z
+%::z
+%:::z
+%Y%m%d
+%H%M%S
+%b %-d %Y
++
