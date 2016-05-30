b360d19976c4ad763ebd660698b5afd46a321e28
Steven Penny <svnpenn@gmail.com>
2016-05-30 18:26:52 -0500

    j=$1

--- etc/stat.sh
+++ etc/stat.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/dash
 if [ "$#" != 1 ]
 then
   echo 'stat.sh [item]'
   exit
 fi
+j=$1
 
-for each in a b c d f g h i l m n o s t u w x y z A B D F G N S T U W X Y Z
+for k in a b c d f g h i l m n o s t u w x y z A B D F G N S T U W X Y Z
 do
-  printf '%s\t' $each
-  stat -c %$each "$1"
+  printf '%s\t' "$k"
+  stat -c %"$k" "$j"
 done
