0d212327dc64be48c05b93d7fd8b824dd3e293e0
Steven Penny <svnpenn@gmail.com>
2016-06-04 15:53:47 -0500

    find "$PWD" -mindepth 2 -maxdepth 2 '(' -name '*.sh' -o -name '*.awk' ')' \

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
-find "$PWD" -mindepth 2 '(' -name '*.sh' -o -name '*.awk' ')' \
+find "$PWD" -mindepth 2 -maxdepth 2 '(' -name '*.sh' -o -name '*.awk' ')' \
 -exec ln -sft /usr/local/bin {} +
