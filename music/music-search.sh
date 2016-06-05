e65c0555f7e21bee0a14ffb8acf850d1499e819f
Steven Penny <svnpenn@gmail.com>
2016-06-04 20:18:46 -0500

    youtube.com/results?q=-intitle:$1 $1 intitle:"$2", hd

--- music/music-search.sh
+++ music/music-search.sh
@@ -15,7 +15,7 @@ do
   "$BROWSER" "$j"
 done <<+
 youtube.com/results?q="$1 - Topic" intitle:$2
-youtube.com/results?q=-intitle:$1 $1 intitle:$2, hd
+youtube.com/results?q=-intitle:$1 $1 intitle:"$2", hd
 youtube.com/results?q=allintitle:$1 $2, hd
 youtube.com/results?q=allintitle:$1 $2
 google.com/search?tbm=vid&q=$1 intitle:"$2"
