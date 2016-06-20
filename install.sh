f276cbf4653adc9a2faf5799678202019effa6a9
Steven Penny <svnpenn@gmail.com>
2016-06-20 06:38:16 -0500

    '(' -name '*.sh' -o -name '*.awk' -o -name '*.vim' ')' \

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/dash
 find "$PWD" -mindepth 2 -maxdepth 2 \
-'(' -name '*.sh' -o -name '*.awk' -o -name '*.ex' ')' \
+'(' -name '*.sh' -o -name '*.awk' -o -name '*.vim' ')' \
 -exec ln -sft /usr/local/bin {} +
