26231e8d956b69b9f08c09d0ec815577cced983f
Steven Penny <svnpenn@gmail.com>
2016-07-04 14:23:01 -0500

    youtube.com/results?q="$1 - Topic" intitle:"$2"

--- music/music-search.sh
+++ music/music-search.sh
@@ -14,7 +14,7 @@ while read j
 do
   "$BROWSER" "$j"
 done <<+
-youtube.com/results?q="$1 - Topic" intitle:$2
+youtube.com/results?q="$1 - Topic" intitle:"$2"
 youtube.com/results?q=-intitle:"$1" "$1" intitle:"$2", hd
 youtube.com/results?q=allintitle:$1 $2, hd
 youtube.com/results?q=allintitle:$1 $2
